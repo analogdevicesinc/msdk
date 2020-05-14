f545a996345e0be955c0fb8552375044206154f4
Sadik Ozer <sadik.ozer@maximintegrated.com>
2020-05-14 10:21:37 +0300

    [MSDK-152] Fix issue: Generate doxygen documentation
    Signed-off-by: Sadik Ozer <sadik.ozer@maximintegrated.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #
-#	Script to build example
+#	Script to build / clean and generate documentation
 #
 
 if [ $# -lt 1 ]; then
 	echo "Usage error"
 	echo "Usage: build.sh <operation> <float_type>"
-	echo "<operation> : build or clean"
+	echo "<operation> : "
+	echo "              build: To build NFC example"
+	echo "              doc  : To generate doxygen documentation"
+	echo "              clean: To clean generated files and create fresh setup"
 	echo "<float_type>: (optional) hard or soft"
 	exit 1
 fi
@@ -16,38 +19,102 @@ fp_type=$2
 
 root_dir=$(pwd)
 
-if [ "${fp_type}" == "hard" ] || [ "${fp_type}" == "Hard" ]; then
-	fp_type="hard"
-else
-	fp_type="soft"
-fi
+example="NFC_PCD_DTE_EMV_Loopback"
 
 export PATH="/c/MaximSDK/Tools/GNUTools/bin:/c/MaximSDK/Tools/MinGW/msys/1.0/bin:$PATH"
-cp ${root_dir}/Tools/SBT/bin/sign_app.exe ./Tools/SBT/bin/sign_app
 
+#
+#	Decide Operation
+#
+case "$operation" in
+	build)
+		echo "------- Build START: $example -------"
+		
+		if [ "${fp_type}" == "hard" ] || [ "${fp_type}" == "Hard" ]; then
+			fp_type="hard"
+		else
+			fp_type="soft"
+		fi
 
-declare -a examples
-examples+=('NFC_PCD_DTE_EMV_Loopback')
+		cp ${root_dir}/Tools/SBT/bin/sign_app.exe ${root_dir}/Tools/SBT/bin/sign_app
 
-cd ./Examples/MAX32570/
-	for example in "${examples[@]}"
-	do
-		cd $example/
-			if [ "${operation}" == "clean" ] || [ "${operation}" == "Clean" ]; then
-				echo "------- Clean START : $example  -------"
-				make ECLIPSE=1 clean
-				rm -rf ${root_dir}/Libraries/NFC/*/*.a
-				echo " "
-				echo "------- Clean END   : $example -------"
-			else
-				echo "------- Build START: $example -------"
-				make ECLIPSE=1 MFLOAT_FLAGS=${fp_type}
-				echo " "
-				echo "------- Build END  : $example -------"
+		cd ./Examples/MAX32570/$example/
+			make ECLIPSE=1 MFLOAT_FLAGS=${fp_type}
+			echo " "
+		cd $root_dir
+		
+		echo "------- Build END  : $example -------"
+		;;
+	
+	clean)	
+		echo "------- Clean START : $example  -------"
+		
+		cd ./Examples/MAX32570/$example/
+			make ECLIPSE=1 clean
+			rm -rf ${root_dir}/Libraries/NFC/*/*.a
+			
+			# remove doxy files
+			rm -rf Internal_Only/DoxyOutput
+			rm -rf Internal_Only/doxy_warnings.log
+			git checkout Internal_Only/doxyfile
+			
+			echo " "
+		cd $root_dir	
+		
+		# revert out sing_app file
+		git checkout ${root_dir}/Tools/SBT/bin/sign_app
+			
+		
+		echo "------- Clean END   : $example -------"
+		;;	
+		
+	doc)
+		echo "------- Documentation START : $example  -------"
+		
+		cd ./Examples/MAX32570/$example/Internal_Only
+		 
+			gittag=`git describe --abbrev=0 --tags`
+			gittag=${gittag%%-*}
+
+			echo "Current tag: $gittag"
+
+			out_dir="DoxyOutput"
+			file_name="NFC_PCD_CSP"
+
+			sed -i -e "s/<__PROJECT_VERSION__>/$gittag/"  ./doxyfile
+			sed -i -e "s/<__CHM_FILE_NAME__>/$file_name/" ./doxyfile
+			
+			# define MAXIM_PATH
+			export MAXIM_PATH="../../../.."
+					
+			doxygen ./doxyfile
+
+			doxy_warning_file=$(cat ./doxy_warnings.log)
+
+			if [[ $doxy_warning_file != "" ]]
+			then
+			   echo "Error!!!: Please check doxy_warning.log file:"
+			   cat $doxy_warning_file
+			   echo "------------------ END doxy warning file ---------------------"
+			   cd $root_dir
+			   exit 1
 			fi
-		cd ../
-	done
-cd $root_dir
+
+			# Create packet
+			mv ./$out_dir/html/${file_name}.chm ./$out_dir/
+
+			7z a -tzip ./$out_dir/${file_name}_html-$tag.zip ./$out_dir/html
+
+		cd $root_dir
+		
+		echo "------- Documentation END   : $example -------"
+		
+		;;	
+		
+	*)
+		echo "Error! Not supported command"
+		exit 1
+esac
 
 
 exit 0
