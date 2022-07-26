5bb2278826a7fdef1924bfefaad6311ab6104593
Jake Carter <Jake.Carter@analog.com>
2022-07-26 18:36:07 -0500

    [MSDK-688] Use new library toggle switches

--- Examples/MAX78000/SDHC_FTHR/project.mk
+++ Examples/MAX78000/SDHC_FTHR/project.mk
@@ -12,8 +12,5 @@
 override BOARD=FTHR_RevA
 $(warning Warning: This project is forced to compile for the FTHR board only!)
 
-SDHC_DRIVER_DIR=$(LIBS_DIR)/SDHC
-FAT32_DRIVER_DIR=$(SDHC_DRIVER_DIR)/ff14
-include $(FAT32_DRIVER_DIR)/fat32.mk
-include $(SDHC_DRIVER_DIR)/sdhc.mk
+LIB_SDHC = 1
 
