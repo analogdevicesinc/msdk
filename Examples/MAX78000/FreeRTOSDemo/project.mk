40aa0a1aeeaf01f3b318f257440914315b36a3a4
Kevin Gillespie <Kevin.Gillespie@analog.com>
2022-07-28 13:59:32 -0500

    Adjusting Makefile includes.
    Removing unused libraries.
    Moving extra library includes to project.mk.

--- Examples/MAX78000/FreeRTOSDemo/project.mk
+++ Examples/MAX78000/FreeRTOSDemo/project.mk
@@ -9,3 +9,19 @@
 
 LIB_FREERTOS = 1
 
+# FreeRTOS (Disabled by default)
+# ************************
+LIB_FREERTOS ?= 0
+ifeq ($(LIB_FREERTOS), 1)
+# Where to find FreeRTOSConfig.h
+RTOS_CONFIG_DIR ?= .
+
+# Include FreeRTOS-Plus-CLI
+IPATH += $(LIBS_DIR)/FreeRTOS-Plus/Source/FreeRTOS-Plus-CLI
+VPATH += $(LIBS_DIR)/FreeRTOS-Plus/Source/FreeRTOS-Plus-CLI
+SRCS += FreeRTOS_CLI.c
+
+# Include the FreeRTOS library
+include $(LIBS_DIR)/FreeRTOS/freertos.mk
+endif
+# ************************
