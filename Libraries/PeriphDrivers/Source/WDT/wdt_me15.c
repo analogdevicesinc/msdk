925089d7068eabaa76807f072f0c979926ea02d8
Scheiffler <Jacob.Scheiffler@analog.com>
2022-07-29 13:59:35 -0500

    Fixing MAX32670 RevA silicon watchdog enable/disable bug.

--- Libraries/PeriphDrivers/Source/WDT/wdt_me15.c
+++ Libraries/PeriphDrivers/Source/WDT/wdt_me15.c
@@ -81,18 +81,28 @@ void MXC_WDT_SetResetPeriod(mxc_wdt_regs_t* wdt, mxc_wdt_cfg_t* cfg)
 
 void MXC_WDT_Enable(mxc_wdt_regs_t* wdt)
 {
-    __disable_irq();
-    wdt->rst = 0xFE;
-    wdt->rst = 0XED;
-    __enable_irq();
+	if((MXC_GCR->revision & 0x00F0) == 0x00A0) {
+		MXC_WDT_RevB_Enable((mxc_wdt_revb_regs_t*) wdt);
+	}
+	else {
+		__disable_irq();
+		wdt->rst = 0xFE;
+		wdt->rst = 0XED;
+		__enable_irq();
+	}
 }
 
 void MXC_WDT_Disable(mxc_wdt_regs_t* wdt)
 {
-    __disable_irq();
-    wdt->rst = 0xDE;
-    wdt->rst = 0XAD;
-    __enable_irq();
+	if((MXC_GCR->revision & 0x00F0) == 0x00A0) {
+		MXC_WDT_RevB_Disable((mxc_wdt_revb_regs_t*) wdt);
+	}
+	else {
+		__disable_irq();
+		wdt->rst = 0xDE;
+		wdt->rst = 0XAD;
+		__enable_irq();
+	}
 }
 
 void MXC_WDT_EnableInt(mxc_wdt_regs_t* wdt)
