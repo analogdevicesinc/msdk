1e462b1312cfab1d0f06824f90ae2b6803c45289
SRV-JenkinsIDE <SRV-JenkinsIDE@maximintegrated.com>
2022-08-01 16:25:38 -0500

    IAR/KEIL Project files added for MAX32655, MAX32672, MAX32675, MAX32690

---
+++ Examples/MAX32672/DMA/ARM/RTE/_Flash_Debug/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DMA' 
+ * Target:  'Flash Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "max32672.h"
+
+/* Maxim.MAX32672 BSP::BSP:EvKit V1:1.4.1 */
+#define BOARD EvKit_V1 /* Target Board */
+
+
+#endif /* RTE_COMPONENTS_H */
