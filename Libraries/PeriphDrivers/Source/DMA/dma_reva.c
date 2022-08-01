df82f82374ef521a7103911de37dd9d589eb45a0
Woo <Sihyung.Woo@analog.com>
2022-08-01 12:44:01 -0500

    MSDK-449: Updated memcpy_callback (dma_reva.c) function to release memcpy resource, thus allowing multiple MXC_DMA_MemCpy function calls.

--- Libraries/PeriphDrivers/Source/DMA/dma_reva.c
+++ Libraries/PeriphDrivers/Source/DMA/dma_reva.c
@@ -466,7 +466,10 @@ void memcpy_callback(int ch, int error)
     
     callback(memcpy_resource[ch].dest);
     
+    // Release global objects and local resources
     callback = NULL;
+    memcpy_resource[ch].userCallback = NULL;
+    memcpy_resource[ch].dest = NULL;
     MXC_DMA_ReleaseChannel(ch);
 }
 
