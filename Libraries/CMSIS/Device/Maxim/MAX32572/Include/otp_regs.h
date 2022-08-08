22eebc77b1867100bf8b59cf57b889564d5072c5
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2022-08-08 15:45:05 -0500

    OTP & ME55: Initial Commit for OTP, and added OTP register and library files for ME55.

---
+++ Libraries/CMSIS/Device/Maxim/MAX32572/Include/otp_regs.h
@@ -0,0 +1,222 @@
+/**
+ * @file    otp_regs.h
+ * @brief   Registers, Bit Masks and Bit Positions for the OTP Peripheral Module.
+ */
+
+/******************************************************************************
+* Copyright (C) 2022 Maxim Integrated Products, Inc., All Rights Reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* Except as contained in this notice, the name of Maxim Integrated
+* Products, Inc. shall not be used except as stated in the Maxim Integrated
+* Products, Inc. Branding Policy.
+*
+* The mere transfer of this software does not imply any licenses
+* of trade secrets, proprietary technology, copyrights, patents,
+* trademarks, maskwork rights, or any other form of intellectual
+* property whatsoever. Maxim Integrated Products, Inc. retains all
+* ownership rights.
+*
+******************************************************************************/
+
+#ifndef _OTP_REGS_H_
+#define _OTP_REGS_H_
+
+/* **** Includes **** */
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined (__ICCARM__)
+  #pragma system_include
+#endif
+
+#if defined (__CC_ARM)
+  #pragma anon_unions
+#endif
+/// @cond
+/*
+    If types are not defined elsewhere (CMSIS) define them here
+*/
+#ifndef __IO
+#define __IO volatile
+#endif
+#ifndef __I
+#define __I  volatile const
+#endif
+#ifndef __O
+#define __O  volatile
+#endif
+#ifndef __R
+#define __R  volatile const
+#endif
+/// @endcond
+
+/* **** Definitions **** */
+
+/**
+ * @ingroup     otp
+ * @defgroup    otp_registers OTP_Registers
+ * @brief       Registers, Bit Masks and Bit Positions for the OTP Peripheral Module.
+ * @details One-Time Programmable (OTP) Memory Controller.
+ */
+
+/**
+ * @ingroup otp_registers
+ * Structure type to access the OTP Registers.
+ */
+typedef struct {
+    __IO uint32_t ctrl;                 /**< <tt>\b 0x00:</tt> OTP CTRL Register */
+    __IO uint32_t clkdiv;               /**< <tt>\b 0x04:</tt> OTP CLKDIV Register */
+    __IO uint32_t rdata;                /**< <tt>\b 0x08:</tt> OTP RDATA Register */
+    __IO uint32_t status;               /**< <tt>\b 0x0C:</tt> OTP STATUS Register */
+    __R  uint32_t rsv_0x10_0x2f[8];
+    __IO uint32_t wdata;                /**< <tt>\b 0x30:</tt> OTP WDATA Register */
+    __R  uint32_t rsv_0x34_0x3b[2];
+    __IO uint32_t actrl0;               /**< <tt>\b 0x3C:</tt> OTP ACTRL0 Register */
+    __IO uint32_t actrl1;               /**< <tt>\b 0x40:</tt> OTP ACTRL1 Register */
+} mxc_otp_regs_t;
+
+/* Register offsets for module OTP */
+/**
+ * @ingroup    otp_registers
+ * @defgroup   OTP_Register_Offsets Register Offsets
+ * @brief      OTP Peripheral Register Offsets from the OTP Base Peripheral Address.
+ * @{
+ */
+ #define MXC_R_OTP_CTRL                     ((uint32_t)0x00000000UL) /**< Offset from OTP Base Address: <tt> 0x0000</tt> */ 
+ #define MXC_R_OTP_CLKDIV                   ((uint32_t)0x00000004UL) /**< Offset from OTP Base Address: <tt> 0x0004</tt> */ 
+ #define MXC_R_OTP_RDATA                    ((uint32_t)0x00000008UL) /**< Offset from OTP Base Address: <tt> 0x0008</tt> */ 
+ #define MXC_R_OTP_STATUS                   ((uint32_t)0x0000000CUL) /**< Offset from OTP Base Address: <tt> 0x000C</tt> */ 
+ #define MXC_R_OTP_WDATA                    ((uint32_t)0x00000030UL) /**< Offset from OTP Base Address: <tt> 0x0030</tt> */ 
+ #define MXC_R_OTP_ACTRL0                   ((uint32_t)0x0000003CUL) /**< Offset from OTP Base Address: <tt> 0x003C</tt> */ 
+ #define MXC_R_OTP_ACTRL1                   ((uint32_t)0x00000040UL) /**< Offset from OTP Base Address: <tt> 0x0040</tt> */ 
+/**@} end of group otp_registers */
+
+/**
+ * @ingroup  otp_registers
+ * @defgroup OTP_CTRL OTP_CTRL
+ * @brief    OTP Control Register.
+ * @{
+ */
+ #define MXC_F_OTP_CTRL_ADDR_POS                        0 /**< CTRL_ADDR Position */
+ #define MXC_F_OTP_CTRL_ADDR                            ((uint32_t)(0x1FFUL << MXC_F_OTP_CTRL_ADDR_POS)) /**< CTRL_ADDR Mask */
+
+ #define MXC_F_OTP_CTRL_READ_POS                        10 /**< CTRL_READ Position */
+ #define MXC_F_OTP_CTRL_READ                            ((uint32_t)(0x1UL << MXC_F_OTP_CTRL_READ_POS)) /**< CTRL_READ Mask */
+
+ #define MXC_F_OTP_CTRL_WRITE_POS                       11 /**< CTRL_WRITE Position */
+ #define MXC_F_OTP_CTRL_WRITE                           ((uint32_t)(0x1UL << MXC_F_OTP_CTRL_WRITE_POS)) /**< CTRL_WRITE Mask */
+
+/**@} end of group OTP_CTRL_Register */
+
+/**
+ * @ingroup  otp_registers
+ * @defgroup OTP_CLKDIV OTP_CLKDIV
+ * @brief    OTP Clock Divide Register.
+ * @{
+ */
+ #define MXC_F_OTP_CLKDIV_CLKDIV_POS                    0 /**< CLKDIV_CLKDIV Position */
+ #define MXC_F_OTP_CLKDIV_CLKDIV                        ((uint32_t)(0x3FUL << MXC_F_OTP_CLKDIV_CLKDIV_POS)) /**< CLKDIV_CLKDIV Mask */
+
+ #define MXC_F_OTP_CLKDIV_SPWE_POS                      8 /**< CLKDIV_SPWE Position */
+ #define MXC_F_OTP_CLKDIV_SPWE                          ((uint32_t)(0x1UL << MXC_F_OTP_CLKDIV_SPWE_POS)) /**< CLKDIV_SPWE Mask */
+
+ #define MXC_F_OTP_CLKDIV_PD_POS                        9 /**< CLKDIV_PD Position */
+ #define MXC_F_OTP_CLKDIV_PD                            ((uint32_t)(0x1UL << MXC_F_OTP_CLKDIV_PD_POS)) /**< CLKDIV_PD Mask */
+
+ #define MXC_F_OTP_CLKDIV_RDY_POS                       16 /**< CLKDIV_RDY Position */
+ #define MXC_F_OTP_CLKDIV_RDY                           ((uint32_t)(0x1UL << MXC_F_OTP_CLKDIV_RDY_POS)) /**< CLKDIV_RDY Mask */
+
+/**@} end of group OTP_CLKDIV_Register */
+
+/**
+ * @ingroup  otp_registers
+ * @defgroup OTP_RDATA OTP_RDATA
+ * @brief    GPIO Clear Function Enable Register. Writing a 1 to one or more bits in this
+ *           register clears the bits in the same positions in GPIO_EN to 0, without
+ *           affecting other bits in that register.
+ * @{
+ */
+ #define MXC_F_OTP_RDATA_DATA_POS                       0 /**< RDATA_DATA Position */
+ #define MXC_F_OTP_RDATA_DATA                           ((uint32_t)(0xFFFFFFFFUL << MXC_F_OTP_RDATA_DATA_POS)) /**< RDATA_DATA Mask */
+
+/**@} end of group OTP_RDATA_Register */
+
+/**
+ * @ingroup  otp_registers
+ * @defgroup OTP_STATUS OTP_STATUS
+ * @brief    OTP Status Register.
+ * @{
+ */
+ #define MXC_F_OTP_STATUS_BUSY_POS                      0 /**< STATUS_BUSY Position */
+ #define MXC_F_OTP_STATUS_BUSY                          ((uint32_t)(0x1UL << MXC_F_OTP_STATUS_BUSY_POS)) /**< STATUS_BUSY Mask */
+
+ #define MXC_F_OTP_STATUS_FAIL_POS                      1 /**< STATUS_FAIL Position */
+ #define MXC_F_OTP_STATUS_FAIL                          ((uint32_t)(0x1UL << MXC_F_OTP_STATUS_FAIL_POS)) /**< STATUS_FAIL Mask */
+
+ #define MXC_F_OTP_STATUS_UNLOCK1_POS                   8 /**< STATUS_UNLOCK1 Position */
+ #define MXC_F_OTP_STATUS_UNLOCK1                       ((uint32_t)(0x1UL << MXC_F_OTP_STATUS_UNLOCK1_POS)) /**< STATUS_UNLOCK1 Mask */
+
+ #define MXC_F_OTP_STATUS_UNLOCK3_POS                   9 /**< STATUS_UNLOCK3 Position */
+ #define MXC_F_OTP_STATUS_UNLOCK3                       ((uint32_t)(0x1UL << MXC_F_OTP_STATUS_UNLOCK3_POS)) /**< STATUS_UNLOCK3 Mask */
+
+/**@} end of group OTP_STATUS_Register */
+
+/**
+ * @ingroup  otp_registers
+ * @defgroup OTP_WDATA OTP_WDATA
+ * @brief    OTP Write Data Register.
+ * @{
+ */
+ #define MXC_F_OTP_WDATA_DATA_POS                       0 /**< WDATA_DATA Position */
+ #define MXC_F_OTP_WDATA_DATA                           ((uint32_t)(0xFFFFFFFFUL << MXC_F_OTP_WDATA_DATA_POS)) /**< WDATA_DATA Mask */
+
+/**@} end of group OTP_WDATA_Register */
+
+/**
+ * @ingroup  otp_registers
+ * @defgroup OTP_ACTRL0 OTP_ACTRL0
+ * @brief    Access Control for user block.
+ * @{
+ */
+ #define MXC_F_OTP_ACTRL0_ADATA_POS                     0 /**< ACTRL0_ADATA Position */
+ #define MXC_F_OTP_ACTRL0_ADATA                         ((uint32_t)(0xFFFFFFFFUL << MXC_F_OTP_ACTRL0_ADATA_POS)) /**< ACTRL0_ADATA Mask */
+
+/**@} end of group OTP_ACTRL0_Register */
+
+/**
+ * @ingroup  otp_registers
+ * @defgroup OTP_ACTRL1 OTP_ACTRL1
+ * @brief    Access Control for sys and user block.
+ * @{
+ */
+ #define MXC_F_OTP_ACTRL1_ADATA_POS                     0 /**< ACTRL1_ADATA Position */
+ #define MXC_F_OTP_ACTRL1_ADATA                         ((uint32_t)(0xFFFFFFFFUL << MXC_F_OTP_ACTRL1_ADATA_POS)) /**< ACTRL1_ADATA Mask */
+
+/**@} end of group OTP_ACTRL1_Register */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _OTP_REGS_H_ */
---
+++ Libraries/PeriphDrivers/Source/OTP/otp_reva.c
@@ -0,0 +1,195 @@
+/******************************************************************************
+ * Copyright (C) 2022 Maxim Integrated Products, Inc., All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Maxim Integrated
+ * Products, Inc. shall not be used except as stated in the Maxim Integrated
+ * Products, Inc. Branding Policy.
+ *
+ * The mere transfer of this software does not imply any licenses
+ * of trade secrets, proprietary technology, copyrights, patents,
+ * trademarks, maskwork rights, or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * ownership rights.
+ *
+ ******************************************************************************/
+
+/* **** Includes **** */
+#include <stddef.h>
+#include "mxc_device.h"
+#include "mxc_assert.h"
+#include "mxc_errors.h"
+#include "otp.h"
+#include "otp_reva.h"
+
+/* **** Functions **** */
+int MXC_OTP_RevA_CheckUnlockMode(mxc_otp_reva_regs_t* otp)
+{
+    return 0;
+}
+
+void MXC_OTP_RevA_Unlock(mxc_otp_reva_regs_t* otp)
+{
+    otp->actrl1 = 0x3A7F5CA3;
+    otp->actrl1 = 0xA1E34F20;
+    otp->actrl1 = 0x9608B2C1;
+}
+
+void MXC_OTP_RevA_Lock(mxc_otp_reva_regs_t* otp)
+{
+    otp->actrl1 = 0;
+}
+
+void MXC_OTP_RevA_UnlockUserBlock(mxc_otp_reva_regs_t* otp)
+{
+    otp->actrl0 = 0xBEEFBA55;
+}
+
+void MXC_OTP_RevA_LockUserBlock(mxc_otp_reva_regs_t* otp)
+{
+    otp->actrl0 = 0;
+}
+
+int MXC_OTP_RevA_Write(mxc_otp_reva_regs_t* otp, uint16_t addr, uint32_t data)
+{
+    // Check address range and mode access (unlocked).
+    if (otp->status & MXC_F_OTP_REVA_STATUS_UNLOCK3) {
+        // Sys and User Block (0-2k)
+        if (addr > MXC_OTP_MEM_SIZE) {
+            return E_BAD_PARAM;
+        }
+    } else if (otp->status & MXC_F_OTP_REVA_STATUS_UNLOCK1) {
+        // User block only (1k-2k)
+        if ((addr < (MXC_OTP_MEM_SIZE / 2)) || (addr > MXC_OTP_MEM_SIZE)) {
+            return E_BAD_PARAM;
+        }
+    } else {
+        // OTP not unlocked
+        return E_BAD_STATE;
+    }
+
+    // TODO: Check if order of writing data and address matters.
+    otp->wdata = data;
+
+    MXC_SETFIELD(otp->ctrl, MXC_F_OTP_REVA_CTRL_ADDR, (addr << MXC_F_OTP_REVA_CTRL_ADDR_POS));
+
+    // Start Write operation
+    otp->ctrl |= MXC_F_OTP_REVA_CTRL_WRITE;
+
+    // Wait until operation is complete then verify it hasn't failed
+    while (otp->status & MXC_F_OTP_REVA_STATUS_BUSY)
+        ;
+
+    if (otp->status & MXC_F_OTP_REVA_STATUS_FAIL) {
+        MXC_OTP_Lock();
+        return E_BAD_STATE;
+    }
+
+    return E_NO_ERROR;
+}
+
+int MXC_OTP_RevA_MultiWrite(mxc_otp_reva_regs_t* otp, uint16_t addr, uint32_t* data, uint16_t size)
+{
+    int i;
+    int error;
+
+    // Don't write out of accessible block space.
+    if (addr > MXC_OTP_MEM_SIZE) {
+        return E_BAD_PARAM;
+    }
+
+    if ((addr + size) > MXC_OTP_MEM_SIZE) {
+        return E_BAD_PARAM;
+    }
+
+    MXC_OTP_Unlock();
+
+    for (i = 0; i < size; i++) {
+        error = MXC_OTP_Write(addr + i, data[i]);
+        if (error != E_NO_ERROR) {
+            MXC_OTP_Lock();
+            return error;
+        }
+    }
+
+    MXC_OTP_Lock();
+
+    return E_NO_ERROR;
+}
+
+int MXC_OTP_RevA_Read(mxc_otp_reva_regs_t* otp, uint16_t addr, uint32_t* data)
+{
+    // User block (1k-2k) is readable in normal mode (without unlocking).
+    // Check for address range and appropriate read access.
+    if (addr < (MXC_OTP_MEM_SIZE / 2)) {
+        if (!(otp->status & MXC_F_OTP_REVA_STATUS_UNLOCK3)) {
+            return E_BAD_STATE;
+        }
+    } else if (addr > MXC_OTP_MEM_SIZE) {
+        return E_BAD_PARAM;
+    }
+
+    MXC_SETFIELD(otp->ctrl, MXC_F_OTP_REVA_CTRL_ADDR, (addr << MXC_F_OTP_REVA_CTRL_ADDR_POS));
+
+    // Start Read Operation
+    otp->ctrl |= MXC_F_OTP_REVA_CTRL_READ;
+
+    // Wait until operation is complete then verify it hasn't failed
+    while (otp->status & MXC_F_OTP_REVA_STATUS_BUSY)
+        ;
+
+    if (otp->status & MXC_F_OTP_REVA_STATUS_FAIL) {
+        MXC_OTP_Lock();
+        return E_BAD_STATE;
+    }
+
+    *data = otp->rdata;
+
+    return E_NO_ERROR;
+}
+
+int MXC_OTP_RevA_MultiRead(mxc_otp_reva_regs_t* otp, uint16_t addr, uint32_t* data, uint16_t size)
+{
+    int i;
+    int error;
+
+    // Don't read out of accessible block space.
+    if (addr > MXC_OTP_MEM_SIZE) {
+        return E_BAD_PARAM;
+    }
+
+    if ((addr + size) > MXC_OTP_MEM_SIZE) {
+        return E_BAD_PARAM;
+    }
+
+    MXC_OTP_Unlock();
+
+    for (i = 0; i < size; i++) {
+        error = MXC_OTP_Read(addr + i, data + i);
+        if (error != E_NO_ERROR) {
+            MXC_OTP_Lock();
+            return error;
+        }
+    }
+
+    MXC_OTP_Lock();
+
+    return E_NO_ERROR;
+}
