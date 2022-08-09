d60a25fb663ddc0dccbda3c49040024a46990895
kevin-gillespie <kevin.gillespie@maximintegrated.com>
2022-08-09 18:36:56 -0500

    Iar keil (#36)
    * IAR/KEIL Project files added for MAX32655, MAX32672, MAX32675, MAX32690
    
    * Allowing for PR runs of clang-format-run.
    
    * Reformatting files.
    
    Co-authored-by: SRV-JenkinsIDE <SRV-JenkinsIDE@maximintegrated.com>
    Co-authored-by: Kevin Gillespie <Kevin.Gillespie@analog.com>

---
+++ Examples/MAX32690/ADC/ARM/RTE/_Flash_Debug/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ADC' 
+ * Target:  'Flash Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "max32690.h"
+
+/* Maxim.MAX32690 BSP::BSP:EvKit V1:1.0.0 */
+#define BOARD EvKit_V1 /* Target Board */
+
+#endif /* RTE_COMPONENTS_H */
