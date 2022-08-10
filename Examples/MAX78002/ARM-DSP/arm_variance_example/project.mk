78c6ff703276248c46405d6aaef31522f692eeb7
Jake Carter <jake.carter@analog.com>
2022-08-10 11:42:54 -0500

    [MSDK-788] Use new library toggle switches for AI87 examples (#50)
    * Update AI87 core Makefiles
    
    * Use new library toggle switches
    
    * Update project.mk
    
    * Use LITTLEFS library toggle for WearLeveling
    
    * Fix 'LTTLEFS' -> 'LITTLEFS' comment typo in core Makefile

--- Examples/MAX78002/ARM-DSP/arm_variance_example/project.mk
+++ Examples/MAX78002/ARM-DSP/arm_variance_example/project.mk
@@ -1,13 +1,14 @@
 # This file can be used for project configuration.
 # It's a sibling to the core "Makefile", which offers
 # various configuration variables that you can set here
-# if the default project configuration isn't suitable.
+# if the default setup isn't suitable.
 
 # See the comments in the "Makefile" for a detailed
 # description of the default behavior and the full list of
-# available config variables.
+# available options.
 
-# Include the CMSIS-DSP library
+# Set hard floating point
 MFLOAT_ABI = hard
-include $(CMSIS_ROOT)/5.9.0/DSP/CMSIS-DSP.mk 
+
+LIB_CMSIS_DSP = 1
 
--- Examples/MAX78002/WearLeveling/project.mk
+++ Examples/MAX78002/WearLeveling/project.mk
@@ -1,12 +1,11 @@
 # This file can be used for project configuration.
 # It's a sibling to the core "Makefile", which offers
 # various configuration variables that you can set here
-# if the default project configuration isn't suitable.
+# if the default setup isn't suitable.
 
 # See the comments in the "Makefile" for a detailed
 # description of the default behavior and the full list of
-# available config variables.
+# available options.
 
+LIB_LITTLEFS = 1
 
-
-include $(MAXIM_PATH)/Libraries/littlefs/littlefs.mk
