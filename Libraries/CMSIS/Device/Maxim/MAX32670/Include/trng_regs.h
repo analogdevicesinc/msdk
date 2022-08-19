2cd18a83eaedb2cc55b5cb7d4198e33add7a62a9
Scheiffler <Jacob.Scheiffler@analog.com>
2022-08-19 11:16:54 -0500

    Undoing clang-format for MAX32670 trng_regs.h.

--- Libraries/CMSIS/Device/Maxim/MAX32670/Include/trng_regs.h
+++ Libraries/CMSIS/Device/Maxim/MAX32670/Include/trng_regs.h
@@ -46,12 +46,12 @@
 extern "C" {
 #endif
 
-#if defined(__ICCARM__)
-#pragma system_include
+#if defined (__ICCARM__)
+  #pragma system_include
 #endif
 
-#if defined(__CC_ARM)
-#pragma anon_unions
+#if defined (__CC_ARM)
+  #pragma anon_unions
 #endif
 /// @cond
 /*
@@ -61,13 +61,13 @@ extern "C" {
 #define __IO volatile
 #endif
 #ifndef __I
-#define __I volatile const
+#define __I  volatile const
 #endif
 #ifndef __O
-#define __O volatile
+#define __O  volatile
 #endif
 #ifndef __R
-#define __R volatile const
+#define __R  volatile const
 #endif
 /// @endcond
 
@@ -85,9 +85,9 @@ extern "C" {
  * Structure type to access the TRNG Registers.
  */
 typedef struct {
-    __IO uint32_t ctrl;   /**< <tt>\b 0x00:</tt> TRNG CTRL Register */
-    __IO uint32_t status; /**< <tt>\b 0x04:</tt> TRNG STATUS Register */
-    __I uint32_t data;    /**< <tt>\b 0x08:</tt> TRNG DATA Register */
+    __IO uint32_t ctrl;                 /**< <tt>\b 0x00:</tt> TRNG CTRL Register */
+    __IO uint32_t status;               /**< <tt>\b 0x04:</tt> TRNG STATUS Register */
+    __I  uint32_t data;                 /**< <tt>\b 0x08:</tt> TRNG DATA Register */
 } mxc_trng_regs_t;
 
 /* Register offsets for module TRNG */
@@ -97,13 +97,10 @@ typedef struct {
  * @brief      TRNG Peripheral Register Offsets from the TRNG Base Peripheral Address.
  * @{
  */
-#define MXC_R_TRNG_CTRL \
-    ((uint32_t)0x00000000UL) /**< Offset from TRNG Base Address: <tt> 0x0000</tt> */
-#define MXC_R_TRNG_STATUS \
-    ((uint32_t)0x00000004UL) /**< Offset from TRNG Base Address: <tt> 0x0004</tt> */
-#define MXC_R_TRNG_DATA \
-    ((uint32_t)0x00000008UL) /**< Offset from TRNG Base Address: <tt> 0x0008</tt> */
-                             /**@} end of group trng_registers */
+ #define MXC_R_TRNG_CTRL                    ((uint32_t)0x00000000UL) /**< Offset from TRNG Base Address: <tt> 0x0000</tt> */ 
+ #define MXC_R_TRNG_STATUS                  ((uint32_t)0x00000004UL) /**< Offset from TRNG Base Address: <tt> 0x0004</tt> */ 
+ #define MXC_R_TRNG_DATA                    ((uint32_t)0x00000008UL) /**< Offset from TRNG Base Address: <tt> 0x0008</tt> */ 
+/**@} end of group trng_registers */
 
 /**
  * @ingroup  trng_registers
@@ -111,24 +108,20 @@ typedef struct {
  * @brief    TRNG Control Register.
  * @{
  */
-#define MXC_F_TRNG_CTRL_ODHT_POS 0 /**< CTRL_ODHT Position */
-#define MXC_F_TRNG_CTRL_ODHT     ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_ODHT_POS)) /**< CTRL_ODHT Mask */
+ #define MXC_F_TRNG_CTRL_ODHT_POS                       0 /**< CTRL_ODHT Position */
+ #define MXC_F_TRNG_CTRL_ODHT                           ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_ODHT_POS)) /**< CTRL_ODHT Mask */
 
-#define MXC_F_TRNG_CTRL_RND_IE_POS 1 /**< CTRL_RND_IE Position */
-#define MXC_F_TRNG_CTRL_RND_IE \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_RND_IE_POS)) /**< CTRL_RND_IE Mask */
+ #define MXC_F_TRNG_CTRL_RND_IE_POS                     1 /**< CTRL_RND_IE Position */
+ #define MXC_F_TRNG_CTRL_RND_IE                         ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_RND_IE_POS)) /**< CTRL_RND_IE Mask */
 
-#define MXC_F_TRNG_CTRL_HEALTH_EN_POS 2 /**< CTRL_HEALTH_EN Position */
-#define MXC_F_TRNG_CTRL_HEALTH_EN \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_HEALTH_EN_POS)) /**< CTRL_HEALTH_EN Mask */
+ #define MXC_F_TRNG_CTRL_HEALTH_EN_POS                  2 /**< CTRL_HEALTH_EN Position */
+ #define MXC_F_TRNG_CTRL_HEALTH_EN                      ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_HEALTH_EN_POS)) /**< CTRL_HEALTH_EN Mask */
 
-#define MXC_F_TRNG_CTRL_KEYGEN_POS 3 /**< CTRL_KEYGEN Position */
-#define MXC_F_TRNG_CTRL_KEYGEN \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_KEYGEN_POS)) /**< CTRL_KEYGEN Mask */
+ #define MXC_F_TRNG_CTRL_KEYGEN_POS                     3 /**< CTRL_KEYGEN Position */
+ #define MXC_F_TRNG_CTRL_KEYGEN                         ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_KEYGEN_POS)) /**< CTRL_KEYGEN Mask */
 
-#define MXC_F_TRNG_CTRL_KEYWIPE_POS 15 /**< CTRL_KEYWIPE Position */
-#define MXC_F_TRNG_CTRL_KEYWIPE \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_KEYWIPE_POS)) /**< CTRL_KEYWIPE Mask */
+ #define MXC_F_TRNG_CTRL_KEYWIPE_POS                    15 /**< CTRL_KEYWIPE Position */
+ #define MXC_F_TRNG_CTRL_KEYWIPE                        ((uint32_t)(0x1UL << MXC_F_TRNG_CTRL_KEYWIPE_POS)) /**< CTRL_KEYWIPE Mask */
 
 /**@} end of group TRNG_CTRL_Register */
 
@@ -139,28 +132,23 @@ typedef struct {
  *           disabled, read returns 0x0000 0000.
  * @{
  */
-#define MXC_F_TRNG_STATUS_RDY_POS 0 /**< STATUS_RDY Position */
-#define MXC_F_TRNG_STATUS_RDY \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_RDY_POS)) /**< STATUS_RDY Mask */
+ #define MXC_F_TRNG_STATUS_RDY_POS                      0 /**< STATUS_RDY Position */
+ #define MXC_F_TRNG_STATUS_RDY                          ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_RDY_POS)) /**< STATUS_RDY Mask */
 
-#define MXC_F_TRNG_STATUS_ODHT_POS 1 /**< STATUS_ODHT Position */
-#define MXC_F_TRNG_STATUS_ODHT \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_ODHT_POS)) /**< STATUS_ODHT Mask */
+ #define MXC_F_TRNG_STATUS_ODHT_POS                     1 /**< STATUS_ODHT Position */
+ #define MXC_F_TRNG_STATUS_ODHT                         ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_ODHT_POS)) /**< STATUS_ODHT Mask */
 
-#define MXC_F_TRNG_STATUS_HT_POS 2 /**< STATUS_HT Position */
-#define MXC_F_TRNG_STATUS_HT     ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_HT_POS)) /**< STATUS_HT Mask */
+ #define MXC_F_TRNG_STATUS_HT_POS                       2 /**< STATUS_HT Position */
+ #define MXC_F_TRNG_STATUS_HT                           ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_HT_POS)) /**< STATUS_HT Mask */
 
-#define MXC_F_TRNG_STATUS_SRCFAIL_POS 3 /**< STATUS_SRCFAIL Position */
-#define MXC_F_TRNG_STATUS_SRCFAIL \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_SRCFAIL_POS)) /**< STATUS_SRCFAIL Mask */
+ #define MXC_F_TRNG_STATUS_SRCFAIL_POS                  3 /**< STATUS_SRCFAIL Position */
+ #define MXC_F_TRNG_STATUS_SRCFAIL                      ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_SRCFAIL_POS)) /**< STATUS_SRCFAIL Mask */
 
-#define MXC_F_TRNG_STATUS_AESKGD_POS 4 /**< STATUS_AESKGD Position */
-#define MXC_F_TRNG_STATUS_AESKGD \
-    ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_AESKGD_POS)) /**< STATUS_AESKGD Mask */
+ #define MXC_F_TRNG_STATUS_AESKGD_POS                   4 /**< STATUS_AESKGD Position */
+ #define MXC_F_TRNG_STATUS_AESKGD                       ((uint32_t)(0x1UL << MXC_F_TRNG_STATUS_AESKGD_POS)) /**< STATUS_AESKGD Mask */
 
-#define MXC_F_TRNG_STATUS_LD_CNT_POS 24 /**< STATUS_LD_CNT Position */
-#define MXC_F_TRNG_STATUS_LD_CNT \
-    ((uint32_t)(0xFFUL << MXC_F_TRNG_STATUS_LD_CNT_POS)) /**< STATUS_LD_CNT Mask */
+ #define MXC_F_TRNG_STATUS_LD_CNT_POS                   24 /**< STATUS_LD_CNT Position */
+ #define MXC_F_TRNG_STATUS_LD_CNT                       ((uint32_t)(0xFFUL << MXC_F_TRNG_STATUS_LD_CNT_POS)) /**< STATUS_LD_CNT Mask */
 
 /**@} end of group TRNG_STATUS_Register */
 
@@ -171,9 +159,8 @@ typedef struct {
  *           disabled, read returns 0x0000 0000.
  * @{
  */
-#define MXC_F_TRNG_DATA_DATA_POS 0 /**< DATA_DATA Position */
-#define MXC_F_TRNG_DATA_DATA \
-    ((uint32_t)(0xFFFFFFFFUL << MXC_F_TRNG_DATA_DATA_POS)) /**< DATA_DATA Mask */
+ #define MXC_F_TRNG_DATA_DATA_POS                       0 /**< DATA_DATA Position */
+ #define MXC_F_TRNG_DATA_DATA                           ((uint32_t)(0xFFFFFFFFUL << MXC_F_TRNG_DATA_DATA_POS)) /**< DATA_DATA Mask */
 
 /**@} end of group TRNG_DATA_Register */
 
