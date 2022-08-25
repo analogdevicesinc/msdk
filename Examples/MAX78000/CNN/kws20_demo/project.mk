caeae3696003bccf13b4e3e78f3b4fac516438b2
Jake Carter <jake.carter@analog.com>
2022-08-25 17:38:40 -0500

    Remove SRCS+= in project.mk (new system auto searches)

--- Examples/MAX78000/CNN/kws20_demo/project.mk
+++ Examples/MAX78000/CNN/kws20_demo/project.mk
@@ -12,7 +12,6 @@ ifeq "$(BOARD)" "EvKit_V1"
 PROJ_CFLAGS+=-DENABLE_TFT
 IPATH += TFT/evkit/
 VPATH += TFT/evkit/
-SRCS += all_imgs.c
 endif
 
 # Place build files specific to FTHR_RevA here.
@@ -21,9 +20,6 @@ ifeq "$(BOARD)" "FTHR_RevA"
 #PROJ_CFLAGS+=-DENABLE_TFT
 IPATH += TFT/fthr
 VPATH += TFT/fthr
-SRCS += img_1_rgb565.c
-SRCS += logo_rgb565.c
-SRCS += SansSerif16x16.c
 endif
 
 
--- Examples/MAX78000/CNN/snake_game_demo/project.mk
+++ Examples/MAX78000/CNN/snake_game_demo/project.mk
@@ -11,16 +11,12 @@
 ifeq "$(BOARD)" "EvKit_V1"
 IPATH += TFT/evkit/
 VPATH += TFT/evkit/
-SRCS += all_imgs.c
 endif
 
 # Place build files specific to FTHR_RevA here.
 ifeq "$(BOARD)" "FTHR_RevA"
 IPATH += TFT/fthr
 VPATH += TFT/fthr
-SRCS += img_1_rgb565.c
-SRCS += logo_rgb565.c
-SRCS += SansSerif16x16.c
 endif
 
 
