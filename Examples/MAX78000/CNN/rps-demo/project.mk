1cd574df4c6ef669b9688cc531394837cdde9b2f
Victor Loginov <Victor.Loginov@maximintegrated.com>
2022-08-24 17:39:29 -0700

    Added TFT_ENABLE flag to project.mk

--- Examples/MAX78000/CNN/rps-demo/project.mk
+++ Examples/MAX78000/CNN/rps-demo/project.mk
@@ -9,6 +9,7 @@
 
 # Place build files specific to EvKit_V1 here.
 ifeq "$(BOARD)" "EvKit_V1"
+PROJ_CFLAGS+=-DTFT_ENABLE
 IPATH += TFT/evkit/
 VPATH += TFT/evkit/
 SRCS += all_imgs.c
@@ -16,6 +17,8 @@ endif
 
 # Place build files specific to FTHR_RevA here.
 ifeq "$(BOARD)" "FTHR_RevA"
+# Only Enable if 2.4" TFT is connected to Feather
+#PROJ_CFLAGS+=-DTFT_ENABLE
 IPATH += TFT/fthr
 VPATH += TFT/fthr
 SRCS += img_1_rgb565.c
