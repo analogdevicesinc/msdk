3db32995541986c9aae8406d033adf887568135e
SRV-JenkinsIDE <SRV-JenkinsIDE@maximintegrated.com>
2022-09-02 17:35:20 -0500

    updating more local changes as bugs are fixed during IAR/KEIL CI

---
+++ Examples/MAX32672/RTC/RTE/RTE_Components.h
@@ -0,0 +1,19 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: RTC
+ * RTE configuration: RTC.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "max32672.h"
+
+/* Maxim.MAX32672 BSP::BSP.EvKit V1 */
+#define BOARD EvKit_V1 /* Target Board */
+
+#endif /* RTE_COMPONENTS_H */
