c76c34a9a800d4070c09df81fa103572f4a66ff5
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2022-09-07 11:25:04 -0500

    ES17: Updated copyright notices for Board files.

--- Libraries/Boards/MAX32520/EvKit_V1/Source/rom_stub.c
+++ Libraries/Boards/MAX32520/EvKit_V1/Source/rom_stub.c
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright (C) 2016 Maxim Integrated Products, Inc., All Rights Reserved.
+/******************************************************************************
+ * Copyright (C) 2022 Maxim Integrated Products, Inc., All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -28,9 +28,6 @@
  * trademarks, maskwork rights, or any other form of intellectual
  * property whatsoever. Maxim Integrated Products, Inc. retains all
  * ownership rights.
- *
- * $Date: 2017-05-15 09:31:32 -0500 (Mon, 15 May 2017) $
- * $Revision: 27976 $
  *
  ******************************************************************************/
 
