aa77f0d02e6fc0f7fb37db027aa787e3eac558a5
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2022-09-12 09:45:33 -0500

    ME55: clang formatting on max32572.h.

--- Libraries/CMSIS/Device/Maxim/MAX32572/Include/max32572.h
+++ Libraries/CMSIS/Device/Maxim/MAX32572/Include/max32572.h
@@ -196,66 +196,66 @@ typedef enum {
     RSV111_IRQn,   /* 0x7F  0x01FC  127: Reserved */
 #else              // __riscv
     HardFault_IRQn = 3, /* 0x03,3 HardFault */
-    RSV4_IRQn = 4, /* 0x04,4 Reserved */
-    RSV5_IRQn,     /* 0x05,5 Reserved */
-    MSRADC_IRQn,   /* 0x06,6 Magstripe DSP */
-    RSV7_IRQn,     /* 0x07,7 Reserved */
-    RSV8_IRQn,     /* 0x08,8 Reserved */
-    RSV9_IRQn,     /* 0x09,9 Reserved */
-    RSV10_IRQn,    /* 0x0A,10 Reserved */
-    RSV11_IRQn,    /* 0x0B,11 Reserved */
-    RSV12_IRQn,    /* 0x0C,12 Reserved */
-    TMR5_IRQn,     /* 0x0D,13 Timer 5 */
-    RSV14_IRQn,    /* 0x0E,14 Reserved */
-    RSV15_IRQn,    /* 0x0F,15 Reserved */
-    CM4_IRQn,      /* 0x10,16 CM4 */
-    RSV17_IRQn,    /* 0x11,17 Reserved */
-    RSV18_IRQn,    /* 0x12,18 Reserved */
-    RSV19_IRQn,    /* 0x13,19 Reserved */
-    RSV20_IRQn,    /* 0x14,20 Reserved */
-    RSV21_IRQn,    /* 0x15,21 Reserved */
-    RSV22_IRQn,    /* 0x16,22 Reserved */
-    RSV23_IRQn,    /* 0x17,23 Reserved */
-    RSV24_IRQn,    /* 0x18,24 Reserved */
-    GPIO0_IRQn,    /* 0x19,25 GPIO0 */
-    GPIO1_IRQn,    /* 0x1A,26 GPIO1 */
-    RSV27_IRQn,    /* 0x1B,27 Reserved */
-    RSV28_IRQn,    /* 0x1C,28 Reserved */
-    RSV29_IRQn,    /* 0x1D,29 Reserved */
-    RSV30_IRQn,    /* 0x1E,30 Reserved */
-    RSV31_IRQn,    /* 0x1F,31 Reserved */
-    RSV32_IRQn,    /* 0x20,32 Reserved */
-    RSV33_IRQn,    /* 0x21,33 Reserved */
-    RSV34_IRQn,    /* 0x22,34 Reserved */
-    RSV35_IRQn,    /* 0x23,35 Reserved */
-    RSV36_IRQn,    /* 0x24,36 Reserved */
-    RSV37_IRQn,    /* 0x25,37 Reserved */
-    RSV38_IRQn,    /* 0x26,38 Reserved */
-    RSV39_IRQn,    /* 0x27,39 Reserved */
-    RSV40_IRQn,    /* 0x28,40 Reserved */
-    RSV41_IRQn,    /* 0x29,41 Reserved */
-    RSV42_IRQn,    /* 0x2A,42 Reserved */
-    RSV43_IRQn,    /* 0x2B,43 Reserved */
-    RSV44_IRQn,    /* 0x2C,44 Reserved */
-    RSV45_IRQn,    /* 0x2D,45 Reserved */
-    RSV46_IRQn,    /* 0x2E,46 Reserved */
-    RSV47_IRQn,    /* 0x2F,47 Reserved */
-    RSV48_IRQn,    /* 0x30,48 Reserved */
-    RSV49_IRQn,    /* 0x31,49 Reserved */
-    RSV50_IRQn,    /* 0x32,50 Reserved */
-    RSV51_IRQn,    /* 0x33,51 Reserved */
-    RSV52_IRQn,    /* 0x34,52 Reserved */
-    RSV53_IRQn,    /* 0x35,53 Reserved */
-    RSV54_IRQn,    /* 0x36,54 Reserved */
-    RSV55_IRQn,    /* 0x37,55 Reserved */
-    RSV56_IRQn,    /* 0x38,56 Reserved */
-    RSV57_IRQn,    /* 0x39,57 Reserved */
-    RSV58_IRQn,    /* 0x3A,58 Reserved */
-    RSV59_IRQn,    /* 0x3B,59 Reserved */
-    RSV60_IRQn,    /* 0x3C,60 Reserved */
-    RSV61_IRQn,    /* 0x3D,61 Reserved */
-    RSV62_IRQn,    /* 0x3E,62 Reserved */
-    RSV63_IRQn,    /* 0x3F,63 Reserved */
+    RSV4_IRQn      = 4, /* 0x04,4 Reserved */
+    RSV5_IRQn,          /* 0x05,5 Reserved */
+    MSRADC_IRQn,        /* 0x06,6 Magstripe DSP */
+    RSV7_IRQn,          /* 0x07,7 Reserved */
+    RSV8_IRQn,          /* 0x08,8 Reserved */
+    RSV9_IRQn,          /* 0x09,9 Reserved */
+    RSV10_IRQn,         /* 0x0A,10 Reserved */
+    RSV11_IRQn,         /* 0x0B,11 Reserved */
+    RSV12_IRQn,         /* 0x0C,12 Reserved */
+    TMR5_IRQn,          /* 0x0D,13 Timer 5 */
+    RSV14_IRQn,         /* 0x0E,14 Reserved */
+    RSV15_IRQn,         /* 0x0F,15 Reserved */
+    CM4_IRQn,           /* 0x10,16 CM4 */
+    RSV17_IRQn,         /* 0x11,17 Reserved */
+    RSV18_IRQn,         /* 0x12,18 Reserved */
+    RSV19_IRQn,         /* 0x13,19 Reserved */
+    RSV20_IRQn,         /* 0x14,20 Reserved */
+    RSV21_IRQn,         /* 0x15,21 Reserved */
+    RSV22_IRQn,         /* 0x16,22 Reserved */
+    RSV23_IRQn,         /* 0x17,23 Reserved */
+    RSV24_IRQn,         /* 0x18,24 Reserved */
+    GPIO0_IRQn,         /* 0x19,25 GPIO0 */
+    GPIO1_IRQn,         /* 0x1A,26 GPIO1 */
+    RSV27_IRQn,         /* 0x1B,27 Reserved */
+    RSV28_IRQn,         /* 0x1C,28 Reserved */
+    RSV29_IRQn,         /* 0x1D,29 Reserved */
+    RSV30_IRQn,         /* 0x1E,30 Reserved */
+    RSV31_IRQn,         /* 0x1F,31 Reserved */
+    RSV32_IRQn,         /* 0x20,32 Reserved */
+    RSV33_IRQn,         /* 0x21,33 Reserved */
+    RSV34_IRQn,         /* 0x22,34 Reserved */
+    RSV35_IRQn,         /* 0x23,35 Reserved */
+    RSV36_IRQn,         /* 0x24,36 Reserved */
+    RSV37_IRQn,         /* 0x25,37 Reserved */
+    RSV38_IRQn,         /* 0x26,38 Reserved */
+    RSV39_IRQn,         /* 0x27,39 Reserved */
+    RSV40_IRQn,         /* 0x28,40 Reserved */
+    RSV41_IRQn,         /* 0x29,41 Reserved */
+    RSV42_IRQn,         /* 0x2A,42 Reserved */
+    RSV43_IRQn,         /* 0x2B,43 Reserved */
+    RSV44_IRQn,         /* 0x2C,44 Reserved */
+    RSV45_IRQn,         /* 0x2D,45 Reserved */
+    RSV46_IRQn,         /* 0x2E,46 Reserved */
+    RSV47_IRQn,         /* 0x2F,47 Reserved */
+    RSV48_IRQn,         /* 0x30,48 Reserved */
+    RSV49_IRQn,         /* 0x31,49 Reserved */
+    RSV50_IRQn,         /* 0x32,50 Reserved */
+    RSV51_IRQn,         /* 0x33,51 Reserved */
+    RSV52_IRQn,         /* 0x34,52 Reserved */
+    RSV53_IRQn,         /* 0x35,53 Reserved */
+    RSV54_IRQn,         /* 0x36,54 Reserved */
+    RSV55_IRQn,         /* 0x37,55 Reserved */
+    RSV56_IRQn,         /* 0x38,56 Reserved */
+    RSV57_IRQn,         /* 0x39,57 Reserved */
+    RSV58_IRQn,         /* 0x3A,58 Reserved */
+    RSV59_IRQn,         /* 0x3B,59 Reserved */
+    RSV60_IRQn,         /* 0x3C,60 Reserved */
+    RSV61_IRQn,         /* 0x3D,61 Reserved */
+    RSV62_IRQn,         /* 0x3E,62 Reserved */
+    RSV63_IRQn,         /* 0x3F,63 Reserved */
 #endif             // __riscv
     MXC_IRQ_EXT_COUNT
 } IRQn_Type;
@@ -412,37 +412,42 @@ typedef enum {
 #ifndef __riscv
 #define MXC_TMR_GET_IRQ(i)             \
     (IRQn_Type)((i) == 0 ? TMR0_IRQn : \
-                           (i) == 1 ?  \
-                           TMR1_IRQn : \
-                           (i) == 2 ?  \
-                           TMR2_IRQn : \
-                           (i) == 3 ? TMR3_IRQn : (i) == 4 ? TMR4_IRQn : (i) == 5 ? TMR5_IRQn : 0)
+                (i) == 1 ? TMR1_IRQn : \
+                (i) == 2 ? TMR2_IRQn : \
+                (i) == 3 ? TMR3_IRQn : \
+                (i) == 4 ? TMR4_IRQn : \
+                (i) == 5 ? TMR5_IRQn : \
+                           0)
 #else
 #define MXC_TMR_GET_IRQ(i) ((i) == 5 ? TMR5_IRQn : 0)
 #endif
 
-#define MXC_TMR_GET_BASE(i)                               \
-    ((i) == 0 ?                                           \
-         MXC_BASE_TMR0 :                                  \
-         (i) == 1 ? MXC_BASE_TMR1 :                       \
-                    (i) == 2 ? MXC_BASE_TMR2 :            \
-                               (i) == 3 ? MXC_BASE_TMR3 : \
-                                          (i) == 4 ? MXC_BASE_TMR4 : (i) == 5 ? MXC_BASE_TMR5 : 0)
+#define MXC_TMR_GET_BASE(i)     \
+    ((i) == 0 ? MXC_BASE_TMR0 : \
+     (i) == 1 ? MXC_BASE_TMR1 : \
+     (i) == 2 ? MXC_BASE_TMR2 : \
+     (i) == 3 ? MXC_BASE_TMR3 : \
+     (i) == 4 ? MXC_BASE_TMR4 : \
+     (i) == 5 ? MXC_BASE_TMR5 : \
+                0)
 
-#define MXC_TMR_GET_TMR(i)            \
-    ((i) == 0 ? MXC_TMR0 :            \
-                (i) == 1 ?            \
-                MXC_TMR1 :            \
-                (i) == 2 ? MXC_TMR2 : \
-                           (i) == 3 ? MXC_TMR3 : (i) == 4 ? MXC_TMR4 : (i) == 5 ? MXC_TMR5 : 0)
+#define MXC_TMR_GET_TMR(i) \
+    ((i) == 0 ? MXC_TMR0 : \
+     (i) == 1 ? MXC_TMR1 : \
+     (i) == 2 ? MXC_TMR2 : \
+     (i) == 3 ? MXC_TMR3 : \
+     (i) == 4 ? MXC_TMR4 : \
+     (i) == 5 ? MXC_TMR5 : \
+                0)
 
-#define MXC_TMR_GET_IDX(p)     \
-    ((p) == MXC_TMR0 ?         \
-         0 :                   \
-         (p) == MXC_TMR1 ?     \
-         1 :                   \
-         (p) == MXC_TMR2 ? 2 : \
-                           (p) == MXC_TMR3 ? 3 : (p) == MXC_TMR4 ? 4 : (p) == MXC_TMR5 ? 5 : -1)
+#define MXC_TMR_GET_IDX(p) \
+    ((p) == MXC_TMR0 ? 0 : \
+     (p) == MXC_TMR1 ? 1 : \
+     (p) == MXC_TMR2 ? 2 : \
+     (p) == MXC_TMR3 ? 3 : \
+     (p) == MXC_TMR4 ? 4 : \
+     (p) == MXC_TMR5 ? 5 : \
+                       -1)
 
 /******************************************************************************/
 /*                                                           High Speed Timer */
@@ -553,37 +558,36 @@ typedef enum {
 
 #define MXC_PT_GET_BASE(i)     \
     ((i) == 0 ? MXC_BASE_PT0 : \
-                (i) == 1 ?     \
-                MXC_BASE_PT1 : \
-                (i) == 2 ?     \
-                MXC_BASE_PT2 : \
-                (i) == 3 ?     \
-                MXC_BASE_PT3 : \
-                (i) == 4 ?     \
-                MXC_BASE_PT4 : \
-                (i) == 5 ? MXC_BASE_PT5 : (i) == 6 ? MXC_BASE_PT6 : (i) == 7 ? MXC_BASE_PT7 : 0)
+     (i) == 1 ? MXC_BASE_PT1 : \
+     (i) == 2 ? MXC_BASE_PT2 : \
+     (i) == 3 ? MXC_BASE_PT3 : \
+     (i) == 4 ? MXC_BASE_PT4 : \
+     (i) == 5 ? MXC_BASE_PT5 : \
+     (i) == 6 ? MXC_BASE_PT6 : \
+     (i) == 7 ? MXC_BASE_PT7 : \
+                0)
 
-#define MXC_PT_GET_PT(i) \
-    ((i) == 0 ?          \
-         MXC_PT0 :       \
-         (i) == 1 ?      \
-         MXC_PT1 :       \
-         (i) == 2 ?      \
-         MXC_PT2 :       \
-         (i) == 3 ?      \
-         MXC_PT3 :       \
-         (i) == 4 ? MXC_PT4 : (i) == 5 ? MXC_PT5 : (i) == 6 ? MXC_PT6 : (i) == 7 ? MXC_PT7 : 0)
+#define MXC_PT_GET_PT(i)  \
+    ((i) == 0 ? MXC_PT0 : \
+     (i) == 1 ? MXC_PT1 : \
+     (i) == 2 ? MXC_PT2 : \
+     (i) == 3 ? MXC_PT3 : \
+     (i) == 4 ? MXC_PT4 : \
+     (i) == 5 ? MXC_PT5 : \
+     (i) == 6 ? MXC_PT6 : \
+     (i) == 7 ? MXC_PT7 : \
+                0)
 
 #define MXC_PT_GET_IDX(p) \
-    ((p) == MXC_PT0 ?     \
-         0 :              \
-         (p) == MXC_PT1 ? \
-         1 :              \
-         (p) == MXC_PT2 ? \
-         2 :              \
-         (p) == MXC_PT3 ? \
-         3 :              \
-         (p) == MXC_PT4 ? 4 : (p) == MXC_PT5 ? 5 : (p) == MXC_PT6 ? 6 : (p) == MXC_PT7 ? 7 : -1)
+    ((p) == MXC_PT0 ? 0 : \
+     (p) == MXC_PT1 ? 1 : \
+     (p) == MXC_PT2 ? 2 : \
+     (p) == MXC_PT3 ? 3 : \
+     (p) == MXC_PT4 ? 4 : \
+     (p) == MXC_PT5 ? 5 : \
+     (p) == MXC_PT6 ? 6 : \
+     (p) == MXC_PT7 ? 7 : \
+                      -1)
 
 /******************************************************************************/
 /*                                                                 Semaphores */
@@ -610,16 +614,20 @@ typedef enum {
 #define MXC_UART3      ((mxc_uart_regs_t*)MXC_BASE_UART3)
 
 #ifndef __riscv
-#define MXC_UART_GET_IRQ(i)      \
-    (IRQn_Type)((i) == 0 ?       \
-                    UART0_IRQn : \
-                    (i) == 1 ? UART1_IRQn : (i) == 2 ? UART2_IRQn : (i) == 3 ? UART3_IRQn : 0)
+#define MXC_UART_GET_IRQ(i)             \
+    (IRQn_Type)((i) == 0 ? UART0_IRQn : \
+                (i) == 1 ? UART1_IRQn : \
+                (i) == 2 ? UART2_IRQn : \
+                (i) == 3 ? UART3_IRQn : \
+                           0)
 #endif
 
-#define MXC_UART_GET_BASE(i) \
-    ((i) == 0 ?              \
-         MXC_BASE_UART0 :    \
-         (i) == 1 ? MXC_BASE_UART1 : (i) == 2 ? MXC_BASE_UART2 : (i) == 3 ? MXC_BASE_UART3 : 0)
+#define MXC_UART_GET_BASE(i)     \
+    ((i) == 0 ? MXC_BASE_UART0 : \
+     (i) == 1 ? MXC_BASE_UART1 : \
+     (i) == 2 ? MXC_BASE_UART2 : \
+     (i) == 3 ? MXC_BASE_UART3 : \
+                0)
 
 #define MXC_UART_GET_UART(i) \
     ((i) == 0 ? MXC_UART0 : (i) == 1 ? MXC_UART1 : (i) == 2 ? MXC_UART2 : (i) == 3 ? MXC_UART3 : 0)
