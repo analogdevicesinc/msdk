c4e6574e6f416c0591eb86e4ebad0866a91cdf5a
Jake Carter <Jake.Carter@analog.com>
2022-09-14 13:31:20 -0500

    Update all MAX32520 (ES17) Makefiles to project.mk system
    Enable FCL and add compiler flags for ECDSA example
    
    Enable FreeRTOS lib for Flash_CLI and FreeRTOSDemo
    
    Update SCPA_OTP_Dump readme

---
+++ Examples/MAX32520/ECDSA/project.mk
@@ -0,0 +1,16 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+#BOARD=FTHR_RevA
+# ^ For example, you can uncomment this line to make the 
+# project build for the "FTHR_RevA" board.
+
+# **********************************************************
+
+PROJ_CFLAGS += -DASYMMETRIC
+PROJ_CFLAGS += -DWORD32
+LIB_FCL=1
