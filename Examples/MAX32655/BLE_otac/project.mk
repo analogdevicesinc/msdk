18825c915d0c498805a0670aae4f49d8f47d7646
Jake Carter <Jake.Carter@analog.com>
2022-09-21 20:14:45 -0500

    Fix otac/otas firmware loader
    The old firmware loader had some issues with Makefile recursion that
    were very difficult to resolve.  Applying the same strategy as the RISCV
    loader of building the external application's firmware locally in the
    project, then linking it "in-place" resolved the issue.
    
    The external firmware app will now be built in build/buildfw

--- Examples/MAX32655/BLE_otac/project.mk
+++ Examples/MAX32655/BLE_otac/project.mk
@@ -13,26 +13,44 @@ LIB_CORDIO = 1
 # Optimize for size
 MXC_OPTIMIZE_CFLAGS = -Os
 
+# CORDIO library options
+INIT_PERIPHERAL = 0
+INIT_BROADCASTER = 0
+INIT_CENTRAL = 1
+INIT_OBSERVER = 1
+
+# **********************************************************
+# Firmware builder
+# The section below will compile the application specified
+# by FW_UPDATE_DIR.  Then, it will combine the application into
+# the same single output binary alongside this one. 
+
 # Build directory for image that peer will be updated with
 FW_UPDATE_DIR=../BLE_otas
 
-# Firmware update files, do not rename
-FW_UPDATE_BIN=fw_update.bin
-FW_UPDATE_OBJ=build/fw_update.o
+BUILD_DIR := $(abspath ./build)
+FW_BUILD_DIR := $(BUILD_DIR)/buildfw
 
+# Firmware update files, do not rename
+FW_UPDATE_BIN=$(FW_BUILD_DIR)/fw_update.bin
+FW_UPDATE_OBJ=$(FW_BUILD_DIR)/fw_update.o
+
+# This is the 'magic' line that gets the linker to combine in
+# the external application's object file.
 PROJ_OBJS = ${FW_UPDATE_OBJ}
 
 # Target for creating the fw_update bin file
+.PHONY: fw_bin
+fw_bin: $(FW_UPDATE_BIN)
+
 ${FW_UPDATE_BIN}:
-	$(MAKE) -C ${FW_UPDATE_DIR}
-	$(MAKE) -C ${FW_UPDATE_DIR} build/${PROJECT}.bin
-	@cp ${FW_UPDATE_DIR}/build/${PROJECT}.bin ${FW_UPDATE_BIN}
+	$(MAKE) -C ${FW_UPDATE_DIR} BUILD_DIR=$(FW_BUILD_DIR) PROJECT=fw_update
+	$(MAKE) -C $(FW_UPDATE_DIR) BUILD_DIR=$(FW_BUILD_DIR) $(FW_UPDATE_BIN)
+
+# Target for creating the firmware update obj file
+.PHONY: fw_obj
+fw_obj: $(FW_UPDATE_OBJ)
 
 ${FW_UPDATE_OBJ}: fw_update.S ${FW_UPDATE_BIN}
-	@mkdir -p build
-	@${CC} ${AFLAGS} -o ${@} -c fw_update.S
+	${CC} ${AFLAGS} -o ${@} -c fw_update.S
 
-clean.ota:
-	@rm -f ${FW_UPDATE_BIN}
-
-clean: clean.ota
