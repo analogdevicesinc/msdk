73f6840734c643f7ee5e272e3011f6e3bcd0f081
Jake Carter <Jake.Carter@analog.com>
2022-09-22 14:33:11 -0500

    Upgrade NFC_PCD_DTE_EMV_Loopback Makefiles

---
+++ Examples/MAX32570/NFC_PCD_DTE_EMV_Loopback/project.mk
@@ -0,0 +1,24 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+# Override default optimization level
+MXC_OPTIMIZE_CFLAGS=-O3
+
+# Enable NFC library
+LIB_NFC = 1
+
+# Add project's include and source paths
+VPATH += src/nfc
+VPATH += src/nfc/contactless_l1_app
+VPATH += src/nfc/emv_l1_stack
+VPATH += resources
+IPATH += include/nfc
+IPATH += include/nfc/contactless_l1_app
+IPATH += include/nfc/emv_l1_stack
+IPATH += resources
