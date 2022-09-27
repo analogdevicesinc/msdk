4d1f5fba316e0d768a87f70e28b1be49840cb41e
Jake Carter <Jake.Carter@analog.com>
2022-09-27 15:23:31 -0500

    Add project.mk files for MAX32665

---
+++ Examples/MAX32665/BLE_datc/project.mk
@@ -0,0 +1,21 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+# Enable CORDIO library
+LIB_CORDIO = 1
+
+# Optimize for size
+MXC_OPTIMIZE_CFLAGS = -Os
+
+# Disable peripheral and broadcaster.  They're
+# not needed for this client app.
+INIT_PERIPHERAL = 0
+INIT_BROADCASTER = 0
+INIT_CENTRAL = 1
+INIT_OBSERVER = 1
