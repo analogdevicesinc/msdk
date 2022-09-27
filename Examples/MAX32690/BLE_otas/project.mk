d3d4c99e5b52e849d1b2052503599e89c41bbc3f
Jake Carter <Jake.Carter@analog.com>
2022-09-27 16:25:32 -0500

    Add project.mk files

---
+++ Examples/MAX32690/BLE_otas/project.mk
@@ -0,0 +1,25 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+# Enable CORDIO library
+LIB_CORDIO = 1
+
+# Cordio library options
+STANDBY_ENABLED = 0
+INIT_PERIPHERAL = 1
+INIT_BROADCASTER = 1
+INIT_CENTRAL = 0
+INIT_OBSERVER = 0
+
+
+# Optimize for size
+MXC_OPTIMIZE_CFLAGS = -Os
+
+# Use local linkerfile
+LINKERFILE = ota.ld
