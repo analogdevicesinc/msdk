28e4314234c408e0285b3b07232d97bf85a4680a
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2022-09-27 15:20:13 -0500

    SPIXR: Fixed linter errors and added SPIXR pins to ME18.

--- Libraries/PeriphDrivers/Source/SYS/pins_me18.c
+++ Libraries/PeriphDrivers/Source/SYS/pins_me18.c
@@ -198,6 +198,16 @@ const mxc_gpio_cfg_t gpio_cfg_spi4_ss1 = { MXC_GPIO1, (MXC_GPIO_PIN_6), MXC_GPIO
 const mxc_gpio_cfg_t gpio_cfg_spi4_ss2 = { MXC_GPIO1, (MXC_GPIO_PIN_11), MXC_GPIO_FUNC_ALT1,
                                            MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO };
 
+const mxc_gpio_cfg_t gpio_cfg_spixr = { MXC_GPIO0,
+                                       (MXC_GPIO_PIN_1 | MXC_GPIO_PIN_2 | MXC_GPIO_PIN_3 | MXC_GPIO_PIN_4 | MXC_GPIO_PIN_5 | MXC_GPIO_PIN_6),
+                                       MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_NONE,
+                                       MXC_GPIO_VSSEL_VDDIO };
+
+const mxc_gpio_cfg_t gpio_cfg_spixf = { MXC_GPIO0,
+                                       (MXC_GPIO_PIN_1 | MXC_GPIO_PIN_2 | MXC_GPIO_PIN_3 | MXC_GPIO_PIN_4 | MXC_GPIO_PIN_5 | MXC_GPIO_PIN_6),
+                                       MXC_GPIO_FUNC_ALT2, MXC_GPIO_PAD_NONE,
+                                       MXC_GPIO_VSSEL_VDDIO };
+
 const mxc_gpio_cfg_t gpio_cfg_rtcsqw = { MXC_GPIO4, MXC_GPIO_PIN_1, MXC_GPIO_FUNC_ALT1,
                                          MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO };
 
