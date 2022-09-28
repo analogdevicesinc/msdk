7a68c56072cacda379fc745242937602ccffabd2
Jake Carter <Jake.Carter@analog.com>
2022-09-28 10:06:00 -0500

    Remove EMV from NFC_PCD_DTE

--- Examples/MAX32570/NFC_PCD_DTE_EMV_Loopback/project.mk
+++ Examples/MAX32570/NFC_PCD_DTE_EMV_Loopback/project.mk
@@ -14,13 +14,8 @@ ifeq ("$(wildcard $(LIBS_DIR)/NFC)","")
 $(error This demo example requires NFC package. Please install the NFC package to Libraries/NFC.)
 endif
 
-ifeq ("$(wildcard $(LIBS_DIR)/EMV)","")
-$(error This demo example requires EMV package. Please install the EMV package to Libraries/EMV.)
-endif
-
 # Enable NFC library
 LIB_NFC = 1
-LIB_EMV = 1
 
 # Enable SBT
 SBT = 1
