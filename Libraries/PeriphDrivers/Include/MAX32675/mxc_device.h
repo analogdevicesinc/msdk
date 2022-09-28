ed5e2aacc7b832e6961451247e287bf2a5e934d8
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2022-09-28 10:53:09 -0500

    ME16: Resolve linter errors for SPI to WUT in PeriphDrivers/Include.

--- Libraries/PeriphDrivers/Include/MAX32675/mxc_device.h
+++ Libraries/PeriphDrivers/Include/MAX32675/mxc_device.h
@@ -36,8 +36,8 @@
  *
  ******************************************************************************/
 
-#ifndef _MXC_DEVICE_H_
-#define _MXC_DEVICE_H_
+#ifndef LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32675_MXC_DEVICE_H_
+#define LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32675_MXC_DEVICE_H_
 
 #include "max32675.h"
 #include "mxc_errors.h"
@@ -71,4 +71,4 @@
 
 #endif /* if(TARGET_REV == ...) */
 
-#endif /* _MXC_DEVICE_H_ */
+#endif // LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32675_MXC_DEVICE_H_
--- Libraries/PeriphDrivers/Include/MAX32675/trng.h
+++ Libraries/PeriphDrivers/Include/MAX32675/trng.h
@@ -36,8 +36,8 @@
  *
  *************************************************************************** */
 
-#ifndef _TRNG_H_
-#define _TRNG_H_
+#ifndef LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32675_TRNG_H_
+#define LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32675_TRNG_H_
 
 /***** Includes *****/
 #include "trng_regs.h"
@@ -140,4 +140,4 @@ int MXC_TRNG_HealthTest(void);
 #endif
 /**@} end of group trng */
 
-#endif /* _TRNG_H_ */
+#endif // LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32675_TRNG_H_
