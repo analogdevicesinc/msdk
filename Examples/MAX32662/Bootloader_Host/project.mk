ee16dd3ca6fd5c3fa03744fb5e47391fd4643136
Jake Carter <jake.carter@analog.com>
2022-09-28 10:41:37 -0500

    Update Build System for MAX32570, MAX32662, MAX32672, MAX32675 (#107)
    * Update Makefiles & project.mk
    
    * Add max32570_sla.ld file for SBTs
    
    * Add NFC to libs.mk
    
    * Add EMV to libs.mk
    
    * Set optimization level to -O1 for NFC/EMV demos because of reported runtime error at -O3
    
    * Add development vs. release setup for NFC library
    
    By default, libs.mk will attempt to link the pre-compiled release
    libraries for NFC.  For development, DEV_LIB_NFC=1 can be used to add
    the configuration for building from source.

---
+++ Examples/MAX32662/Bootloader_Host/project.mk
@@ -0,0 +1,15 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+IPATH += inc
+IPATH += bootloader
+IPATH += test_images
+
+VPATH += bootloader
+VPATH += test_images/MAX32662
