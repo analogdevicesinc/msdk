513f50dd388c363f0d6207e04e9bee1f94ef9170
Jake Carter <jake.carter@analog.com>
2022-09-28 12:39:26 -0500

    Upgrade MAX32665 (ME14) Build System (#114)
    * Add project.mk files for MAX32665
    
    * Look for fw_update.bin in build/buildfw
    
    * Update core Makefiles
    
    * Replace instances of 'Makefile' in readmes with link to project.mk

---
+++ Examples/MAX32665/BLE_mcs/project.mk
@@ -0,0 +1,31 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+# Enable CORDIO library
+LIB_CORDIO = 1
+
+# CORDIO library options
+INIT_PERIPHERAL = 1
+INIT_BROADCASTER = 0
+INIT_CENTRAL = 0
+INIT_OBSERVER = 0
+
+# TRACE option
+# Set to 1 to enable serial port trace messages
+# Set to 0 to disable
+TRACE = 1
+
+# Optimize for size
+MXC_OPTIMIZE_CFLAGS = -Os
+
+# Add services and profiles folders to build
+IPATH += services
+VPATH += services
+IPATH += profiles/mcs
+VPATH += profiles/mcs
