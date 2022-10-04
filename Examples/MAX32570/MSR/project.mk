5fadf7763c6f0898e9d901b24fbcb4c6cc044f63
Jake Carter <Jake.Carter@analog.com>
2022-10-04 11:12:02 -0500

    Clean up project.mk extra lines

--- Examples/MAX32570/MSR/project.mk
+++ Examples/MAX32570/MSR/project.mk
@@ -5,8 +5,6 @@
 # For instructions on how to use this system, see
 # https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
 
-
-
 # **********************************************************
 
 # Set SDMA size by passing a flag to the assembler
