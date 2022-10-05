d12855802b73f39fddbd1e152fe258301052723f
Jake Carter <Jake.Carter@analog.com>
2022-10-05 17:11:14 -0500

    Fix Bootloader linter errors

--- Examples/MAX32655/Bootloader/main.c
+++ Examples/MAX32655/Bootloader/main.c
@@ -102,7 +102,7 @@ void ledFailPattern(void)
     }
 }
 
-/* http://home.thep.lu.se/~bjorn/crc/ */
+// http://home.thep.lu.se/~bjorn/crc/
 /*************************************************************************************************/
 /*!
  *  \brief  Create the CRC32 table.
@@ -132,10 +132,15 @@ uint32_t crc32_for_byte(uint32_t r)
 static uint32_t table[0x100] = { 0 };
 void crc32(const void *data, size_t n_bytes, uint32_t *crc)
 {
-    if (!*table)
-        for (size_t i = 0; i < 0x100; ++i) table[i] = crc32_for_byte(i);
-    for (size_t i = 0; i < n_bytes; ++i)
+    if (!*table) {
+        for (size_t i = 0; i < 0x100; ++i) {
+            table[i] = crc32_for_byte(i);
+        }
+    }
+
+    for (size_t i = 0; i < n_bytes; ++i) {
         *crc = table[(uint8_t)*crc ^ ((uint8_t *)data)[i]] ^ *crc >> 8;
+    }
 }
 
 void bootError(void)
