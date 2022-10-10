fb7bb620fe1d981be41bb6e4a7920bbad1256df9
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2022-10-10 13:40:18 -0500

    ME14: Added TRIMSIR_SIR13 and _SIR17 registers for LP.

--- Libraries/CMSIS/Device/Maxim/MAX32665/Include/trimsir_regs.h
+++ Libraries/CMSIS/Device/Maxim/MAX32665/Include/trimsir_regs.h
@@ -1,44 +1,44 @@
 /**
  * @file    trimsir_regs.h
  * @brief   Registers, Bit Masks and Bit Positions for the TRIMSIR Peripheral Module.
+ * @note    This file is @generated.
  */
 
-/* ****************************************************************************
- * Copyright (C) Maxim Integrated Products, Inc., All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
- * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * Except as contained in this notice, the name of Maxim Integrated
- * Products, Inc. shall not be used except as stated in the Maxim Integrated
- * Products, Inc. Branding Policy.
- *
- * The mere transfer of this software does not imply any licenses
- * of trade secrets, proprietary technology, copyrights, patents,
- * trademarks, maskwork rights, or any other form of intellectual
- * property whatsoever. Maxim Integrated Products, Inc. retains all
- * ownership rights.
- *
- *
- *************************************************************************** */
+/******************************************************************************
+* Copyright (C) 2022 Maxim Integrated Products, Inc., All Rights Reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* Except as contained in this notice, the name of Maxim Integrated
+* Products, Inc. shall not be used except as stated in the Maxim Integrated
+* Products, Inc. Branding Policy.
+*
+* The mere transfer of this software does not imply any licenses
+* of trade secrets, proprietary technology, copyrights, patents,
+* trademarks, maskwork rights, or any other form of intellectual
+* property whatsoever. Maxim Integrated Products, Inc. retains all
+* ownership rights.
+*
+******************************************************************************/
 
-#ifndef _TRIMSIR_REGS_H_
-#define _TRIMSIR_REGS_H_
+#ifndef LIBRARIES_CMSIS_DEVICE_MAXIM_MAX32665_INCLUDE_TRIMSIR_REGS_H_
+#define LIBRARIES_CMSIS_DEVICE_MAXIM_MAX32665_INCLUDE_TRIMSIR_REGS_H_
 
 /* **** Includes **** */
 #include <stdint.h>
@@ -78,7 +78,7 @@ extern "C" {
  * @ingroup     trimsir
  * @defgroup    trimsir_registers TRIMSIR_Registers
  * @brief       Registers, Bit Masks and Bit Positions for the TRIMSIR Peripheral Module.
- * @details Trim System Initilazation Registers
+ * @details     Trim System Initilazation Registers
  */
 
 /**
@@ -87,38 +87,66 @@ extern "C" {
  */
 typedef struct {
     __R  uint32_t rsv_0x0_0x7[2];
-    __IO uint32_t rtc;                  /**< <tt>\b 0x08:</tt> TRIMSIR RTC Register */
+    __I  uint32_t rtc;                  /**< <tt>\b 0x08:</tt> TRIMSIR RTC Register */
+    __R  uint32_t rsv_0xc_0x33[10];
+    __IO uint32_t sir13;                /**< <tt>\b 0x34:</tt> TRIMSIR SIR13 Register */
+    __R  uint32_t rsv_0x38_0x43[3];
+    __IO uint32_t sir17;                /**< <tt>\b 0x44:</tt> TRIMSIR SIR17 Register */
 } mxc_trimsir_regs_t;
 
 /* Register offsets for module TRIMSIR */
 /**
  * @ingroup    trimsir_registers
  * @defgroup   TRIMSIR_Register_Offsets Register Offsets
- * @brief      TRIMSIR Peripheral Register Offsets from the TRIMSIR Base Peripheral Address. 
+ * @brief      TRIMSIR Peripheral Register Offsets from the TRIMSIR Base Peripheral Address.
  * @{
  */
- #define MXC_R_TRIMSIR_RTC                  ((uint32_t)0x00000008UL) /**< Offset from TRIMSIR Base Address: <tt> 0x0000</tt> */ 
+#define MXC_R_TRIMSIR_RTC                  ((uint32_t)0x00000008UL) /**< Offset from TRIMSIR Base Address: <tt> 0x0008</tt> */
+#define MXC_R_TRIMSIR_SIR13                ((uint32_t)0x00000034UL) /**< Offset from TRIMSIR Base Address: <tt> 0x0034</tt> */
+#define MXC_R_TRIMSIR_SIR17                ((uint32_t)0x00000044UL) /**< Offset from TRIMSIR Base Address: <tt> 0x0044</tt> */
 /**@} end of group trimsir_registers */
 
 /**
  * @ingroup  trimsir_registers
  * @defgroup TRIMSIR_RTC TRIMSIR_RTC
- * @brief    RTC Trim System Initialization Register.
+ * @brief    System Init. Configuration Register 2.
  * @{
  */
- #define MXC_F_TRIMSIR_RTC_RTCX1_POS                    16 /**< RTC_RTCX1 Position */
- #define MXC_F_TRIMSIR_RTC_RTCX1                        ((uint32_t)(0x1FUL << MXC_F_TRIMSIR_RTC_RTCX1_POS)) /**< RTC_RTCX1 Mask */
+#define MXC_F_TRIMSIR_RTC_RTCX1_POS                    16 /**< RTC_RTCX1 Position */
+#define MXC_F_TRIMSIR_RTC_RTCX1                        ((uint32_t)(0x1FUL << MXC_F_TRIMSIR_RTC_RTCX1_POS)) /**< RTC_RTCX1 Mask */
 
- #define MXC_F_TRIMSIR_RTC_RTCX2_POS                    21 /**< RTC_RTCX2 Position */
- #define MXC_F_TRIMSIR_RTC_RTCX2                        ((uint32_t)(0x1FUL << MXC_F_TRIMSIR_RTC_RTCX2_POS)) /**< RTC_RTCX2 Mask */
+#define MXC_F_TRIMSIR_RTC_RTCX2_POS                    21 /**< RTC_RTCX2 Position */
+#define MXC_F_TRIMSIR_RTC_RTCX2                        ((uint32_t)(0x1FUL << MXC_F_TRIMSIR_RTC_RTCX2_POS)) /**< RTC_RTCX2 Mask */
 
- #define MXC_F_TRIMSIR_RTC_LOCK_POS                     31 /**< RTC_LOCK Position */
- #define MXC_F_TRIMSIR_RTC_LOCK                         ((uint32_t)(0x1UL << MXC_F_TRIMSIR_RTC_LOCK_POS)) /**< RTC_LOCK Mask */
+#define MXC_F_TRIMSIR_RTC_LOCK_POS                     31 /**< RTC_LOCK Position */
+#define MXC_F_TRIMSIR_RTC_LOCK                         ((uint32_t)(0x1UL << MXC_F_TRIMSIR_RTC_LOCK_POS)) /**< RTC_LOCK Mask */
 
 /**@} end of group TRIMSIR_RTC_Register */
+
+/**
+ * @ingroup  trimsir_registers
+ * @defgroup TRIMSIR_SIR13 TRIMSIR_SIR13
+ * @brief    System Init. Configuration Register 13.
+ * @{
+ */
+#define MXC_F_TRIMSIR_SIR13_SIMOCLKDIV_POS             0 /**< SIR13_SIMOCLKDIV Position */
+#define MXC_F_TRIMSIR_SIR13_SIMOCLKDIV                 ((uint32_t)(0x3UL << MXC_F_TRIMSIR_SIR13_SIMOCLKDIV_POS)) /**< SIR13_SIMOCLKDIV Mask */
+
+/**@} end of group TRIMSIR_SIR13_Register */
+
+/**
+ * @ingroup  trimsir_registers
+ * @defgroup TRIMSIR_SIR17 TRIMSIR_SIR17
+ * @brief    System Init. Configuration Register 17.
+ * @{
+ */
+#define MXC_F_TRIMSIR_SIR17_BUCKCLKSELLP_POS           6 /**< SIR17_BUCKCLKSELLP Position */
+#define MXC_F_TRIMSIR_SIR17_BUCKCLKSELLP               ((uint32_t)(0x3UL << MXC_F_TRIMSIR_SIR17_BUCKCLKSELLP_POS)) /**< SIR17_BUCKCLKSELLP Mask */
+
+/**@} end of group TRIMSIR_SIR17_Register */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* _TRIMSIR_REGS_H_ */
+#endif // LIBRARIES_CMSIS_DEVICE_MAXIM_MAX32665_INCLUDE_TRIMSIR_REGS_H_
