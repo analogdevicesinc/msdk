176b585944563dcd405f9ce9b0cb0b24f37f1cce
Jake Carter <Jake.Carter@analog.com>
2022-10-10 12:11:15 -0500

    Add AI87 CameraIF_Debayer

---
+++ Examples/MAX78002/CameraIF_Debayer/README.md
@@ -0,0 +1,2 @@
+# hm0360_color_MAX78000Evkit
+hm0360 Himax color sensor with MAX78000 EVKit
---
+++ Examples/MAX78002/CameraIF_Debayer/project.mk
@@ -0,0 +1,11 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+CAMERA=HM0360_COLOR
+PROJ_CFLAGS+=-DENABLE_TFT
