def26389a94529dba704520120abd168b9b7cb7e
Ying Cai <ying.cai@analog.com>
2022-10-16 13:41:37 -0500

    Applied clang-formatter on the new added c files.

--- Examples/MAX32655/Dual_core_sync_arm/main.c
+++ Examples/MAX32655/Dual_core_sync_arm/main.c
@@ -33,120 +33,125 @@
 
 /**
  * @file    main.c
- * @brief   A basic getting started program for the RISCV, run from the ARM core.
- * @details Dual_core_sync_arm runs on the ARM core to load the RISCV code space, setup the RISCV debugger pins,
-            and start the RISCV core.
+ * @brief   A basic getting started program for the RISCV, run from the ARM
+ * core.
+ * @details Dual_core_sync_arm runs on the ARM core to load the RISCV code
+ * space, setup the RISCV debugger pins, and start the RISCV core.
  */
 
 /***** Includes *****/
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include "mxc_device.h"
 #include "mxc_delay.h"
+#include "mxc_device.h"
 #include "mxc_sys.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "sema_reva.h"
 
 /***** Definitions *****/
-#define DUAL_CORE_SYNC          (1)
+#define DUAL_CORE_SYNC (1)
 
-#define NDX_ARM                 (0)
-#define NDX_RISCV               (1)
+#define NDX_ARM (0)
+#define NDX_RISCV (1)
 
 #define MAILBOX_OVERHEAD (2 * sizeof(uint16_t))
 #define MAILBOX_PAYLOAD_LEN (MAILBOX_SIZE - MAILBOX_OVERHEAD)
 typedef struct {
-    uint16_t readLocation;
-    uint16_t writeLocation;
+  uint16_t readLocation;
+  uint16_t writeLocation;
 #if (MAILBOX_SIZE == 0)
-    uint8_t payload[1];
+  uint8_t payload[1];
 #else
-    uint8_t payload[MAILBOX_PAYLOAD_LEN];
+  uint8_t payload[MAILBOX_PAYLOAD_LEN];
 #endif
 
 } mxcSemaBox_t;
 
 /***** Globals *****/
-extern mxcSemaBox_t *mxcSemaBox0;  // ARM writes, RISCV reads,
-extern mxcSemaBox_t *mxcSemaBox1;  // ARM reads,  RISCV writes
+extern mxcSemaBox_t *mxcSemaBox0; // ARM writes, RISCV reads,
+extern mxcSemaBox_t *mxcSemaBox1; // ARM reads,  RISCV writes
 
 /***** Functions *****/
 
 // *****************************************************************************
-int main(void)
-{
+int main(void) {
 #if DUAL_CORE_SYNC
-    printf("\n\n\n-----------------------------------\n");
-    printf("ARM   : Start.\n");
+  printf("\n\n\n-----------------------------------\n");
+  printf("ARM   : Start.\n");
 
-    MXC_SEMA_Init();
+  MXC_SEMA_Init();
 
-    int ret = MXC_SEMA_CheckSema(NDX_ARM);
-    printf("ARM   : After init, CheckSema(%d) returned %s.\n", NDX_ARM, ret == E_BUSY ? "BUSY" : "NOT BUSY");
+  int ret = MXC_SEMA_CheckSema(NDX_ARM);
+  printf("ARM   : After init, CheckSema(%d) returned %s.\n", NDX_ARM,
+         ret == E_BUSY ? "BUSY" : "NOT BUSY");
 
-    if ((MXC_SEMA_GetSema(NDX_ARM)) == E_NO_ERROR) {
-        printf("ARM   : GetSema returned NOT BUSY with previous semaphore value %d.\n", MXC_SEMA->semaphores[NDX_ARM]);
-    } else {
-        printf("ARM   : GetSema returned - BUSY - with previous semaphore value %d.\n", MXC_SEMA->semaphores[NDX_ARM]);
-    }
+  if ((MXC_SEMA_GetSema(NDX_ARM)) == E_NO_ERROR) {
+    printf(
+        "ARM   : GetSema returned NOT BUSY with previous semaphore value %d.\n",
+        MXC_SEMA->semaphores[NDX_ARM]);
+  } else {
+    printf(
+        "ARM   : GetSema returned - BUSY - with previous semaphore value %d.\n",
+        MXC_SEMA->semaphores[NDX_ARM]);
+  }
 
-    printf("ARM   : Wait 2 secs then start the RISC-V core.\n");
+  printf("ARM   : Wait 2 secs then start the RISC-V core.\n");
 #endif
-    MXC_Delay(MXC_DELAY_SEC(2));
+  MXC_Delay(MXC_DELAY_SEC(2));
 
-    /* Enable RISCV debugger GPIO */
-    MXC_GPIO_Config(&gpio_cfg_rv_jtag);
+  /* Enable RISCV debugger GPIO */
+  MXC_GPIO_Config(&gpio_cfg_rv_jtag);
 
-    /* Start the RISCV core */
-    MXC_SYS_RISCVRun();
-    
-#if DUAL_CORE_SYNC    
-    /* Wait the RISC-V core to start */
-    ret = E_BUSY;
-    while (E_BUSY == ret) {
-        ret = MXC_SEMA_CheckSema(NDX_ARM);
-    }
-    MXC_SEMA_GetSema(NDX_ARM);
+  /* Start the RISCV core */
+  MXC_SYS_RISCVRun();
 
-    /* Init code here. */
-    printf("ARM   : Do initialization works here.\n");
-    MXC_SEMA_InitBoxes();
-
-    /* Signal RISC-V core to run */
-    printf("ARM   : Signal RISC-V.\n");
-    MXC_SEMA_FreeSema(NDX_RISCV);
-
-    uint32_t cnt;
-#endif
-    /* Enter LPM */
-    while (1) {
 #if DUAL_CORE_SYNC
-        /* Wait */
-        ret = MXC_SEMA_CheckSema(NDX_ARM);
-        if (E_BUSY != ret) {
-            MXC_SEMA_GetSema(NDX_ARM);
+  /* Wait the RISC-V core to start */
+  ret = E_BUSY;
+  while (E_BUSY == ret) {
+    ret = MXC_SEMA_CheckSema(NDX_ARM);
+  }
+  MXC_SEMA_GetSema(NDX_ARM);
 
-            /* Do the job. */
-            // Retrieve the data from the mailbox1
-            cnt  = mxcSemaBox1->payload[0] << (8 * 0);
-            cnt += mxcSemaBox1->payload[1] << (8 * 1);
-            cnt += mxcSemaBox1->payload[2] << (8 * 2);
-            cnt += mxcSemaBox1->payload[3] << (8 * 3);
-            
-            printf("ARM   : cnt=%d\n", cnt++);            
+  /* Init code here. */
+  printf("ARM   : Do initialization works here.\n");
+  MXC_SEMA_InitBoxes();
 
-            mxcSemaBox0->payload[0] = (cnt >> 8 * 0) & 0xFF;
-            mxcSemaBox0->payload[1] = (cnt >> 8 * 1) & 0xFF;
-            mxcSemaBox0->payload[2] = (cnt >> 8 * 2) & 0xFF;
-            mxcSemaBox0->payload[3] = (cnt >> 8 * 3) & 0xFF;
+  /* Signal RISC-V core to run */
+  printf("ARM   : Signal RISC-V.\n");
+  MXC_SEMA_FreeSema(NDX_RISCV);
 
-            /* Do other jobs here. */
-            MXC_Delay(MXC_DELAY_SEC(1));
-
-            /* Signal */
-            MXC_SEMA_FreeSema(NDX_RISCV);
-        }
+  uint32_t cnt;
 #endif
+  /* Enter LPM */
+  while (1) {
+#if DUAL_CORE_SYNC
+    /* Wait */
+    ret = MXC_SEMA_CheckSema(NDX_ARM);
+    if (E_BUSY != ret) {
+      MXC_SEMA_GetSema(NDX_ARM);
+
+      /* Do the job. */
+      // Retrieve the data from the mailbox1
+      cnt = mxcSemaBox1->payload[0] << (8 * 0);
+      cnt += mxcSemaBox1->payload[1] << (8 * 1);
+      cnt += mxcSemaBox1->payload[2] << (8 * 2);
+      cnt += mxcSemaBox1->payload[3] << (8 * 3);
+
+      printf("ARM   : cnt=%d\n", cnt++);
+
+      mxcSemaBox0->payload[0] = (cnt >> 8 * 0) & 0xFF;
+      mxcSemaBox0->payload[1] = (cnt >> 8 * 1) & 0xFF;
+      mxcSemaBox0->payload[2] = (cnt >> 8 * 2) & 0xFF;
+      mxcSemaBox0->payload[3] = (cnt >> 8 * 3) & 0xFF;
+
+      /* Do other jobs here. */
+      MXC_Delay(MXC_DELAY_SEC(1));
+
+      /* Signal */
+      MXC_SEMA_FreeSema(NDX_RISCV);
     }
+#endif
+  }
 }
--- Examples/MAX32655/Dual_core_sync_riscv/main.c
+++ Examples/MAX32655/Dual_core_sync_riscv/main.c
@@ -34,114 +34,119 @@
 /**
  * @file    main.c
  * @brief   Dual-core synchronization demo code for RISC-V core.
- * @details This example uses the UART to print to a terminal and flashes an LED.
+ * @details This example uses the UART to print to a terminal and flashes an
+ * LED.
  */
 
 /***** Includes *****/
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include "mxc_device.h"
-#include "led.h"
-#include "pb.h"
 #include "board.h"
+#include "led.h"
 #include "mxc_delay.h"
+#include "mxc_device.h"
+#include "pb.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "sema_reva.h"
 
 /***** Definitions *****/
-#define DUAL_CORE_SYNC          (1)
+#define DUAL_CORE_SYNC (1)
 
-#define NDX_ARM                 (0)
-#define NDX_RISCV               (1)
+#define NDX_ARM (0)
+#define NDX_RISCV (1)
 
 #define MAILBOX_OVERHEAD (2 * sizeof(uint16_t))
 #define MAILBOX_PAYLOAD_LEN (MAILBOX_SIZE - MAILBOX_OVERHEAD)
 typedef struct {
-    uint16_t readLocation;
-    uint16_t writeLocation;
+  uint16_t readLocation;
+  uint16_t writeLocation;
 #if (MAILBOX_SIZE == 0)
-    uint8_t payload[1];
+  uint8_t payload[1];
 #else
-    uint8_t payload[MAILBOX_PAYLOAD_LEN];
+  uint8_t payload[MAILBOX_PAYLOAD_LEN];
 #endif
 
 } mxcSemaBox_t;
 
 /***** Globals *****/
-extern mxcSemaBox_t *mxcSemaBox0;  // ARM writes, RISCV reads,
-extern mxcSemaBox_t *mxcSemaBox1;  // ARM reads,  RISCV writes
+extern mxcSemaBox_t *mxcSemaBox0; // ARM writes, RISCV reads,
+extern mxcSemaBox_t *mxcSemaBox1; // ARM reads,  RISCV writes
 
 /***** Functions *****/
 
 // *****************************************************************************
-int main(void)
-{
+int main(void) {
 #if DUAL_CORE_SYNC
-    printf("\nRISC-V: Start.\n");
+  printf("\nRISC-V: Start.\n");
 #else
-    printf("Start!\n");
+  printf("Start!\n");
 #endif
 
 #if DUAL_CORE_SYNC
-    MXC_SEMA_Init(); 
+  MXC_SEMA_Init();
 
+  int ret = MXC_SEMA_CheckSema(NDX_RISCV);
+  printf("RISC-V: After init, CheckSema(%d) returned %s.\n", NDX_RISCV,
+         ret == E_BUSY ? "BUSY" : "NOT BUSY");
+
+  if ((MXC_SEMA_GetSema(NDX_RISCV)) == E_NO_ERROR) {
+    printf(
+        "RISC-V: GetSema returned NOT BUSY with previous semaphore value %d.\n",
+        MXC_SEMA->semaphores[NDX_RISCV]);
+  } else {
+    printf(
+        "RISC-V: GetSema returned - BUSY - with previous semaphore value %d.\n",
+        MXC_SEMA->semaphores[NDX_RISCV]);
+  }
+
+  /* Init code here. */
+  printf("RISC-V: Do initialization works here.\n");
+  MXC_SEMA_InitBoxes();
+
+  /* Signal ARM core to run. */
+  printf("RISC-V: Signal ARM.\n");
+  MXC_SEMA_FreeSema(NDX_ARM);
+#endif
+
+  uint32_t cnt = 0;
+
+  /* Enter LPM */
+  while (1) {
+#if DUAL_CORE_SYNC
+    /* Wait */
     int ret = MXC_SEMA_CheckSema(NDX_RISCV);
-    printf("RISC-V: After init, CheckSema(%d) returned %s.\n", NDX_RISCV, ret == E_BUSY ? "BUSY" : "NOT BUSY");
+    if (E_BUSY != ret) {
+      MXC_SEMA_GetSema(NDX_RISCV);
 
-    if ((MXC_SEMA_GetSema(NDX_RISCV)) == E_NO_ERROR) {
-        printf("RISC-V: GetSema returned NOT BUSY with previous semaphore value %d.\n", MXC_SEMA->semaphores[NDX_RISCV]);
-    } else {
-        printf("RISC-V: GetSema returned - BUSY - with previous semaphore value %d.\n", MXC_SEMA->semaphores[NDX_RISCV]);
-    }
+      /* Do the job */
+      // Retrieve the data from the mailbox0
+      cnt = mxcSemaBox0->payload[0] << (8 * 0);
+      cnt += mxcSemaBox0->payload[1] << (8 * 1);
+      cnt += mxcSemaBox0->payload[2] << (8 * 2);
+      cnt += mxcSemaBox0->payload[3] << (8 * 3);
 
-    /* Init code here. */
-    printf("RISC-V: Do initialization works here.\n");    
-    MXC_SEMA_InitBoxes();
-
-    /* Signal ARM core to run. */
-    printf("RISC-V: Signal ARM.\n");
-    MXC_SEMA_FreeSema(NDX_ARM);    
-#endif
-
-    uint32_t cnt = 0;
-
-    /* Enter LPM */
-    while (1) {
-#if DUAL_CORE_SYNC
-        /* Wait */
-        int ret = MXC_SEMA_CheckSema(NDX_RISCV);
-        if (E_BUSY != ret) {
-            MXC_SEMA_GetSema(NDX_RISCV);
-
-            /* Do the job */
-            // Retrieve the data from the mailbox0
-            cnt  = mxcSemaBox0->payload[0] << (8 * 0);
-            cnt += mxcSemaBox0->payload[1] << (8 * 1);
-            cnt += mxcSemaBox0->payload[2] << (8 * 2);
-            cnt += mxcSemaBox0->payload[3] << (8 * 3);
-
-            printf("RISC-V: cnt=%d\n", cnt++);
+      printf("RISC-V: cnt=%d\n", cnt++);
 #else
-            printf("count = %d\n", cnt++);
+    printf("count = %d\n", cnt++);
 #endif
 
 #if DUAL_CORE_SYNC
-            mxcSemaBox1->payload[0] = (cnt >> 8 * 0) & 0xFF;
-            mxcSemaBox1->payload[1] = (cnt >> 8 * 1) & 0xFF;
-            mxcSemaBox1->payload[2] = (cnt >> 8 * 2) & 0xFF;
-            mxcSemaBox1->payload[3] = (cnt >> 8 * 3) & 0xFF;
+      mxcSemaBox1->payload[0] = (cnt >> 8 * 0) & 0xFF;
+      mxcSemaBox1->payload[1] = (cnt >> 8 * 1) & 0xFF;
+      mxcSemaBox1->payload[2] = (cnt >> 8 * 2) & 0xFF;
+      mxcSemaBox1->payload[3] = (cnt >> 8 * 3) & 0xFF;
 
-            /* Do other jobs here */
+      /* Do other jobs here */
 #endif
-            LED_On(LED_RED);
-            MXC_Delay(500000);
-            LED_Off(LED_RED);
-            MXC_Delay(500000);
+      LED_On(LED_RED);
+      MXC_Delay(500000);
+      LED_Off(LED_RED);
+      MXC_Delay(500000);
 #if DUAL_CORE_SYNC
-            /* Signal */
-            MXC_SEMA_FreeSema(NDX_ARM);
-        }
-#endif
+      /* Signal */
+      MXC_SEMA_FreeSema(NDX_ARM);
     }
+#endif
+  }
 }
