570ed684fcdca5ec0651109b977a9f6522cbbdd6
ANALOG\AKara <anil.kara@analog.com>
2022-10-20 14:13:25 +0300

    [MSDK-911]MAX32520 return value update

--- Examples/MAX32520/ICC/main.c
+++ Examples/MAX32520/ICC/main.c
@@ -153,8 +153,10 @@ int main(void)
 
     if (fail == 0) {
         printf("\nEXAMPLE SUCCEEDED\n");
+        return 0;
     } else {
         printf("\nEXAMPLE FAILED\n");
+        return -1;
     }
 
     return 0;
--- Examples/MAX32520/SFE_Host/main.c
+++ Examples/MAX32520/SFE_Host/main.c
@@ -86,17 +86,17 @@ int main(void)
     printf("SPI1 pins to SFE Slave device. You can test RAM\n");
     printf("or Flash write and read loopback\n");
 
+    int fail = 0;
+
     // Configure the peripheral
     if (MXC_SPI_Init(MASTER_SPI, 1, 0, 1, 0, MASTER_SPI_SPEED) != E_NO_ERROR) {
         printf("\nError configuring SPI\n");
-
-        while (1) {}
+        return -1;
     }
 
     if (MXC_SPI_SetDataSize(MASTER_SPI, 8) != E_NO_ERROR) {
         printf("\nError setting Data size\n");
-
-        while (1) {}
+        return -1;
     }
 
     // Initialize RX buffer to store data
@@ -142,9 +142,10 @@ int main(void)
         printf("\nData Verified\n");
     } else {
         printf("\nData Not Verified\n");
+        fail = -1;
     }
 
     MXC_SPI_Shutdown(MASTER_SPI);
 
-    return 0;
+    return fail;
 }
