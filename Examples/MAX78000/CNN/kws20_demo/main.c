f30bc2bfd5432bf60897672e6cc810e87c0f0e94
Reza Ahmadi <reza.ahmadi@maximintegrated.com>
2022-10-21 00:14:49 -0700

    Formatting issue

--- Examples/MAX78000/CNN/kws20_demo/main.c
+++ Examples/MAX78000/CNN/kws20_demo/main.c
@@ -705,7 +705,7 @@ int main(void)
                 }
                 if (out_class == 20) // Unknown class
                     LED_On(LED_RED);
-                
+
                 int i = 0;
                 for (i = 0; i < SAMPLE_SIZE; i++) {
                     // printf("%d\n",serialMicBuff[(serialMicBufIndex+i)%SAMPLE_SIZE]);
@@ -716,7 +716,7 @@ int main(void)
                     snprintf(fileName, sizeof(fileName), "%04d_%s", fileCount, keywords[out_class]);
                 } else {
                     // Low confidence: add "L" at the end of file name
-                    snprintf(fileName, sizeof(fileName), "%04d_%s_L", fileCount, 
+                    snprintf(fileName, sizeof(fileName), "%04d_%s_L", fileCount,
                              keywords[out_class]);
                 }
                 if (writeSoundSnippet((char *)fileName, snippetLength, &snippet[0]) != E_NO_ERROR) {
