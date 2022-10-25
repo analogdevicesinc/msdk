0a6676f59acd8c31b4973a7aeb6d6b70943621d7
Sadik.Ozer <sadik.ozer@analog.com>
2022-10-25 15:30:32 +0300

    UCL: Add new devices MAX32650, MAX32665
    Signed-off-by: Sadik.Ozer <sadik.ozer@analog.com>

---
+++ Examples/MAX32665/UCL/project.mk
@@ -0,0 +1,23 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+# Enable UCL library
+LIB_UCL = 1
+export UCL_VERSION=2.7.0
+
+MFLOAT_ABI=hard
+MFPU_FLAGS=fpv4-sp-d16
+
+# Add project's include and source paths
+VPATH += ./src
+VPATH += ./src/cipher
+VPATH += ./src/public_key
+VPATH += ./src/mac
+
+IPATH += ./src/include
