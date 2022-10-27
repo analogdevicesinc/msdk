261a3a3bc3bb57b17d9391850d7ff23a3b5b946a
Jake Carter <jake.carter@analog.com>
2022-10-27 11:19:59 -0500

    Port memory addresses for AI87

--- Examples/MAX78002/ImgCapture/include/cnn_memutils.h
+++ Examples/MAX78002/ImgCapture/include/cnn_memutils.h
@@ -59,8 +59,13 @@ int cnn_enable(uint32_t clock_source, uint32_t clock_divider)
     MXC_GCFR->reg3 = 0xf; // Reset
     MXC_GCFR->reg1 = 0xf; // Mask memory
     MXC_GCFR->reg0 = 0xf; // Power
+    MXC_Delay(MSEC(10)); // Wait for load switches
     MXC_GCFR->reg2 = 0x0; // Iso
     MXC_GCFR->reg3 = 0x0; // Reset
+
+    if (clock_source == MXC_S_GCR_PCLKDIV_CNNCLKSEL_IPLL)
+        while ((MXC_GCR->ipll_ctrl & MXC_F_GCR_IPLL_CTRL_RDY) != MXC_F_GCR_IPLL_CTRL_RDY) {}
+    // Wait for PLL
 
     MXC_GCR->pclkdiv =
         (MXC_GCR->pclkdiv & ~(MXC_F_GCR_PCLKDIV_CNNCLKDIV | MXC_F_GCR_PCLKDIV_CNNCLKSEL)) |
@@ -90,17 +95,17 @@ int cnn_init(void)
 {
     *((volatile uint32_t *)0x50001000) = 0x00000000; // AON control
     // Quadrant 0
-    *((volatile uint32_t *)0x50100000) = 0x00100008; // Stop SM
-    *((volatile uint32_t *)0x50100004) = 0x0000040e; // SRAM control
+    *((volatile uint32_t *)0x51000000) = 0x00100008; // Stop SM
+    *((volatile uint32_t *)0x51000004) = 0x0000040e; // SRAM control
     // Quadrant 1
-    *((volatile uint32_t *)0x50500000) = 0x00100008; // Stop SM
-    *((volatile uint32_t *)0x50500004) = 0x0000040e; // SRAM control
+    *((volatile uint32_t *)0x52000000) = 0x00100008; // Stop SM
+    *((volatile uint32_t *)0x52000004) = 0x0000040e; // SRAM control
     // Quadrant 2
-    *((volatile uint32_t *)0x50900000) = 0x00100008; // Stop SM
-    *((volatile uint32_t *)0x50900004) = 0x0000040e; // SRAM control
+    *((volatile uint32_t *)0x53000000) = 0x00100008; // Stop SM
+    *((volatile uint32_t *)0x53000004) = 0x0000040e; // SRAM control
     // Quadrant 3
-    *((volatile uint32_t *)0x50D00000) = 0x00100008; // Stop SM
-    *((volatile uint32_t *)0x50D00004) = 0x0000040e; // SRAM control
+    *((volatile uint32_t *)0x54000000) = 0x00100008; // Stop SM
+    *((volatile uint32_t *)0x54000004) = 0x0000040e; // SRAM control
 
     return 1;
 }
