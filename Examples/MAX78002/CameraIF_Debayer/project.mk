d60ba2dfccbe8201a3cd8db8abe44dd67911c6e5
Jake Carter <jake.carter@analog.com>
2022-10-28 18:01:12 -0500

    Port CameraIF_Debayer to AI87

---
+++ Examples/MAX78002/CameraIF_Debayer/project.mk
@@ -0,0 +1,14 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+# **********************************************************
+
+# Set the camera driver to the HM0360 color drivers
+CAMERA=HM0360_COLOR
+
+# Enable optimization level 2 (faster code but this should be turned off for debugging)
+MXC_OPTIMIZE_CFLAGS = -O2
