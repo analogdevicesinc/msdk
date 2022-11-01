23ec82a6919be6f0b8bba873b018a46e44cc3336
Eddie <eddie.amaya@analog.com>
2022-11-01 11:11:15 -0500

    working  multi target launch script

--- .github/workflows/ci-tests/Examples_tests/test_launcher.sh
+++ .github/workflows/ci-tests/Examples_tests/test_launcher.sh
@@ -10,7 +10,7 @@ MAIN_DEVICE_SERIAL_PORT=/dev/"$(ls -la /dev/serial/by-id | grep -n 'D309ZDEM' | 
 dut_list=(max32655 max32665 )
 # List of serial IDs for DUT, must correlate with list above
 dut_list_ID=(0409000098d9439b00000000000000000000000097969906 0409170246dfc09500000000000000000000000097969906 )
-# List of serail devices associated with eeach DUT msut correlate with device list above
+# List of serail devices associated with each DUT msut correlate with device list above
 dut_list_serial=(D309ZDFO D30A1X9X )
 
 # Will hold values of the current device undertest from the lists above
@@ -103,7 +103,7 @@ function run_notConntectedTest() {
     cd build/
     flash_with_openocd $DUT_NAME_LOWER $DUT_ID
     #place to store robotframework results
-    mkdir $EXAMPLE_TEST_PATH/results/$DUT_NAME_UPPER
+
     cd $EXAMPLE_TEST_PATH/results/$DUT_NAME_UPPER
     mkdir $PROJECT_NAME
 
@@ -120,32 +120,66 @@ function run_notConntectedTest() {
     fi
     set -e
   
-    #get back to target directory
+    # get back to target directory
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER
 }
 
+function flash_bootloader(){
+    #------ -----------Build & Flash Bootloader onto Device 2  : ME17
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/Bootloader
+    make -j8
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/Bootloader/build
+    printf "> Flashing Bootloader on DUT\r\n\r\n"
+    #not using the flash_with_openocd function here because that causes the application code to be erased and only
+    #bootloader to remain
+    set +e
+    $OPENOCD -f $OPENOCD_TCL_PATH/interface/cmsis-dap.cfg -f $OPENOCD_TCL_PATH/target/$DUT_NAME_LOWER.cfg -s $OPENOCD_TCL_PATH -c "cmsis_dap_serial  $DUT_ID" -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -c "init; reset halt" -c "program $DUT_NAME_LOWER.elf verify reset exit" >/dev/null &
+    openocd_dapLink_pid=$!
+    # wait for openocd to finish
+    while kill -0 $openocd_dapLink_pid; do
+    sleep 1
+    # we can add a timeout here if we want
+    done
+    set -e
+    # Attempt to verify the image, prevent exit on error
+    $OPENOCD -f $OPENOCD_TCL_PATH/interface/cmsis-dap.cfg -f $OPENOCD_TCL_PATH/target/$DUT_NAME_LOWER.cfg -s $OPENOCD_TCL_PATH/ -c "cmsis_dap_serial  $DUT_ID" -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -c "init; reset halt; flash verify_image $DUT_NAME_LOWER.elf; reset; exit"
+
+    # Check the return value to see if we received an error
+    if [ "$?" -ne "0" ]; then
+    # Reprogram the device if the verify failed
+    $OPENOCD -f $OPENOCD_TCL_PATH/interface/cmsis-dap.cfg -f $OPENOCD_TCL_PATH/target/$DUT_NAME_LOWER.cfg -s $OPENOCD_TCL_PATH -c "cmsis_dap_serial  $DUT_ID" -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -c "init; reset halt" -c "program $DUT_NAME_LOWER.elf verify reset exit" >/dev/null &
+    openocd_dapLink_pid=$!
+    fi
+}
+
+function erase_all_devices(){
+    # erase main device
+    erase_with_openocd $MAIN_DEVICE_NAME_LOWER $MAIN_DEVICE_ID
+    # erase DUTs
+    for device  in ${!dut_list[@]}; do
+        erase_with_openocd ${dut_list[device]} ${dut_list_ID[device]}
+    done
+}
 function project_marker() {
     echo "=============================================================================="
-    printf "> Start of testing $DUT_NAME_LOWER ($DUT_NAME_UPPER) \r\n> ID:$DUT_ID \r\n> Port:$DUT_SERIAL_PORT \r\n"
+    printf "> Start of testing $DUT_NAME_LOWER ($DUT_NAME_UPPER) \r\n> ID:$DUT_ID \r\n> Port:$DUT_SERIAL_PORT \r\n\r\n"
 }
 
 #***************************************** Start of test script *************************************
 #****************************************************************************************************
-#remove old robtoframework logs
+
+# remove old robtoframework logs
 cd $EXAMPLE_TEST_PATH/results/
 rm -rf *
-# erase main device
-erase_with_openocd $MAIN_DEVICE_NAME_LOWER $MAIN_DEVICE_ID
+
+erase_all_devices
 
 for device  in ${!dut_list[@]}; do
-    #erase the helper devices so that they are not running an app that might connect
-    #to current app under test
-    erase_with_openocd ${dut_list[device]} ${dut_list_ID[device]}
-    # change advertising name for projects under test to avoid 
-    # connections with office devices
     # translate device name to uppercase
     DUT_NAME_UPPER=$(echo ${dut_list[i]} | tr '[:lower:]' '[:upper:]')
-    echo changing advertising names in $MSDK_DIR/Examples/$DUT_NAME_UPPER
+    # change advertising name for projects under test to avoid 
+    # connections with office devices
+    printf "> changing advertising names : $MSDK_DIR/Examples/$DUT_NAME_UPPER\r\n\r\n"
     # cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_dats
     # perl -i -pe "s/\'D\'/\'C\'/g" dats_main.c
 
@@ -174,6 +208,7 @@ for dir in ${SUBDIRS}; do
     # make -C ${dir} -j8
 done
 
+# itterate through devices and conduct standlaones test followed by connected tests
 for i in ${!dut_list[@]}; do
     
     # setup  all DUT varaibles
@@ -182,14 +217,15 @@ for i in ${!dut_list[@]}; do
     DUT_SERIAL_PORT=/dev/$(ls -la /dev/serial/by-id | grep -n ${dut_list_serial[i]} | rev | cut -b 1-7 | rev)
     DUT_ID=${dut_list_ID[i]}
   
-
+    # directory for test resutls
+    mkdir $EXAMPLE_TEST_PATH/results/$DUT_NAME_UPPER
     # enter current device under test directory
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER
 
     #no filter would support ALL projects not just ble
     project_filter='BLE_'
  
-    # Run non connected tests
+    #----------------------------- Run non connected tests
     for dir in ./*/; do
         if [[ "$dir" == *"$project_filter"* ]]; then
 
@@ -238,7 +274,7 @@ for i in ${!dut_list[@]}; do
                 # cd $PROJECT_NAME
                 # make -j8
                 # let "numOfFailedTests+=$?"
-                # cd $MSDK_DIR/Examples/$TARGET_UC
+               
                 ;;
 
             *) ;;
@@ -251,21 +287,21 @@ for i in ${!dut_list[@]}; do
 
     done # end non connected tests
 
-    # start conencted tests
+    #--------------------------start Datc/Dats conencted tests
 
-    # Flash main device with BLE_datc
+    # Flash MAIN_DEVICE with BLE_datc
     cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_datc/build
     printf "> Flashing BLE_datc on main device: $MAIN_DEVICE_NAME_UPPER\r\n "
     flash_with_openocd $MAIN_DEVICE_NAME_LOWER $MAIN_DEVICE_ID
 
     # flash DUT with BLE_dats
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_dats/build
-    echo "> Flashing BLE_dats $DUT_NAME_UPP"
+    echo "> Flashing BLE_dats on DUT $DUT_NAME_UPP"
     flash_with_openocd $DUT_NAME_LOWER $DUT_ID
 
     # Run robot test
     # give them time to connect
-    sleep 5 # give them time to connect
+    sleep 5 
     # directory for resuilts logs
     cd $EXAMPLE_TEST_PATH/results/$DUT_NAME_UPPER/
     mkdir BLE_dat_cs
@@ -277,12 +313,69 @@ for i in ${!dut_list[@]}; do
     if [ "$testResult" -ne "0" ]; then
         # update failed test count
         let "numOfFailedTests+=$testResult"
-        failedTestList+="| BLE_dat_cs "
+        failedTestList+="| BLE_dat_cs ($DUT_NAME_UPPER) "
     fi
     set -e
 
     # make sure device under test is not left with a running app 
     erase_with_openocd $DUT_NAME_LOWER $DUT_ID
 
+    #--------------------------start Otac/Otas conencted tests
+
+    # directory for resuilts logs
+    cd $EXAMPLE_TEST_PATH/results/$DUT_NAME_UPPER/
+    mkdir BLE_ota_cs
+    cd $EXAMPLE_TEST_PATH/tests
+
+    # flash Firmware V1 BLE_otas  onto DUT
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas/build
+    printf "> Flashing BLE_otas on DUT $DUT_NAME_UPP\r\n\r\n"
+    flash_with_openocd $DUT_NAME_LOWER $DUT_ID
+
+    flash_bootloader
+
+    # change firmware version and rebuild
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas
+    # change firmware version to verify otas worked
+    perl -i -pe "s/FW_VERSION 1/FW_VERSION 2/g" wdxs_file.c
+    make -j8
+
+    # flash MAIN_DEVICE with BLE_OTAC, it will use the OTAS bin with new firmware
+    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac
+    make clean
+    make -j8
+    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac/build
+    printf "> Flashing BLE_otac on main device: $MAIN_DEVICE_NAME_UPPER\r\n "
+    flash_with_openocd $MAIN_DEVICE_NAME_LOWER $MAIN_DEVICE_ID
+
+    # give time to connect
+    sleep 15
+
+    set +e
+    # runs desired test
+    cd $EXAMPLE_TEST_PATH/tests
+    $ROBOT -d $EXAMPLE_TEST_PATH/results/$DUT_NAME_UPPER/BLE_ota_cs/ -v VERBOSE:$VERBOSE_TEST -v SERIAL_PORT_1:$MAIN_DEVICE_SERIAL_PORT -v SERIAL_PORT_2:$DUT_SERIAL_PORT BLE_ota_cs.robot
+    let "testResult=$?"
+    if [ "$testResult" -ne "0" ]; then
+        # update failed test count
+        let "numOfFailedTests+=$testResult"
+        failedTestList+="| BLE_ota_cs ($DUT_NAME_UPPER) "
+    fi
+    set -e
+
 done
+
+erase_all_devices
+
+echo "=============================================================================="
+echo "=============================================================================="
+if [ "$numOfFailedTests" -ne "0" ]; then
+    echo "Test completed with $numOfFailedTests failed tests located in: "
+    echo " $failedTestList | "
+else
+    echo "Relax! ALL TESTS PASSED"
+fi
+echo "=============================================================================="
+echo "=============================================================================="
+exit $numOfFailedTests
 
