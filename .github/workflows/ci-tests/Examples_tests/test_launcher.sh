b66daa031993793107a3fe9c1e9481d53df9f8e5
Eddie <eddie.amaya@analog.com>
2022-11-04 17:30:00 -0500

    enable non connected tests

--- .github/workflows/ci-tests/Examples_tests/test_launcher.sh
+++ .github/workflows/ci-tests/Examples_tests/test_launcher.sh
@@ -232,20 +232,20 @@ for i in ${!dut_list[@]}; do
             case $PROJECT_NAME in
 
             "BLE_datc")
-                # run_notConntectedTest
+                run_notConntectedTest
 
                 ;;
 
             "BLE_dats")
-                # run_notConntectedTest
+                run_notConntectedTest
                 ;;
 
             "BLE_mcs")
-                #  run_notConntectedTest
+                run_notConntectedTest
                 ;;
 
             "BLE_fit")
-                # run_notConntectedTest
+                run_notConntectedTest
                 ;;
 
             "BLE_fcc")
@@ -256,11 +256,11 @@ for i in ${!dut_list[@]}; do
                 ;;
 
             "BLE_FreeRTOS")
-                # run_notConntectedTest
+                run_notConntectedTest
                 ;;
 
             "BLE_otac")
-                #  run_notConntectedTest
+                run_notConntectedTest
                 ;;
 
             "BLE_otas")
