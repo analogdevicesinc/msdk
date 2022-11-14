5261f837a0099f8f16b64ff44745e9f6f83ee8e2
Jacob-Scheiffler <86001820+Jacob-Scheiffler@users.noreply.github.com>
2022-11-14 13:29:07 -0600

    Adding ST7735 TFT Controller driver support and TFT display example for MAX32690. (#229)
    * Adding ST7735 TFT Controller driver.
    
    * clang-format bot reformatting.
    
    * Adding MAX32690 TFT_Demo.
    
    * clang-format bot reformatting.
    
    * Bug fix to MAX32690 TFT_Demo. Adding delay so TFT has enough time to startup on POR.
    
    * clang-format bot reformatting.
    
    * Updating examples.txt and adding bit-bang comments to board.c TFT_SPI functions.
    
    * Bumping MiscDrivers tag to resolve merge conflict.
    
    Co-authored-by: Scheiffler <Jacob.Scheiffler@analog.com>
    Co-authored-by: Jacob-Scheiffler <Jacob-Scheiffler@users.noreply.github.com>

--- Libraries/Boards/MAX32690/EvKit_V1/Source/board.c
+++ Libraries/Boards/MAX32690/EvKit_V1/Source/board.c
@@ -32,17 +32,19 @@
  ******************************************************************************/
 
 #include <stdio.h>
-#include "mxc_device.h"
-#include "mxc_sys.h"
-#include "mxc_assert.h"
 #include "board.h"
-#include "uart.h"
+#include "Ext_Flash.h"
 #include "gpio.h"
-#include "mxc_pins.h"
 #include "led.h"
+#include "mxc_assert.h"
+#include "mxc_delay.h"
+#include "mxc_device.h"
+#include "mxc_pins.h"
+#include "mxc_sys.h"
 #include "pb.h"
 #include "spixf.h"
-#include "Ext_Flash.h"
+#include "tft_st7735.h"
+#include "uart.h"
 
 /***** Global Variables *****/
 mxc_uart_regs_t *ConsoleUart = MXC_UART_GET_UART(CONSOLE_UART);
@@ -62,6 +64,22 @@ const mxc_gpio_cfg_t led_pin[] = {
     { MXC_GPIO0, MXC_GPIO_PIN_9, MXC_GPIO_FUNC_OUT, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO },
 };
 const unsigned int num_leds = (sizeof(led_pin) / sizeof(mxc_gpio_cfg_t));
+
+const mxc_gpio_cfg_t tft_mosi = (mxc_gpio_cfg_t){ .port = MXC_GPIO2,
+                                                  .mask = MXC_GPIO_PIN_24,
+                                                  .func = MXC_GPIO_FUNC_OUT,
+                                                  .pad = MXC_GPIO_PAD_NONE,
+                                                  .vssel = MXC_GPIO_VSSEL_VDDIOH };
+const mxc_gpio_cfg_t tft_clk = (mxc_gpio_cfg_t){ .port = MXC_GPIO2,
+                                                 .mask = MXC_GPIO_PIN_25,
+                                                 .func = MXC_GPIO_FUNC_OUT,
+                                                 .pad = MXC_GPIO_PAD_NONE,
+                                                 .vssel = MXC_GPIO_VSSEL_VDDIOH };
+const mxc_gpio_cfg_t tft_cs = (mxc_gpio_cfg_t){ .port = MXC_GPIO2,
+                                                .mask = MXC_GPIO_PIN_11,
+                                                .func = MXC_GPIO_FUNC_OUT,
+                                                .pad = MXC_GPIO_PAD_NONE,
+                                                .vssel = MXC_GPIO_VSSEL_VDDIOH };
 
 /******************************************************************************/
 void mxc_assert(const char *expr, const char *file, int line)
@@ -129,6 +147,73 @@ int Console_Shutdown(void)
 int Console_PrepForSleep(void)
 {
     return MXC_UART_ReadyForSleep(ConsoleUart);
+}
+
+/******************************************************************************/
+void TFT_SPI_Init(void)
+{
+    // The TFT SPI on the MAX32690 EV Kit is bit-banged so there's no need to initialize
+    // a SPI instance. Instead, we only need to configure the GPIO pins which are used to
+    // connect to the SPI bus.
+
+    // Configure GPIO pins
+    MXC_GPIO_Config(&tft_mosi);
+    MXC_GPIO_Config(&tft_clk);
+    MXC_GPIO_Config(&tft_cs);
+
+    // Set chip select and clock lines high (idle state)
+    MXC_GPIO_OutSet(tft_cs.port, tft_cs.mask);
+    MXC_GPIO_OutSet(tft_clk.port, tft_clk.mask);
+}
+
+/******************************************************************************/
+void TFT_SPI_Write(uint8_t *datain, uint32_t count, bool data)
+{
+    // The TFT SPI on the MAX32690 EV Kit is not connected to a SPI instance so it
+    // must be bit-banged.
+
+    uint8_t tx_byte;
+
+    if (datain == NULL) {
+        return;
+    }
+
+    MXC_GPIO_OutClr(tft_cs.port, tft_cs.mask); // Assert chip select (active low)
+    MXC_Delay(10);
+
+    for (int i = 0; i < count; i++) {
+        tx_byte = datain[i];
+
+        for (int j = 0; j < 9; j++) { // 9 Bits per character
+            tft_clk.port->out_clr = tft_clk.mask; //Clk low
+
+            if (j == 0) { //Send D/CX bit before MSB
+                if (data) {
+                    tft_mosi.port->out_set = tft_mosi.mask;
+                } else {
+                    tft_mosi.port->out_clr = tft_mosi.mask;
+                }
+            } else {
+                if (tx_byte & 0x80) { //Shift out data MSB first
+                    tft_mosi.port->out_set = tft_mosi.mask;
+                } else {
+                    tft_mosi.port->out_clr = tft_mosi.mask;
+                }
+                tx_byte = tx_byte << 1;
+            }
+
+            for (int k = 0; k < 2; k++) {}
+
+            tft_clk.port->out_set = tft_clk.mask; // Clk high
+
+            for (int k = 0; k < 2; k++) {}
+        }
+    }
+
+    MXC_Delay(10);
+    MXC_GPIO_OutSet(tft_cs.port, tft_cs.mask); // De-assert chip select (active low)
+
+    return;
 }
 
 /******************************************************************************/
