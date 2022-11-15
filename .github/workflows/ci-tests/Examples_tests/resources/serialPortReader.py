740da8506352e48c4ba9593f062014525a1dc450
Eddie <eddie.amaya@analog.com>
2022-11-15 10:11:42 -0600

    improves ci-tests, adds me14 to tests

---
+++ .github/workflows/ci-tests/Examples_tests/resources/serialPortReader.py
@@ -0,0 +1,106 @@
+import serial
+import time
+from robot.api import logger
+from robot.libraries.BuiltIn import BuiltIn
+def write_to_console(s):
+    logger.console(s,newline=False)
+def expect_and_timeout(send=None,expect=None, timeout= 10, port=None):
+    timeStart = time.time()
+    with serial.Serial() as ser:
+        ser.baudrate = 115200
+        ser.port = port
+        ser.timeout=1
+        ser.open()
+  
+        write_to_console("\n> Trying to open port: " + port  + "\n")
+        while ser.is_open != True:
+            time.sleep(0.1)
+        if ser.is_open == True:
+            write_to_console("> Port opened successfully: " + port  + "\n")
+            #flush junk
+            ser.write(bytes("\n", encoding='utf-8'))
+            ser.reset_input_buffer()
+            ser.reset_output_buffer()
+            time.sleep(0.1)
+            ser.read_all()
+            time.sleep(0.1)
+            # send data if any
+            if send != None:
+                write_to_console("> Writing to port: " + port  + "\n")
+                time.sleep(0.1)
+                # start test, send command
+                ser.write(bytes(send, encoding='utf-8'))
+            # read lines
+            while (time.time()-timeStart) < timeout:
+                x=ser.readline().decode("utf-8")
+                x=str(x)
+                if x != "":
+                    write_to_console(x)
+                    if expect in x:
+                        write_to_console("\r\n")
+                        ser.close()
+                        BuiltIn().pass_execution(".")
+            # test timedout , try one more time
+            write_to_console("\r\nTest Timeout trying one more time")
+            if send != None:
+                write_to_console("> Writing to port: " + port  + "\n")
+                ser.write(bytes("\n", encoding='utf-8'))
+                time.sleep(0.1)
+                ser.write(bytes(send, encoding='utf-8'))
+            # new start time, same timeout
+            timeStart = time.time()
+            while (time.time()-timeStart) < timeout:
+                x=ser.readline().decode("utf-8")
+                x=str(x)
+                if x != "":
+                    write_to_console(x)
+                    if expect in x:
+                        write_to_console("\r\n")
+                        ser.close()
+                        BuiltIn().pass_execution(".")
+
+            ser.close()
+            BuiltIn().fail(".")
+        else:
+            write_to_console("Failed to reopen port\r\n")
+
+def read_all(expect=None,timeout=10,port = None):
+    timeStart = time.time()
+    with serial.Serial() as ser:
+        ser.baudrate = 115200
+        ser.port = port
+        ser.timeout=1
+        ser.open()
+        write_to_console("\n> Trying to open port: " + port  + "\n")
+        while ser.is_open != True:
+            time.sleep(0.01)
+        if ser.is_open == True:
+            write_to_console("> Port opened successfully: " + port  + "\n")
+            while (time.time()-timeStart) < timeout:
+                x=ser.readline().decode("utf-8")
+                x=str(x)
+                if x != "":
+                    write_to_console(x)
+                    if expect in x:                       
+                        ser.close()
+                        BuiltIn().pass_execution(".\r\n")
+            # timed out
+            BuiltIn().fail(" .\r\n")
+
+def flush_junk( port=None):
+
+    with serial.Serial() as ser:
+        ser.baudrate = 115200
+        ser.port = port
+        ser.timeout=1
+        ser.open()
+        # serial tends to return before port is open
+        time.sleep(0.1)
+        # write something and read it back to get any junk data from start up
+        ser.write(bytes("\n", encoding='utf-8'))
+        # flushing does not seem to help
+        ser.reset_input_buffer()
+        ser.reset_output_buffer()
+        time.sleep(0.1)
+        ser.read_all()
+        ser.close()
