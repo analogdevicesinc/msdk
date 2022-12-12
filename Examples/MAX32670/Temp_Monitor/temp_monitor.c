e5b6f0ac4933b79c85f92ba52acadaed0a1fe154
Scheiffler <Jacob.Scheiffler@analog.com>
2022-12-12 16:03:36 -0600

    MAX32670: Fixing Lint errors in Temp_Monitor example.

--- Examples/MAX32670/Temp_Monitor/temp_monitor.c
+++ Examples/MAX32670/Temp_Monitor/temp_monitor.c
@@ -1,5 +1,35 @@
-#include "temp_monitor.h"
-
+/*******************************************************************************
+* Copyright (C) 2022 Maxim Integrated Products, Inc., All Rights Reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* Except as contained in this notice, the name of Maxim Integrated
+* Products, Inc. shall not be used except as stated in the Maxim Integrated
+* Products, Inc. Branding Policy.
+*
+* The mere transfer of this software does not imply any licenses
+* of trade secrets, proprietary technology, copyrights, patents,
+* trademarks, maskwork rights, or any other form of intellectual
+* property whatsoever. Maxim Integrated Products, Inc. retains all
+* ownership rights.
+*******************************************************************************
+*/
 #include <stdbool.h>
 #include <stdio.h>
 #include "flc.h"
@@ -10,6 +40,7 @@
 #include "max31889_driver.h"
 #include "mxc_device.h"
 #include "rtc.h"
+#include "temp_monitor.h"
 
 /***** Definitions *****/
 /***** Modifiable defines *****/
@@ -200,6 +231,8 @@ void temp_monitor_print_temps(void)
 {
     int addr_offset;
     int num_temp;
+    uint32_t temps[12]; //Read a maximum of 12 temperatures
+    uint16_t time, temp;
 
     // Calculate the number of temperature readings currently stored in flash
     num_temp = g_num_readings - (g_num_readings % 4);
@@ -214,9 +247,6 @@ void temp_monitor_print_temps(void)
         addr_offset = (num_temp - 12) * sizeof(g_temp_readings[0]);
         num_temp = 12;
     }
-
-    uint32_t temps[num_temp];
-    uint16_t time, temp;
 
     // Read all temperature readings stored in flash
     MXC_FLC_Read(TR_STORAGE_BASE_ADDR + addr_offset, temps, sizeof(temps));
