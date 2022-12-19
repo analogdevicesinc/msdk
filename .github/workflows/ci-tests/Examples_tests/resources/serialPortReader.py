4890cf33dc7e1cf3359d0eb2e789463ad6c29f45
Eddie <eddie.amaya@analog.com>
2022-12-19 14:13:45 -0600

    test old cordio and slower uart send

--- .github/workflows/ci-tests/Examples_tests/resources/serialPortReader.py
+++ .github/workflows/ci-tests/Examples_tests/resources/serialPortReader.py
@@ -6,12 +6,21 @@ from robot.libraries.BuiltIn import BuiltIn
 # stores open ports for later use
 used_ports=dict()
 #--------------------------------------------------------------------------------------
+'''
+Prints the output form serial port as well as debugging messages
+this switch is used to help differentiate by add "(robot)" to debugging messages
+'''
 def write_to_console(s, log=True):
     if log == True:
         logger.console("(robot)> "+ s ,newline=True)
     else:
         logger.console(s,newline=False)
 #--------------------------------------------------------------------------------------
+'''
+Opens up to 2 ports used during tests,
+stores in global dictionary for late use
+This is only called once upon test setup
+'''
 def open_ports(port_1=None , port_2=None):
     global used_ports
     if port_1 != None :
@@ -28,9 +37,11 @@ def open_ports(port_1=None , port_2=None):
         while used_ports[port_2].is_open != True:
             time.sleep(0.1)
         write_to_console(f"Serial Port {port_2} Opened Succesfully",True)
-       
-    
 #--------------------------------------------------------------------------------------
+'''
+Closes instances of ope ports found in global dicitonary
+This is only called once dudring test teardown
+'''
 def close_ports():
     global used_ports
     for port in used_ports.values():
@@ -42,8 +53,14 @@ def close_ports():
                 write_to_console("Serial Port Closed Succesfully\r\n")
 
         except Exception as err:
-            write_to_console("Port 2 was not open to begin with \r\n")
+            write_to_console("Port was not open to begin with \r\n")
 #--------------------------------------------------------------------------------------
+'''
+Can optioanlly send a string through serial port
+and expect a string in return on  a single port
+if timeout is exceeded the test is attempted once more
+before failing.
+'''
 def expect_and_timeout(send=None,expect=None, timeout= 10, port=None):
     attempt_count=0
     x=""
@@ -51,6 +68,7 @@ def expect_and_timeout(send=None,expect=None, timeout= 10, port=None):
     while True:
         attempt_count+=1
         timeStart = time.time()
+        char_list=[]
         while used_ports[port].is_open != True:
             time.sleep(0.1)
         if used_ports[port].is_open == True:
@@ -62,9 +80,11 @@ def expect_and_timeout(send=None,expect=None, timeout= 10, port=None):
             # send data if any
             if send != None:
                 time.sleep(0.1)
+                char_list = list(send)
+                for char in char_list:
                 # start test, send command
-                used_ports[port].write(bytes(send, encoding='utf-8'))
-                time.sleep(0.2)
+                    used_ports[port].write(bytes(char, encoding='utf-8'))
+                    time.sleep(0.2)
             # read lines
             while (time.time()-timeStart) < timeout:
                 try:
@@ -82,12 +102,16 @@ def expect_and_timeout(send=None,expect=None, timeout= 10, port=None):
     write_to_console("\r\n-------\r\n",False)
     BuiltIn().fail(".")
 #--------------------------------------------------------------------------------------
+'''
+Reads all incoming serial traffic, inclduing
+what is in the buffer, for some given time
+No multiple attemps and also expects a string to
+eventually be read, or else timeouts
+'''
 def read_all(expect=None,timeout=10,port=None):
     global used_ports
     timeStart = time.time()
-    attempt_count=0
     while True:
-        attempt_count+=1
         timeStart = time.time()   
         while used_ports[port].is_open != True:
             time.sleep(0.1)
@@ -102,20 +126,3 @@ def read_all(expect=None,timeout=10,port=None):
             # timed out
             break
     BuiltIn().fail(" .\r\n")
-#--------------------------------------------------------------------------------------
-def flush_junk( port=None):
-    with serial.Serial() as ser:
-        ser.baudrate = 115200
-        ser.port = port
-        ser.timeout=1
-        ser.open()
-        # serial tends to return before port is open
-        time.sleep(0.1)
-        # write something and read it back to get any junk data from start up
-        ser.write(bytes("\n", encoding='utf-8'))
-        # flushing does not seem to help
-        ser.reset_input_buffer()
-        ser.reset_output_buffer()
-        time.sleep(0.1)
-        ser.read_all()
-        ser.close()
