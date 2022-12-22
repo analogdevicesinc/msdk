ffa257827f41aed48744a1132b7a651927216eca
Eddie <eddie.amaya@analog.com>
2022-12-22 14:25:54 -0600

    save elfs of failed tests

--- .github/workflows/ci-tests/Examples_tests/test_launcher.sh
+++ .github/workflows/ci-tests/Examples_tests/test_launcher.sh
@@ -6,65 +6,6 @@ MSDK_DIR=$(pwd)
 failedTestList=" "
 numOfFailedTests=0
 
-#***************************************** Initial setup ********************************************
-#****************************************************************************************************
-if [ $(hostname) == "wall-e" ]; then
-    FILE=/home/$USER/Workspace/Resource_Share/boards_config.json
-    # WALL-E  paths
-    export OPENOCD_TCL_PATH=/home/btm-ci/Tools/openocd/tcl
-    export OPENOCD=/home/btm-ci/Tools/openocd/src/openocd
-    export ROBOT=/home/btm-ci/.local/bin/robot
-
-else
-    # Local- eddie desktop
-    FILE=/home/$USER/boards_config.json
-    # local paths
-    export OPENOCD_TCL_PATH=/home/eddie/workspace/openocd/tcl
-    export OPENOCD=/home/eddie/workspace/openocd/src/openocd
-    export ROBOT=/home/eddie/.local/bin/robot
-fi
-# Main device is used as client during connected tests
-MAIN_DEVICE_NAME_UPPER=MAX32655
-MAIN_DEVICE_NAME_LOWER=max32655
-MAIN_DEVICE_ID=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board1']['daplink'])"`
-main_uart=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board1']['uart0'])"`
-MAIN_DEVICE_SERIAL_PORT=/dev/"$(ls -la /dev/serial/by-id | grep -n $main_uart | rev | cut -d "/" -f1 | rev)"
-
-#get all device IDs 
-DEVICE1=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board1']['daplink'])"`
-DEVICE2=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board2']['daplink'])"`
-DEVICE3=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32665_board1']['daplink'])"`
-DEVICE4=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32690_board_w1']['daplink'])"`
-
-# setup  all DUT varaibles passed into the scripts as arguments
-# eg: test_launcher max32655 DT30DZ0 440002342304433434r323452354696
-DUT_NAME_LOWER=$1
-DUT_NAME_UPPER=$(echo $DUT_NAME_LOWER | tr '[:lower:]' '[:upper:]')
-DUT_SERIAL_PORT=/dev/$(ls -la /dev/serial/by-id | grep -n $2 | rev | cut -d "/" -f1 | rev)
-DUT_ID=$3
-
-# Directories needed for robotframework logs
-cd $EXAMPLE_TEST_PATH
-if [[ -e results ]]
-then
-    echo "results folder exists"
-else
-    mkdir $EXAMPLE_TEST_PATH/results
-fi
-
-cd $EXAMPLE_TEST_PATH/results
-mkdir $DUT_NAME_UPPER
-cd $DUT_NAME_UPPER
-mkdir BLE_dat_cs
-mkdir BLE_datc
-mkdir BLE_dats
-mkdir BLE_fit
-mkdir BLE_FreeRTOS
-mkdir BLE_mcs
-mkdir BLE_otac
-mkdir BLE_ota_cs
-
-
 #***************************************** Helper functions *****************************************
 #****************************************************************************************************
 function script_clean_up() {
@@ -76,11 +17,74 @@ function script_clean_up() {
 
     set -e
 }
-#----------------------------------------------------------------------------------------------------
 #used to kill any openocd instances should the script exit prematurely
 trap script_clean_up EXIT SIGINT
+#****************************************************************************************************
+function initial_setup(){
 
-#----------------------------------------------------------------------------------------------------
+    # Get correct boards config file and tools paths when running on Wall-E
+    if [ $(hostname) == "wall-e" ]; then
+        FILE=/home/$USER/Workspace/Resource_Share/boards_config.json
+        # WALL-E  paths
+        export OPENOCD_TCL_PATH=/home/btm-ci/Tools/openocd/tcl
+        export OPENOCD=/home/btm-ci/Tools/openocd/src/openocd
+        export ROBOT=/home/btm-ci/.local/bin/robot
+
+    else
+        # Local- eddie desktop
+        FILE=/home/$USER/boards_config.json
+        # local paths
+        export OPENOCD_TCL_PATH=/home/eddie/workspace/openocd/tcl
+        export OPENOCD=/home/eddie/workspace/openocd/src/openocd
+        export ROBOT=/home/eddie/.local/bin/robot
+    fi
+    # "Main device" is the ME17 used as the cleint dudring connected tests
+    MAIN_DEVICE_NAME_UPPER=MAX32655
+    MAIN_DEVICE_NAME_LOWER=max32655
+    MAIN_DEVICE_ID=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board1']['daplink'])"`
+    main_uart=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board1']['uart0'])"`
+    MAIN_DEVICE_SERIAL_PORT=/dev/"$(ls -la /dev/serial/by-id | grep -n $main_uart | rev | cut -d "/" -f1 | rev)"
+
+    # Get the serial number of all daplink devices, this is used to erase them all.
+    DEVICE1=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board1']['daplink'])"`
+    DEVICE2=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board2']['daplink'])"`
+    DEVICE3=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32665_board1']['daplink'])"`
+    DEVICE4=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32690_board_w1']['daplink'])"`
+
+    # setup  all DUT (Device Under Test) varaibles passed into the scripts as arguments
+    # eg: 
+    # $1 = max32655 (device name)
+    # $2 = DT30DZ0  ( UART)
+    # $3 = 440002342304433434r323452354696 (Dap-Link ID)
+    DUT_NAME_LOWER=$1
+    DUT_NAME_UPPER=$(echo $DUT_NAME_LOWER | tr '[:lower:]' '[:upper:]')
+    DUT_SERIAL_PORT=/dev/$(ls -la /dev/serial/by-id | grep -n $2 | rev | cut -d "/" -f1 | rev)
+    DUT_ID=$3
+
+    # Directories needed for robotframework logs
+    cd $EXAMPLE_TEST_PATH
+    if [[ -e results ]]
+    then
+        echo "results folder exists"
+    else
+        mkdir $EXAMPLE_TEST_PATH/results
+    fi
+
+    cd $EXAMPLE_TEST_PATH/results
+    mkdir failed_elfs
+    mkdir $DUT_NAME_UPPER
+    ls
+    cd $DUT_NAME_UPPER
+    mkdir BLE_dat_cs
+    mkdir BLE_datc
+    mkdir BLE_dats
+    mkdir BLE_fit
+    mkdir BLE_FreeRTOS
+    mkdir BLE_mcs
+    mkdir BLE_otac
+    mkdir BLE_ota_cs
+}
+#****************************************************************************************************
 # Function accepts parameters: device, CMSIS-DAP serial #
 function flash_with_openocd() {
     # mass erase and flash
@@ -101,9 +105,8 @@ function flash_with_openocd() {
         $OPENOCD -f $OPENOCD_TCL_PATH/interface/cmsis-dap.cfg -f $OPENOCD_TCL_PATH/target/$1.cfg -s $OPENOCD_TCL_PATH -c "cmsis_dap_serial  $2" -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -c "init; reset halt;max32xxx mass_erase 0" -c "program $1.elf verify reset exit" >/dev/null &
         openocd_dapLink_pid=$!
     fi
-
 }
-#----------------------------------------------------------------------------------------------------
+#****************************************************************************************************
 # Function accepts parameters: device, CMSIS-DAP serial #
 function flash_with_openocd_fast() {
     # mass erase and flash
@@ -111,10 +114,8 @@ function flash_with_openocd_fast() {
     $OPENOCD -f $OPENOCD_TCL_PATH/interface/cmsis-dap.cfg -f $OPENOCD_TCL_PATH/target/$1.cfg -s $OPENOCD_TCL_PATH -c "cmsis_dap_serial  $2" -c "gdb_port 333$3" -c "telnet_port 444$3" -c "tcl_port 666$3" -c "init; reset halt;max32xxx mass_erase 0" -c "program $1.elf verify reset exit" >/dev/null &
     openocd_dapLink_pid=$!
     set -e
-  
-
 }
-#----------------------------------------------------------------------------------------------------
+#****************************************************************************************************
 # Function accepts parameters: device, CMSIS-DAP serial #
 function softreset_with_openocd() {
     
@@ -127,9 +128,8 @@ function softreset_with_openocd() {
         # we can add a timeout here if we want
     done
     set -e
-  
 }
-#----------------------------------------------------------------------------------------------------
+#****************************************************************************************************
 # Function accepts parameters:device , CMSIS-DAP serial #
 function erase_with_openocd() {
     printf "> Erasing $1 : $2 \r\n"
@@ -146,11 +146,11 @@ function erase_with_openocd() {
         # wait for openocd to finish
         wait $openocd_dapLink_pid
     fi
-
 }
-#----------------------------------------------------------------------------------------------------
+#****************************************************************************************************
 function run_notConntectedTest() {
-    project_marker
+
+    print_project_banner
     cd $PROJECT_NAME
     set +x
     echo "> Flashing $DUT_NAME_UPPER $PROJECT_NAME"
@@ -171,13 +171,17 @@ function run_notConntectedTest() {
         # update failed test count
         let "numOfFailedTests+=$testResult"
         failedTestList+="| $PROJECT_NAME ($DUT_NAME_UPPER) "
+
+        # save elf of failed test
+        cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/$PROJECT_NAME/build
+        cp $DUT_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$DUT_NAME_LOWER"_"$PROJECT_NAME.elf
     fi
     set -e
 
     # get back to target directory
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER
 }
-#----------------------------------------------------------------------------------------------------
+#****************************************************************************************************
 function flash_bootloader() {
     #------ -----------Build & Flash Bootloader onto Device 2  : ME17
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/Bootloader
@@ -205,52 +209,80 @@ function flash_bootloader() {
         openocd_dapLink_pid=$!
     fi
 }
-#----------------------------------------------------------------------------------------------------
+#****************************************************************************************************
 function erase_all_devices() {
     erase_with_openocd max32655 $DEVICE1
     erase_with_openocd max32655 $DEVICE2
     erase_with_openocd max32665 $DEVICE3
     erase_with_openocd max32690 $DEVICE4
 }
-#----------------------------------------------------------------------------------------------------
-function project_marker() {
+#****************************************************************************************************
+function print_project_banner() {
     echo "=============================================================================="
     printf "> Start of testing $DUT_NAME_LOWER ($DUT_NAME_UPPER) \r\n> ID:$DUT_ID \r\n> Port:$DUT_SERIAL_PORT \r\n\r\n"
 }
-
-#***************************************** Start of test script *************************************
 #****************************************************************************************************
-
-erase_all_devices
-
-# change advertising name for projects under test to avoid
-# connections with office devices
-printf "> changing advertising names : $MSDK_DIR/Examples/$DUT_NAME_UPPER\r\n\r\n"
-if [ $(hostname) == "wall-e" ]; then
+function  change_advertising_names_walle(){
+    # change advertising name for projects under test to avoid
+    # connections with office devices
+    printf "> changing advertising names : $MSDK_DIR/Examples/$DUT_NAME_UPPER\r\n\r\n"
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_dats
     perl -i -pe "s/\'D\'/\'X\'/g" dats_main.c
-
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_datc
     perl -i -pe "s/\'D\'/\'X\'/g" datc_main.c
-
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otac
     perl -i -pe "s/\'S\'/\'X\'/g" datc_main.c
-
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas
     perl -i -pe "s/\'S\'/\'X\'/g" dats_main.c
-
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_FreeRTOS
     perl -i -pe "s/\'S\'/\'X\'/g" dats_main.c
-
     # change advertising name to scan for on the main device client apps
     cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_datc
     perl -i -pe "s/\'D\'/\'X\'/g" datc_main.c
-
     cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac
     perl -i -pe "s/\'S\'/\'X\'/g" datc_main.c
-
     # build BLE examples
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER
+}
+#****************************************************************************************************
+function change_advertising_names_local(){
+    # change advertising name for projects under test to avoid
+    # connections with office devices
+    printf "> changing advertising names : $MSDK_DIR/Examples/$DUT_NAME_UPPER\r\n\r\n"
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_dats
+    perl -i -pe "s/\'D\'/\'Z\'/g" dats_main.c
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_datc
+    perl -i -pe "s/\'D\'/\'Z\'/g" datc_main.c
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otac
+    perl -i -pe "s/\'S\'/\'Z\'/g" datc_main.c
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas
+    perl -i -pe "s/\'S\'/\'Z\'/g" dats_main.c
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_FreeRTOS
+    perl -i -pe "s/\'S\'/\'Z\'/g" dats_main.c
+    # change advertising name to scan for on the main device client apps
+    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_datc
+    perl -i -pe "s/\'D\'/\'Z\'/g" datc_main.c
+    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac
+    perl -i -pe "s/\'S\'/\'Z\'/g" datc_main.c
+}
+
+#****************************************************************************************************
+#***************************************** Start of test script *************************************
+#****************************************************************************************************
+
+# parameterizes all relavent device variables and makes directories to store robot framework log files
+# takes the three args given to the script: eg: max32655 , D3073ICQ , 0409000052fb0cd70000000000000000097969906
+initial_setup $1 $2 $3
+
+# does what it says
+erase_all_devices
+
+# change advertising names to avoid connection with office devices
+# and builds all examples named "BLE" in the directory of the device under test
+if [ $(hostname) == "wall-e" ]; then
+
+    change_advertising_names_walle
+    #build all examples
     SUBDIRS=$(find . -type d -name "BLE*")
     for dir in ${SUBDIRS}; do
         echo "---------------------------------------"
@@ -261,28 +293,8 @@ if [ $(hostname) == "wall-e" ]; then
         make -C ${dir} -j8
     done
 else
- cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_dats
-    perl -i -pe "s/\'D\'/\'Z\'/g" dats_main.c
-
-    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_datc
-    perl -i -pe "s/\'D\'/\'Z\'/g" datc_main.c
-
-    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otac
-    perl -i -pe "s/\'S\'/\'Z\'/g" datc_main.c
-
-    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas
-    perl -i -pe "s/\'S\'/\'Z\'/g" dats_main.c
-
-    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_FreeRTOS
-    perl -i -pe "s/\'S\'/\'Z\'/g" dats_main.c
-
-    # change advertising name to scan for on the main device client apps
-    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_datc
-    perl -i -pe "s/\'D\'/\'Z\'/g" datc_main.c
-
-    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac
-    perl -i -pe "s/\'S\'/\'Z\'/g" datc_main.c
-
+# Allows me to run this script on my local machine with no modifications
+    change_advertising_names_local
     # build BLE examples
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER
     SUBDIRS=$(find . -type d -name "BLE_*")
@@ -296,13 +308,11 @@ else
     done
 fi
 
-# enter current device under test directory
+# enter  device directory
 cd $MSDK_DIR/Examples/$DUT_NAME_UPPER
 
-#no filter would support ALL projects not just ble
+# Iterate through the BLE projects, flash and run robotframework tests
 project_filter='BLE_'
-
-#----------------------------- Run non connected tests
 for dir in ./*/; do
     if [[ "$dir" == *"$project_filter"* ]]; then
 
@@ -357,9 +367,11 @@ for dir in ./*/; do
 
 done # end non connected tests
 
-erase_all_devices
-#--------------------------start Datc/Dats conencted tests
+#****************************************************************************************************
+#*********************************** Start of Datc/s connected tests ********************************
+#****************************************************************************************************
 
+erase_all_devices
 
 # Flash MAIN_DEVICE with BLE_datc
 cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_datc
@@ -376,14 +388,24 @@ printf "> Flashing BLE_dats on DUT $DUT_NAME_UPPER \r\n"
 flash_with_openocd_fast $DUT_NAME_LOWER $DUT_ID 2
 
 cd $EXAMPLE_TEST_PATH/tests
-# runs desired test
+# runs desired test but do not exit on failure, save result to list for printing later
 set +e
+# Robot arguments are: 
+# directory to save log files
+# serial port 1
+# optional serial port 2
+# robot test file
 $ROBOT -d $EXAMPLE_TEST_PATH/results/$DUT_NAME_UPPER/BLE_dat_cs/ -v SERIAL_PORT_1:$MAIN_DEVICE_SERIAL_PORT -v SERIAL_PORT_2:$DUT_SERIAL_PORT BLE_dat_cs.robot
 let "testResult=$?"
 if [ "$testResult" -ne "0" ]; then
     # update failed test count
     let "numOfFailedTests+=$testResult"
     failedTestList+="| BLE_dat_cs ($DUT_NAME_UPPER) "
+    # test failed, save elfs datc/s
+    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_datc/build
+    cp $MAIN_DEVICE_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$MAIN_DEVICE_NAME_LOWER"_BLE_datcs_client.elf"
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_dats/build
+    cp $DUT_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$DUT_NAME_LOWER"_BLE_datcs_server.elf"
 fi
 set -e
 
@@ -391,7 +413,9 @@ set -e
 erase_with_openocd $DUT_NAME_LOWER $DUT_ID
 erase_with_openocd $MAIN_DEVICE_NAME_LOWER $MAIN_DEVICE_ID
 
-#--------------------------start Otac/Otas conencted tests
+#****************************************************************************************************
+#*********************************** Start of OTAC/s connected tests ********************************
+#****************************************************************************************************
 
 cd $EXAMPLE_TEST_PATH/tests
 
@@ -442,6 +466,11 @@ if [ "$testResult" -ne "0" ]; then
     # update failed test count
     let "numOfFailedTests+=$testResult"
     failedTestList+="| BLE_ota_cs ($DUT_NAME_UPPER) "
+    # test failed, save elfs datc/s
+    cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac/build
+    cp $MAIN_DEVICE_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$MAIN_DEVICE_NAME_LOWER"_"$DUT_NAME_LOWER"_BLE_otacs_client.elf"
+    cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas/build
+    cp $DUT_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$DUT_NAME_LOWER"_BLE_otacs_server.elf"
 fi
 set -e
 
