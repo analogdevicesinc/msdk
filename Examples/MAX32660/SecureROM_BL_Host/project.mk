ad747c4f54c58725caa48a480b506816dc2153ad
Sadik.Ozer <sadik.ozer@analog.com>
2022-12-28 12:57:05 +0300

    MSDK-975: Add secure ROM bootloader host example on MAX32660 and MAX32670 MCU This example demonstrate how to communication with secure ROM BL
    Signed-off-by: Sadik.Ozer <sadik.ozer@analog.com>

---
+++ Examples/MAX32660/SecureROM_BL_Host/project.mk
@@ -0,0 +1,18 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://github.com/Analog-Devices-MSDK/VSCode-Maxim/tree/develop#build-configuration
+
+
+# **********************************************************
+
+# Add your config here!
+
+VPATH+=bootloader
+VPATH+=test_images/MAX32520-KIT
+VPATH+=test_images/MAX32651-EVKIT
+
+IPATH+=bootloader
+IPATH+=test_images
