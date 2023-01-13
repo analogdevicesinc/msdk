351a1fa3d51d196138aa9e15574ee6824675ab0c
Ying Cai <ying.cai@analog.com>
2023-01-13 15:40:51 -0600

    Added MAX32690 board WLP_V1.

---
+++ Libraries/Boards/MAX32690/WLP_V1/Include/board.h
@@ -0,0 +1,136 @@
+/******************************************************************************
+ * Copyright (C) 2022 Maxim Integrated Products, Inc., All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Maxim Integrated
+ * Products, Inc. shall not be used except as stated in the Maxim Integrated
+ * Products, Inc. Branding Policy.
+ *
+ * The mere transfer of this software does not imply any licenses
+ * of trade secrets, proprietary technology, copyrights, patents,
+ * trademarks, maskwork rights, or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * ownership rights.
+ *
+ ******************************************************************************/
+
+/**
+ * @file    board.h
+ * @brief   Board support package API.
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifndef LIBRARIES_BOARDS_MAX32690_WLP_V1_INCLUDE_BOARD_H_
+#define LIBRARIES_BOARDS_MAX32690_WLP_V1_INCLUDE_BOARD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef CONSOLE_UART
+#define CONSOLE_UART 1 /// ME18 TO ME17 TEST BOARD
+#endif
+
+#ifndef CONSOLE_BAUD
+#define CONSOLE_BAUD 115200 /// Console baud rate
+#endif
+
+#ifndef HCI_UART
+#define HCI_UART 0  /// ME1 TO ME17 TEST BOARD
+#endif
+
+#ifndef TERMINAL_UART
+#define TERMINAL_UART CONSOLE_UART
+#endif
+
+#ifndef USER_UART
+#define USER_UART 1
+#endif
+
+#define LED_OFF 1 /// Inactive state of LEDs
+#define LED_ON 0 /// Active state of LEDs
+
+#ifndef EXT_FLASH_BAUD
+#define EXT_FLASH_BAUD 4000000
+#endif
+
+#define WLP_V1 /// Used in examples to control program flow
+
+/**
+ *  A reference to LED1 (RED LED in the RGB LED) of the board.
+ *  Can be used with the LED_On, LED_Off, and LED_Toggle functions.
+ */
+#define LED1 0
+#define LED_RED LED1
+
+/**
+ *  A reference to LED2 (GREEN LED in the RGB LED) of the board.
+ *  Can be used with the LED_On, LED_Off, and LED_Toggle functions.
+ */
+#define LED2 1
+#define LED_GREEN LED2
+
+/**
+ * \brief   Initialize the BSP and board interfaces.
+ * \returns #E_NO_ERROR if everything is successful
+ */
+int Board_Init(void);
+
+/**
+ * \brief   Initialize or reinitialize the console. This may be necessary if the
+ *          system clock rate is changed.
+ * \returns #E_NO_ERROR if everything is successful
+ */
+int Console_Init(void);
+
+/**
+ * \brief   Shutdown the console.
+ * \returns #E_NO_ERROR if everything is successful
+ */
+int Console_Shutdown(void);
+
+/**
+ * \brief   Attempt to prepare the console for sleep.
+ * \returns #E_NO_ERROR if ready to sleep, #E_BUSY if not ready for sleep.
+ */
+int Console_PrepForSleep(void);
+
+/*
+ * @brief Initialize the SPI interface used to communicate with the TFT Controller
+ */
+void TFT_SPI_Init(void);
+
+/*
+ * @brief Send command or parameter information to TFT controller
+ *
+ * @param datain    Buffer containing data to send to TFT controller
+ * @param count     Number of bytes to send
+ * @param data      True - data in datain are data/parameter bytes, False - data in datain
+ *                         is a command
+ */
+void TFT_SPI_Write(uint8_t *datain, uint32_t count, bool data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBRARIES_BOARDS_MAX32690_WLP_V1_INCLUDE_BOARD_H_
---
+++ Libraries/Boards/MAX32690/WLP_V1/Source/board.c
@@ -0,0 +1,235 @@
+/******************************************************************************
+ * Copyright (C) 2022 Maxim Integrated Products, Inc., All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Maxim Integrated
+ * Products, Inc. shall not be used except as stated in the Maxim Integrated
+ * Products, Inc. Branding Policy.
+ *
+ * The mere transfer of this software does not imply any licenses
+ * of trade secrets, proprietary technology, copyrights, patents,
+ * trademarks, maskwork rights, or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * ownership rights.
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include "board.h"
+#include "Ext_Flash.h"
+#include "gpio.h"
+#include "led.h"
+#include "mxc_assert.h"
+#include "mxc_delay.h"
+#include "mxc_device.h"
+#include "mxc_pins.h"
+#include "mxc_sys.h"
+#include "pb.h"
+#include "spixf.h"
+#include "tft_st7735.h"
+#include "uart.h"
+
+/***** Global Variables *****/
+mxc_uart_regs_t *ConsoleUart = MXC_UART_GET_UART(CONSOLE_UART);
+extern uint32_t SystemCoreClock;
+
+const mxc_gpio_cfg_t pb_pin[] = {
+    { MXC_GPIO4, MXC_GPIO_PIN_0, MXC_GPIO_FUNC_IN, MXC_GPIO_PAD_PULL_UP, MXC_GPIO_VSSEL_VDDIOH },
+};
+const unsigned int num_pbs = (sizeof(pb_pin) / sizeof(mxc_gpio_cfg_t));
+
+const mxc_gpio_cfg_t led_pin[] = {
+    { MXC_GPIO0, MXC_GPIO_PIN_14, MXC_GPIO_FUNC_OUT, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO },
+    { MXC_GPIO2, MXC_GPIO_PIN_12, MXC_GPIO_FUNC_OUT, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO },
+    /* Logical LEDs for Bluetooth debug signals */
+    { MXC_GPIO0, MXC_GPIO_PIN_7, MXC_GPIO_FUNC_OUT, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO },
+    { MXC_GPIO0, MXC_GPIO_PIN_8, MXC_GPIO_FUNC_OUT, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO },
+    { MXC_GPIO0, MXC_GPIO_PIN_9, MXC_GPIO_FUNC_OUT, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO },
+};
+const unsigned int num_leds = (sizeof(led_pin) / sizeof(mxc_gpio_cfg_t));
+
+const mxc_gpio_cfg_t tft_mosi = (mxc_gpio_cfg_t){ .port = MXC_GPIO2,
+                                                  .mask = MXC_GPIO_PIN_24,
+                                                  .func = MXC_GPIO_FUNC_OUT,
+                                                  .pad = MXC_GPIO_PAD_NONE,
+                                                  .vssel = MXC_GPIO_VSSEL_VDDIOH };
+const mxc_gpio_cfg_t tft_clk = (mxc_gpio_cfg_t){ .port = MXC_GPIO2,
+                                                 .mask = MXC_GPIO_PIN_25,
+                                                 .func = MXC_GPIO_FUNC_OUT,
+                                                 .pad = MXC_GPIO_PAD_NONE,
+                                                 .vssel = MXC_GPIO_VSSEL_VDDIOH };
+const mxc_gpio_cfg_t tft_cs = (mxc_gpio_cfg_t){ .port = MXC_GPIO2,
+                                                .mask = MXC_GPIO_PIN_11,
+                                                .func = MXC_GPIO_FUNC_OUT,
+                                                .pad = MXC_GPIO_PAD_NONE,
+                                                .vssel = MXC_GPIO_VSSEL_VDDIOH };
+
+/******************************************************************************/
+void mxc_assert(const char *expr, const char *file, int line)
+{
+#ifdef DEBUG
+    printf("MXC_ASSERT %s #%d: (%s)\n", file, line, expr);
+#endif
+
+    while (1) {}
+}
+
+/******************************************************************************/
+int Board_Init(void)
+{
+    int err;
+
+    // Enable GPIO
+    MXC_SYS_ClockEnable(MXC_SYS_PERIPH_CLOCK_GPIO0);
+    MXC_SYS_ClockEnable(MXC_SYS_PERIPH_CLOCK_GPIO1);
+    MXC_SYS_ClockEnable(MXC_SYS_PERIPH_CLOCK_GPIO2);
+    MXC_SYS_ClockEnable(MXC_SYS_PERIPH_CLOCK_GPIO3);
+
+    if ((err = Console_Init()) < E_NO_ERROR) {
+        return err;
+    }
+
+    if ((err = PB_Init()) != E_NO_ERROR) {
+        MXC_ASSERT_FAIL();
+        return err;
+    }
+
+    if ((err = LED_Init()) != E_NO_ERROR) {
+        MXC_ASSERT_FAIL();
+        return err;
+    }
+
+    return E_NO_ERROR;
+}
+
+/******************************************************************************/
+int Console_Init(void)
+{
+    int err;
+
+    if ((err = MXC_UART_Init(ConsoleUart, CONSOLE_BAUD, MXC_UART_IBRO_CLK)) != E_NO_ERROR) {
+        return err;
+    }
+
+    return E_NO_ERROR;
+}
+
+/******************************************************************************/
+int Console_Shutdown(void)
+{
+    int err;
+
+    if ((err = MXC_UART_Shutdown(ConsoleUart)) != E_NO_ERROR) {
+        return err;
+    }
+
+    return E_NO_ERROR;
+}
+
+/******************************************************************************/
+int Console_PrepForSleep(void)
+{
+    return MXC_UART_ReadyForSleep(ConsoleUart);
+}
+
+/******************************************************************************/
+void TFT_SPI_Init(void)
+{
+    // The TFT SPI on the MAX32690 EV Kit is bit-banged so there's no need to initialize
+    // a SPI instance. Instead, we only need to configure the GPIO pins which are used to
+    // connect to the SPI bus.
+
+    // Configure GPIO pins
+    MXC_GPIO_Config(&tft_mosi);
+    MXC_GPIO_Config(&tft_clk);
+    MXC_GPIO_Config(&tft_cs);
+
+    // Set chip select and clock lines high (idle state)
+    MXC_GPIO_OutSet(tft_cs.port, tft_cs.mask);
+    MXC_GPIO_OutSet(tft_clk.port, tft_clk.mask);
+}
+
+/******************************************************************************/
+void TFT_SPI_Write(uint8_t *datain, uint32_t count, bool data)
+{
+    // The TFT SPI on the MAX32690 EV Kit is not connected to a SPI instance so it
+    // must be bit-banged.
+
+    uint8_t tx_byte;
+
+    if (datain == NULL) {
+        return;
+    }
+
+    MXC_GPIO_OutClr(tft_cs.port, tft_cs.mask); // Assert chip select (active low)
+    MXC_Delay(10);
+
+    for (int i = 0; i < count; i++) {
+        tx_byte = datain[i];
+
+        for (int j = 0; j < 9; j++) { // 9 Bits per character
+            tft_clk.port->out_clr = tft_clk.mask; //Clk low
+
+            if (j == 0) { //Send D/CX bit before MSB
+                if (data) {
+                    tft_mosi.port->out_set = tft_mosi.mask;
+                } else {
+                    tft_mosi.port->out_clr = tft_mosi.mask;
+                }
+            } else {
+                if (tx_byte & 0x80) { //Shift out data MSB first
+                    tft_mosi.port->out_set = tft_mosi.mask;
+                } else {
+                    tft_mosi.port->out_clr = tft_mosi.mask;
+                }
+                tx_byte = tx_byte << 1;
+            }
+
+            for (int k = 0; k < 2; k++) {}
+
+            tft_clk.port->out_set = tft_clk.mask; // Clk high
+
+            for (int k = 0; k < 2; k++) {}
+        }
+    }
+
+    MXC_Delay(10);
+    MXC_GPIO_OutSet(tft_cs.port, tft_cs.mask); // De-assert chip select (active low)
+
+    return;
+}
+
+/******************************************************************************/
+__weak void NMI_Handler(void)
+{
+#ifdef DEBUG
+    printf("NMI Handler\n");
+#endif
+    while (1) {}
+}
+
+/******************************************************************************/
+__weak void HardFault_Handler(void)
+{
+#ifdef DEBUG
+    printf("HardFault_Handler\n");
+#endif
+    while (1) {}
+}
