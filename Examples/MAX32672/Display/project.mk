9fe5093a841e4ea37f6b2799b91cf9fd42a0ba4a
Sadik.Ozer <sadik.ozer@analog.com>
2023-02-03 16:51:32 +0300

    Update example to use Libraries/LVGL library instead of static one in the example
    Signed-off-by: Sadik.Ozer <sadik.ozer@analog.com>

---
+++ Examples/MAX32672/Display/project.mk
@@ -0,0 +1,21 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://analog-devices-msdk.github.io/msdk/USERGUIDE/#build-system
+
+# **********************************************************
+
+# Add your config here!
+
+# If you have secure version of MCU, set SBT=1 to generate signed binary
+# For more information on how sing process works, see
+# https://www.analog.com/en/education/education-library/videos/6313214207112.html
+SBT=0
+
+
+# Enable LVGL library
+LIB_LVGL = 1
+
+SRCS += st7735s_drv.c
