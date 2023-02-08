b4548edbbd7584d43bf115d57b3e1cf0f435a371
Scheiffler <Jacob.Scheiffler@analog.com>
2023-02-08 11:01:14 -0600

    Removing Coremark Library and examples from clang format checks.

--- .github/workflows/clang-format-run.sh
+++ .github/workflows/clang-format-run.sh
@@ -3,7 +3,7 @@
 CLANG_VERSION=14
 
 # Find the C files
-CFILES=$(find . -iname "*.c" -not -name "*cnn.c" -a -not -name "*softmax.c" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\)/.*")
+CFILES=$(find . -iname "*.c" -not -name "*cnn.c" -a -not -name "*softmax.c" -a -not -regex ".*/Examples/.*/Coremark" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\|Coremark\)/.*")
 
 for c_file in ${CFILES}
 do
@@ -20,7 +20,7 @@ done
 
 
 # Reformat all of the header files
-HFILES=$(find . -iname "*.h" -not -name "*regs*" -a -not -name "*weights.h" -a -not -name "*cnn.h" -a -not -name "*sampledata.h" -a -not -name "*sampleoutput.h" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\)/.*")
+HFILES=$(find . -iname "*.h" -not -name "*regs*" -a -not -name "*weights.h" -a -not -name "*cnn.h" -a -not -name "*sampledata.h" -a -not -name "*sampleoutput.h" -a -not -regex ".*/Examples/.*/Coremark" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\|Coremark\)/.*")
 for h_file in ${HFILES}
 do
   clang-format-${CLANG_VERSION} --verbose -style=file -i ${h_file}
