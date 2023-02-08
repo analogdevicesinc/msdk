b9462af665bc3a4cf1c4ed7738b943d7b0c9e2ba
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2023-02-08 16:22:37 -0600

    PeriphDrivers/Source: Updated copyright notices to 2023.

--- Libraries/PeriphDrivers/Source/SYS/sys_me14.c
+++ Libraries/PeriphDrivers/Source/SYS/sys_me14.c
@@ -1,5 +1,5 @@
-/* ****************************************************************************
- * Copyright (C) 2016 Maxim Integrated Products, Inc., All Rights Reserved.
+/******************************************************************************
+ * Copyright (C) 2023 Maxim Integrated Products, Inc., All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -29,10 +29,7 @@
  * property whatsoever. Maxim Integrated Products, Inc. retains all
  * ownership rights.
  *
- * $Date: 2020-01-17 08:24:56 -0600 (Fri, 17 Jan 2020) $
- * $Revision: 50770 $
- *
- *************************************************************************** */
+ ******************************************************************************/
 
 /**
  * @file mxc_sys.c
