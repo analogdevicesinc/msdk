5d26fba50ed09b501f14b8a2a59f1f3012d9dbcc
kevin-gillespie <kevin.gillespie@maximintegrated.com>
2023-02-14 08:31:57 -0600

    Running on ubuntu-latest

--- .github/workflows/clang-format-run.sh
+++ .github/workflows/clang-format-run.sh
@@ -1,27 +1,50 @@
 #!/bin/bash
 
+# Run this from the root of the msdk
+# bash -ex .github/workflows/clang-format-run.sh
+# 
+# Optional arguments to include the changes file
+# bash -ex .github/workflows/clang-format-run.sh main.c src/foo.c
+
 CLANG_VERSION=14
+CHANGES_NEEDED=0
+FILES=""
 
-# Find the C files
-CFILES=$(find . -iname "*.c" -not -name "*cnn.c" -a -not -name "*softmax.c" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\)/.*")
+if [[ $# -eq 0 ]]
+then
+  # Find the c files
+  FILES=$(find . -iname "*.c" -not -name "*cnn.c" -a -not -name "*softmax.c" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\)/.*")
 
-for c_file in ${CFILES}
+  # Find the header files
+  FILES=$FILES+$(find . -iname "*.h" -not -name "*regs*" -a -not -name "*weights.h" -a -not -name "*cnn.h" -a -not -name "*sampledata.h" -a -not -name "*sampleoutput.h" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\)/.*")
+else
+  # Accumulate the input arguments into FILES
+  FILES="$*"
+fi
+
+for file in ${FILES}
 do
+  # Determine if we need to make changes
+  set +e
+  clang-format-${CLANG_VERSION} --dry-run --Werror -style=file --verbose ${file}
+  RETVAL=$?
+  set -e
+
+  if [[ $RETVAL -ne 0 ]]
+  then
+    CHANGES_NEEDED=1
+  fi
+
   # Format the files, this will turn while(1); into while(1)\n;
-  clang-format-${CLANG_VERSION} --verbose -style=file -i ${c_file}
+  clang-format-${CLANG_VERSION} --verbose -style=file -i ${file}
 
   # Remove single line ';' and replace with "{}"
-  perl -i -pe 's/\s+;\s/{}\n/' ${c_file}
+  perl -i -pe 's/\s+;\s/{}\n/' ${file}
 
   # Re-format the files
-  clang-format-${CLANG_VERSION} --verbose -style=file -i ${c_file}
+  clang-format-${CLANG_VERSION} --verbose -style=file -i ${file}
 
 done
 
-
-# Reformat all of the header files
-HFILES=$(find . -iname "*.h" -not -name "*regs*" -a -not -name "*weights.h" -a -not -name "*cnn.h" -a -not -name "*sampledata.h" -a -not -name "*sampleoutput.h" -a -not -regex ".*/Libraries/\(Cordio\|FCL\|FreeRTOS\|FreeRTOS\-Plus\|LC3\|littlefs\|lwIP\|MAXUSB\|SDHC\|LVGL\)/.*")
-for h_file in ${HFILES}
-do
-  clang-format-${CLANG_VERSION} --verbose -style=file -i ${h_file}
-done
+# Test if files have changed
+exit $CHANGES_NEEDED
