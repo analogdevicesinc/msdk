5e385fed371db2315a32028f8111b3d2802a941e
Kevin Gillespie <Kevin.Gillespie@analog.com>
2023-02-15 10:53:12 -0600

    Adding numPackets parameter.

--- Tools/Bluetooth/dtm_sweep_vs.py
+++ Tools/Bluetooth/dtm_sweep_vs.py
@@ -49,6 +49,7 @@ from mini_RCDAT_USB import mini_RCDAT_USB
 from BLE_hci import BLE_hci
 from BLE_hci import Namespace
 from termcolor import colored
+import math
 
 verbose=True
 
@@ -72,6 +73,62 @@ def printError(msg):
     printTrace('Error', msg, TRACE_ERROR, 'red')
 
 
+LL_CRC_LEN =                  3   # CRC length.
+LL_AA_LEN =                   4   # Access address length.
+LL_PREAMBLE_LEN_1M =          1   # Preamble length (LE 1M PHY)
+LL_PREAMBLE_LEN_2M =          2   # Preamble length (LE 2M PHY)
+LL_PREAMBLE_LEN_CODED_BITS =  10  # Preamble length (LE Coded PHY)
+LL_CI_LEN_BITS =              2   # Coding indicator length (LE Coded PHY)
+LL_TERM1_LEN_BITS =           3   # TERM1 length (LE Coded PHY)
+LL_TERM2_LEN_BITS =           3   # TERM2 length (LE Coded PHY)
+LL_BLE_BIT_PER_US =           1   # BLE PHY rate
+LL_BLE_US_PER_BYTE_1M =       8   # BLE PHY speed (LE 1M PHY)
+LL_BLE_US_PER_BYTE_2M =       4   # BLE PHY speed (LE 2M PHY)
+LL_BLE_US_PER_BYTE_CODED_S8 = 64  # BLE PHY speed (LE Coded PHY, S=8)
+LL_BLE_US_PER_BIT_CODED_S8 =  8   # BLE PHY speed (LE Coded PHY, S=8)
+LL_BLE_US_PER_BYTE_CODED_S2 = 16  # BLE PHY speed (LE Coded PHY, S=2)
+LL_BLE_US_PER_BIT_CODED_S2 =  2   # BLE PHY speed (LE Coded PHY, S=2)
+LL_DTM_HDR_LEN =              2   # Direct Test Mode PDU header length
+
+# Calculate the duration of the test
+def calcTestTime(packetLen, phy, numPackets):
+
+    packetLen=int(packetLen)
+    phy=int(phy)
+    numPackets=int(numPackets)
+    totalTime = 0
+
+    # 1: 1M
+    # 2: 2M
+    # 3: S8
+    # 4: S2
+
+    # Calculate the length of each packet
+    if (phy == 3 or phy == 4):
+        totalTime = (LL_PREAMBLE_LEN_CODED_BITS + (LL_AA_LEN * 8) + LL_CI_LEN_BITS + LL_TERM1_LEN_BITS) * LL_BLE_US_PER_BIT_CODED_S8
+        if (phy == 4):
+            totalTime = totalTime + ((LL_DTM_HDR_LEN + packetLen + LL_CRC_LEN) * LL_BLE_US_PER_BYTE_CODED_S2) + (LL_TERM2_LEN_BITS * LL_BLE_US_PER_BIT_CODED_S2)
+        else:
+            totalTime = totalTime + ((LL_DTM_HDR_LEN + packetLen + LL_CRC_LEN) * LL_BLE_US_PER_BYTE_CODED_S8) + (LL_TERM2_LEN_BITS * LL_BLE_US_PER_BIT_CODED_S8)
+
+    elif (phy == 2):
+        totalTime = (LL_PREAMBLE_LEN_2M + LL_AA_LEN + LL_DTM_HDR_LEN + packetLen + LL_CRC_LEN) * LL_BLE_US_PER_BYTE_2M
+    else:
+        totalTime = (LL_PREAMBLE_LEN_1M + LL_AA_LEN + LL_DTM_HDR_LEN + packetLen + LL_CRC_LEN) * LL_BLE_US_PER_BYTE_1M
+
+    # Add the inter frame spacing
+    totalTime = math.ceil((totalTime + 249) / 625) * 625
+
+    # Multiply by the number of packets we're sending
+    totalTime = totalTime * numPackets
+
+    # Add a constant 10 ms
+    totalTime = totalTime + 10000
+
+    return totalTime
+
+
+
 # Setup the command line description text
 descText = """
 Direct Test Mode Sweep
@@ -93,6 +150,7 @@ parser.add_argument('slaveSerial',help='Serial port for slave device')
 parser.add_argument('masterSerial',help='Serial port for master device')
 parser.add_argument('results',help='CSV files to store the results')
 parser.add_argument('-d', '--delay', default=5,help='Number of seconds to wait before ending the test')
+parser.add_argument('-n', '--numPackets', default=0,help='Number of packets to send per test')
 parser.add_argument('-l', '--limit', default=0,help='PER limit for return value')
 parser.add_argument('-p', '--phys', default="1",help='PHYs to test with, comma separated list with 1-4.')
 parser.add_argument('-t', '--txpows', default="0",help='TX powers to test with, comma separated list.')
@@ -105,6 +163,7 @@ print(args)
 packetLengths    = [250]
 phys             = args.phys.strip().split(",")
 txPowers         = args.txpows.strip().split(",")
+numPackets       = args.numPackets
 
 if(args.attens == None):
     attens = list(range(20,90,10))
@@ -113,6 +172,9 @@ if(args.attens == None):
     attens.append(90)
 else:
     attens = args.attens.strip().split(",")
+
+if(args.disable_atten):
+    attens=[0]
 
 if args.disable_atten is not None:
     disableAttenuator = True
@@ -123,6 +185,7 @@ print("slaveSerial   :",args.slaveSerial)
 print("masterSerial  :",args.masterSerial)
 print("results       :",args.results)
 print("delay         :",args.delay)
+print("numPackets    :",numPackets)
 print("packetLengths :",packetLengths)
 print("phys          :",phys)
 print("attens        :",attens)
@@ -135,13 +198,14 @@ results.write("# slaveSerial   : "+str(args.slaveSerial)+"\n")
 results.write("# masterSerial  : "+str(args.masterSerial)+"\n")
 results.write("# results       : "+str(args.results)+"\n")
 results.write("# delay         : "+str(args.delay)+"\n")
+results.write("# numPackets    : "+str(numPackets)+"\n")
 results.write("# packetLengths : "+str(packetLengths)+"\n")
 results.write("# phys          : "+str(phys)+"\n")
 results.write("# attens        : "+str(attens)+"\n")
 results.write("# PER limit     : "+str(args.limit)+"\n")
 
 # Write the header line
-results.write("packetLen,phy,atten,txPower,perMaster,perSlave\n")
+results.write("packetLen,phy,atten,txPower,perSlave\n")
 
 # Create the BLE_hci objects
 hciSlave = BLE_hci(Namespace(serialPort=args.slaveSerial, monPort="", baud=115200))
@@ -149,37 +213,22 @@ hciMaster = BLE_hci(Namespace(serialPort=args.masterSerial, monPort="", baud=115
 
 perMax = 0
 
-for packetLen,phy,txPower in itertools.product(packetLengths,phys,txPowers):
+# Reset the devices
+hciSlave.resetFunc(None)
+hciMaster.resetFunc(None)
+sleep(0.1)
 
-    # Reset the devices
-    hciSlave.resetFunc(None)
-    hciMaster.resetFunc(None)
-    sleep(0.1)
+for packetLen,phy,txPower in itertools.product(packetLengths,phys,txPowers):
 
     # # Reset the attenuation
     if not disableAttenuator:
         mini_RCDAT = mini_RCDAT_USB(Namespace(atten=30))
         sleep(0.1)
-
-
-    printInfo('Setting Data Length')
-    hciSlave.dataLenFunc(None)
-    hciMaster.dataLenFunc(None)
-
-
     
-
-    printInfo('Setting PHY')
-    # Set the PHY
-    hciMaster.phyFunc(Namespace(phy=str(phy)))
-
-
-
     # Set the TX Power
     printInfo('Setting TX Power')
     hciSlave.txPowerFunc(Namespace(power=txPower, handle="0"))
     hciMaster.txPowerFunc(Namespace(power=txPower, handle="0"))
-
 
     for atten in attens:
         print(packetLen," ",phy," ",atten," ",txPower)
@@ -189,39 +238,46 @@ for packetLen,phy,txPower in itertools.product(packetLengths,phys,txPowers):
             mini_RCDAT = mini_RCDAT_USB(Namespace(atten=atten))
             sleep(0.1)
         
-
-
         #start the test
-        hciSlave.rxTestFunc(Namespace(channel=0, phy=1))
-        hciMaster.txTestFunc(Namespace(channel=0, phy=1, payload=0,packetLength=0))
+        hciSlave.rxTestFunc(Namespace(channel=0, phy=phy))
+        hciMaster.txTestVSFunc(Namespace(channel=0, phy=phy,payload=0,packetLength=packetLen,numPackets=numPackets))
         
-        sleep(int(args.delay))
+        if(numPackets == 0):
+            sleep(int(args.delay))
+        else:
+            # Sleep based on the amount of time it takes to complete the test
+            # Convert us to seconds
+            sleep(calcTestTime(packetLen, phy, numPackets) / 1000000)
         
         packtesReceived = hciSlave.endTestFunc(Namespace(noPrint=True))
         stats = hciMaster.endTestVSFunc(Namespace(noPrint=True))
-        
+
         packetsTransmitted = 0
-
-        if stats is not None:
-            packetsTransmitted = stats['txData']    
         
-        if packetsTransmitted != 0:
-            perSlave = round(100 * (1 - packtesReceived / packetsTransmitted), 2)
+        if(numPackets == 0):
+            if stats is not None:
+                packetsTransmitted = stats['txData']
+
+            if packetsTransmitted != 0:
+                perSlave = round(100 * (1 - packtesReceived / packetsTransmitted), 2)
             
+            else:
+                printWarning('End Test stats returned invalid data. (Packets Transmitted = 0) PER rate being set to 100')
+                perSlave = 100
         else:
-            printWarning('Connection stats returned invalid data. (Packets Transmitted = 0) PER rate being set to 100')
-            perSlave = 100
-        
+            perSlave = round(100 * (1 - packtesReceived / int(numPackets)), 2)
 
-        #PER cannot be defined for the master with this scheme currently so setting to zero
-        perMaster = 0    
+        if(packtesReceived == 0):
+            printWarning('Did not receive any packets')
+
+        if(perSlave >= 50.0):
+            printWarning('Unusually high PER ', perSlave)
     
         if(perSlave > perMax):
             perMax = perSlave
-            
 
         # Save the results to file
-        results.write(str(packetLen)+","+str(phy)+",-"+str(atten)+","+str(txPower)+","+str(perMaster)+","+str(perSlave)+"\n")
+        results.write(str(packetLen)+","+str(phy)+",-"+str(atten)+","+str(txPower)+","+str(perSlave)+"\n")
 
 # Reset the devices
 hciSlave.resetFunc(None)
