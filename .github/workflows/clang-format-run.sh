de79f7725358143abb8a0415e33cedaba49cb54b
Kevin.Gillespie <Kevin.Gillespie@maximintegrated.com>
2023-02-17 18:27:38 -0600

    Adding diff, optimizing flow.

--- .github/workflows/clang-format-run.sh
+++ .github/workflows/clang-format-run.sh
@@ -8,7 +8,7 @@
 
 # Get CLANG_VERSION from environment if available
 if [[ $CLANG_VERSION == "" ]]; then
-CLANG_VERSION=14
+  CLANG_VERSION=14
 fi
 
 CHANGES_NEEDED=0
@@ -28,26 +28,40 @@ fi
 
 for file in ${FILES}
 do
-  # Determine if we need to make changes
-  set +e
-  clang-format-${CLANG_VERSION} --dry-run --Werror -style=file --verbose ${file}
-  RETVAL=$?
-  set -e
-
-  if [[ $RETVAL -ne 0 ]]
+  if [ -f ${file} ];
   then
-    CHANGES_NEEDED=1
+
+    set +e
+    clang-format-${CLANG_VERSION} --verbose --style=file -n -Werror ${file}
+    RETVAL=$?
+    set -e
+
+    if [ $RETVAL != 0 ];
+    then
+      echo "===================================================="
+
+      # Format the files, this will turn while(1); into while(1)\n;
+      clang-format-${CLANG_VERSION} -style=file ${file} > ${file}.clang
+
+      # Remove single line ';' and replace with "{}"
+      perl -i -pe 's/\s+;\s/{}\n/' ${file}.clang
+
+      # Re-format the files
+      clang-format-${CLANG_VERSION} -style=file -i ${file}.clang
+
+      # Print the diff, for when this is run in a check
+      set +e
+      diff -u --color=always ${file} ${file}.clang
+      set -e
+
+      # Replace the temp file
+      mv ${file}.clang ${file}
+
+      CHANGES_NEEDED=1
+      echo "===================================================="
+      echo ""
+    fi
   fi
-
-  # Format the files, this will turn while(1); into while(1)\n;
-  clang-format-${CLANG_VERSION} --verbose -style=file -i ${file}
-
-  # Remove single line ';' and replace with "{}"
-  perl -i -pe 's/\s+;\s/{}\n/' ${file}
-
-  # Re-format the files
-  clang-format-${CLANG_VERSION} --verbose -style=file -i ${file}
-
 done
 
 # Test if files have changed
