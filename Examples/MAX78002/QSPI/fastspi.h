71588c46a75a62ac1cdf006140cdaa65233a8fcf
Jake Carter <jake.carter@analog.com>
2023-02-22 16:54:29 -0600

    Add TODOs

--- Examples/MAX78002/QSPI/fastspi.h
+++ Examples/MAX78002/QSPI/fastspi.h
@@ -3,5 +3,7 @@
 static volatile bool g_tx_done = 0;
 static volatile bool g_rx_done = 0;
 
+
+// TODO:  Generalize to multiple SPI instances
 int spi_init();
 int spi_transmit(uint8_t *src, uint32_t txlen, uint8_t *dest, uint32_t rxlen, bool deassert, bool use_dma, bool block);
