f664084805c83e039b7fda77ce769d6c2a14f13e
kevin-gillespie <kevin.gillespie@maximintegrated.com>
2023-02-22 10:11:16 -0600

    Cordio dtm schedule (#421)
    * added phy to gitignore
    
    * Adding option to use DTM_RX_SCHEDULING.
    
    * Disabling DTM_RX_SCHEDULING by default.
    
    Enabling for all maxim platforms.
    
    * Only scheduing the DTM RX on RX success.
    
    * Fixing build error.
    
    * Fixing bug with interval banding.
    
    * Converting S2(4) to S8/Coded(3).
    
    The RX cannot be pre-set for S2 or S8. It is enabled as just coded
    and then switches to the appropriate PHY once the initial portion of
    the packet has been received.
    
    Calling this function with phy==4 will generate an error.
    
    * Adding numPackets parameter.
    
    * Working around timer driver race condition.
    
    * Clearning buf-free field before calling WsfBufFree.
    
    * fixed bug in dtm sweep
    
    * updated attenuation step to be optional
    
    * updated attenuation step to be optional
    
    * updated attenuation step to be optional
    
    * Clearing buf->free field.
    
    ---------
    
    Co-authored-by: EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>

--- Libraries/Cordio/controller/sources/ble/ll/ll_main_dtm.c
+++ Libraries/Cordio/controller/sources/ble/ll/ll_main_dtm.c
@@ -109,7 +109,7 @@ static struct
  *  \return     Packet length in microseconds.
  */
 /*************************************************************************************************/
-static uint32_t llCalcPacketInterval(uint8_t len, uint8_t phy, uint8_t phyOptions)
+uint32_t llCalcPacketTime(uint8_t len, uint8_t phy, uint8_t phyOptions)
 {
   unsigned int totalTime;
 
@@ -136,6 +136,24 @@ static uint32_t llCalcPacketInterval(uint8_t len, uint8_t phy, uint8_t phyOption
       totalTime = (LL_PREAMBLE_LEN_1M + LL_AA_LEN + LL_DTM_HDR_LEN + len + LL_CRC_LEN) * LL_BLE_US_PER_BYTE_1M;
       break;
   }
+
+  return totalTime;
+}
+
+/*************************************************************************************************/
+/*!
+ *  \brief      Calculate packet interval.
+ *
+ *  \param      len         Packet length.
+ *  \param      phy         PHY.
+ *  \param      phyOptions  PHY options.
+ *
+ *  \return     Interval between packets in microseconds.
+ */
+/*************************************************************************************************/
+uint32_t llCalcPacketInterval(uint8_t len, uint8_t phy, uint8_t phyOptions)
+{
+  unsigned int totalTime = llCalcPacketTime(len, phy, phyOptions);
 
   /* ceil((L + 249) / 625) * 625 */
   return LL_MATH_DIV_625(totalTime + 249 + 624) * 625;
@@ -412,6 +430,7 @@ uint8_t LlEnhancedTxTest(uint8_t rfChan, uint8_t len, uint8_t pktType, uint8_t p
   WSF_CS_INIT();
 
   LL_TRACE_INFO3("### LlApi ###  LlTxTest, rfChan=%u, len=%u, pktType=%u", rfChan, len, pktType);
+  LL_TRACE_INFO1("  numPkt=%u", numPkt);
 
   if ((LL_API_PARAM_CHECK == 1) &&
       (!((llTestCb.state == LL_TEST_STATE_IDLE) || (llTestCb.state == LL_TEST_STATE_TX)) ||
@@ -777,7 +796,7 @@ static bool_t llTestRxComplete(BbOpDesc_t *pBod, uint8_t status)
 /*************************************************************************************************/
 uint8_t LlEnhancedRxTest(uint8_t rfChan, uint8_t phy, uint8_t modIdx, uint16_t numPkt)
 {
-  LL_TRACE_INFO1("### LlApi ###  LlRxTest, rfChan=%u", rfChan);
+  LL_TRACE_INFO2("### LlApi ###  LlRxTest, rfChan=%u phy=%u", rfChan, phy);
 
   if ((LL_API_PARAM_CHECK == 1) &&
       ((llTestCb.state != LL_TEST_STATE_IDLE) ||
