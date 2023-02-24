135fce2222657853554a77aed46a2c8d626c0b5a
Scheiffler <Jacob.Scheiffler@analog.com>
2023-02-24 08:03:48 -0600

    Adding comments to Coremark example project.mk's.

--- Examples/MAX32690/Coremark/project.mk
+++ Examples/MAX32690/Coremark/project.mk
@@ -9,6 +9,7 @@
 
 # Add your config here!
 
+# Include Coremark library in search paths.
 IPATH+=$(MAXIM_PATH)/Libraries/Coremark
 VPATH+=$(MAXIM_PATH)/Libraries/Coremark
 
@@ -19,16 +20,25 @@ PROJ_CFLAGS+=-fgcse-sm
 PROJ_CFLAGS+=-fgcse-las
 PROJ_CFLAGS+=-finline-functions
 PROJ_CFLAGS+=-finline-limit=1000
-PROJ_CFLAGS+=-DPERFORMANCE_RUN=1
-PROJ_CFLAGS+=-DITERATIONS=4000
+
+# CoreMark build flags
+PROJ_CFLAGS+=-DPERFORMANCE_RUN=1  # Run the CoreMark test with performance parameters
+PROJ_CFLAGS+=-DITERATIONS=4000    # Number of times the coremark test is executed
 
 MFLOAT_ABI=soft
 
+# To comply with the Coremark rules, these source files
+# must be unmodified. They are located in the Coremark
+# library.
 SRCS+=core_main.c
 SRCS+=core_list_join.c
 SRCS+=core_matrix.c
 SRCS+=core_state.c
 SRCS+=core_util.c
+
+# This is the source file can be modified and has
+# already been set up for the MAX32xxx and MAX78xxx
+# series microcontrollers.
 SRCS+=core_portme.c
 
 AUTOSEARCH=0
