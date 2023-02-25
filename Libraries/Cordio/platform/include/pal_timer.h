75c322786411997cf54db45d389bae7049361bea
Eddie <62710807+EdwinFairchild@users.noreply.github.com>
2023-02-25 01:48:25 -0500

    prioritize HCI UART to avoid RX overrun (#419)
    * prioritize HCI UART to avoid RX overrun
    
    * clang format
    
    * adds function to set pal timer IRQ priority, get HCI IRQn from definition
    
    * clang format
    
    * remove unused macros
    
    * add BLE5_ctr changes to me14 me18
    
    * clang format
    
    ---------
    
    Co-authored-by: eddie <eddie@edwinfairchild.com>

--- Libraries/Cordio/platform/include/pal_timer.h
+++ Libraries/Cordio/platform/include/pal_timer.h
@@ -70,7 +70,7 @@ uint32_t PalTimerGetCurrentTime(void);
 uint32_t PalTimerGetExpTime(void);
 void PalTimerSleep(uint32_t expUsec);
 void PalTimerRestore(uint32_t schTime);
-
+void PalTimerSetIRQPriority(uint32_t priority);
 /*! \} */    /* PAL_TIMER */
 
 #ifdef __cplusplus
