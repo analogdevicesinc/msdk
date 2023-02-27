10386c34e4b59d67032ac835bf2fdc78e042f1b7
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2023-02-27 16:33:19 -0600

    TMR RevB: Implemented Set/Get ClockSourceFreq functions. (#454)
    * [MSDK-1036] AI85/ME17: I2C2 RX/TX DMA added to Read/Write DMA functions.
    
    * [MSDK-1029] AI85: Removed unsupported UART HFC pins.
    
    * [MSDK-941] AI85: Added UART3 DMA Transaction Option.
    
    * [MSDK-941] GPIO: Reversed Interrupt Polarity for level-triggered mode.
    
    * [MSDK-1026] AI85: Added GPIO2 (LPGPIO) support.
    
    * [MSDK-1023] TMR: AI85: Save selected clock source frequencies for GetTime.
    
    * [MSDK-1035] Added I2C HS Mode support.
    
    * clang-format bot reformatting.
    
    * Requested changes: Removed leftover prints and renamed a few things.
    
    * I2C: ME17: Renamed HS_MODE to HIGH_SPEED.
    
    * TMR: Implemented Get/Set ClockSourceFreq functions for all Timer RevB supported parts.
    
    * ME12 TMR: Missed parameter.
    
    * TMR: Publicly removed Set/Get ClockSourceFreq functions.
    
    * Clang-formatted locally.
    
    * clang-formatting locally.
    
    ---------
    
    Co-authored-by: sihyung-maxim <sihyung-maxim@users.noreply.github.com>

--- Libraries/PeriphDrivers/Source/TMR/tmr_ai87.c
+++ Libraries/PeriphDrivers/Source/TMR/tmr_ai87.c
@@ -57,6 +57,7 @@ int MXC_TMR_Init(mxc_tmr_regs_t *tmr, mxc_tmr_cfg_t *cfg, bool init_pins)
 
         clockSource = MXC_TMR_CLK1;
         MXC_SYS_ClockSourceEnable(MXC_SYS_CLOCK_ISO);
+        MXC_TMR_RevB_SetClockSourceFreq((mxc_tmr_revb_regs_t *)tmr, ISO_FREQ);
         break;
 
     case MXC_TMR_8M_CLK:
@@ -66,6 +67,7 @@ int MXC_TMR_Init(mxc_tmr_regs_t *tmr, mxc_tmr_cfg_t *cfg, bool init_pins)
 
         clockSource = MXC_TMR_CLK2;
         MXC_SYS_ClockSourceEnable(MXC_SYS_CLOCK_IBRO);
+        MXC_TMR_RevB_SetClockSourceFreq((mxc_tmr_revb_regs_t *)tmr, IBRO_FREQ);
         break;
 
     case MXC_TMR_32K_CLK:
@@ -78,6 +80,7 @@ int MXC_TMR_Init(mxc_tmr_regs_t *tmr, mxc_tmr_cfg_t *cfg, bool init_pins)
         }
 
         MXC_SYS_ClockSourceEnable(MXC_SYS_CLOCK_ERTCO);
+        MXC_TMR_RevB_SetClockSourceFreq((mxc_tmr_revb_regs_t *)tmr, ERTCO_FREQ);
         break;
 
     case MXC_TMR_8K_CLK:
@@ -87,9 +90,11 @@ int MXC_TMR_Init(mxc_tmr_regs_t *tmr, mxc_tmr_cfg_t *cfg, bool init_pins)
 
         clockSource = MXC_TMR_CLK2;
         MXC_SYS_ClockSourceEnable(MXC_SYS_CLOCK_INRO);
+        MXC_TMR_RevB_SetClockSourceFreq((mxc_tmr_revb_regs_t *)tmr, INRO_FREQ);
         break;
 
     default:
+        MXC_TMR_RevB_SetClockSourceFreq((mxc_tmr_revb_regs_t *)tmr, PeripheralClock);
         break;
     }
 
