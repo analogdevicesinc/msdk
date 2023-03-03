1203544e372cc0839305f5da8679f5f13f963e5c
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2023-03-03 16:04:34 -0600

    small hex merge tool

---
+++ Tools/misc/hexmerge.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python3
+
+################################################################################
+ # Copyright (C) 2020 Maxim Integrated Products, Inc., All Rights Reserved.
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a
+ # copy of this software and associated documentation files (the "Software"),
+ # to deal in the Software without restriction, including without limitation
+ # the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ # and/or sell copies of the Software, and to permit persons to whom the
+ # Software is furnished to do so, subject to the following conditions:
+ #
+ # The above copyright notice and this permission notice shall be included
+ # in all copies or substantial portions of the Software.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ # IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+ # OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ # OTHER DEALINGS IN THE SOFTWARE.
+ #
+ # Except as contained in this notice, the name of Maxim Integrated
+ # Products, Inc. shall not be used except as stated in the Maxim Integrated
+ # Products, Inc. Branding Policy.
+ #
+ # The mere transfer of this software does not imply any licenses
+ # of trade secrets, proprietary technology, copyrights, patents,
+ # trademarks, maskwork rights, or any other form of intellectual
+ # property whatsoever. Maxim Integrated Products, Inc. retains all
+ # ownership rights.
+ #
+ ###############################################################################
+
+## dtm_sweep.py
+ #
+ # Sweep connection parameters.
+ #
+ # Ensure that both targets are built with BT_VER := 9
+ #
+
+import sys
+import argparse
+from argparse import RawTextHelpFormatter
+from time import sleep
+import itertools
+from intelhex import IntelHex
+
+
+
+# Setup the command line description text
+descText = """
+    Merge Two Hex files together to make one super hex
+"""
+
+# Parse the command line arguments
+parser = argparse.ArgumentParser(description=descText, formatter_class=RawTextHelpFormatter)
+parser.add_argument('hex1',help='Hex File 1')
+parser.add_argument('hex2',help='Hex File 2')
+parser.add_argument('-o','--output',default='merged.hex')
+args = parser.parse_args()
+
+print(args)
+
+
+
+original = IntelHex(args.hex1)
+new = IntelHex(args.hex2)
+original.merge(new, overlap='replace')
+
+original.write_hex_file(args.output)
