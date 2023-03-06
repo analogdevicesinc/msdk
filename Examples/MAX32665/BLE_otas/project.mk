939dafafc0a6af8f01d692e4d63df6b4f0c421a8
abby wolf <Abigail.Wolf@analog.com>
2023-03-06 12:11:38 -0600

    add automatic loading of bootloader for MAX32665

--- Examples/MAX32665/BLE_otas/project.mk
+++ Examples/MAX32665/BLE_otas/project.mk
@@ -32,6 +32,8 @@ SRCS += stack_dats.c
 SRCS += dats_main.c
 SRCS += main.c
 
+PROJ_CFLAGS += -Wl,--section-start=.text=0x10004000
+
 USE_INTERNAL_FLASH ?=0
 ifeq ($(USE_INTERNAL_FLASH), 1)
 LINKERFILE = ota_internal_mem.ld
@@ -40,3 +42,27 @@ else
 LINKERFILE = ota_external_mem.ld
 SRCS += wdxs_file_ext.c
 endif
+
+# build bootloader
+BOOTLOADER_DIR=../Bootloader
+
+BUILD_DIR := $(abspath ./build)
+BOOTLOADER_BUILD_DIR := $(BUILD_DIR)/buildbl
+
+BOOTLOADER_BIN=$(BOOTLOADER_BUILD_DIR)/bootloader.bin
+BOOTLOADER_OBJ=$(BOOTLOADER_BUILD_DIR)/bootloader.o
+
+PROJ_OBJS = ${BOOTLOADER_OBJ}
+
+.PHONY: bl_bin
+bl_bin: $(BOOTLOADER_BIN)
+
+${BOOTLOADER_BIN}:
+	$(MAKE) -C ${BOOTLOADER_DIR} BUILD_DIR=$(BOOTLOADER_BUILD_DIR) PROJECT=bootloader
+	$(MAKE) -C $(BOOTLOADER_DIR) BUILD_DIR=$(BOOTLOADER_BUILD_DIR) $(BOOTLOADER_BIN)
+
+.PHONY: bl_obj
+bl_obj: $(BOOTLOADER_OBJ)
+
+${BOOTLOADER_OBJ}: bl_build.S ${BOOTLOADER_BIN}
+	${CC} ${AFLAGS} -o ${@} -c bl_build.S 
