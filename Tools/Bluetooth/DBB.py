bbbd269af970d56bbbaaf61edb018350f48adb3c
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2023-03-24 11:18:21 -0500

    added more functionality

--- Tools/Bluetooth/DBB.py
+++ Tools/Bluetooth/DBB.py
@@ -63,15 +63,58 @@ class CtrlReg:
     aes_ctr_blk = 0x0120
 
 
-class Spim:
-    def __init__(self, pyOcdTarget):
-        pass
-
-    def read(self, address):
-        pass
-
-    def write(self, address):
-        pass
+class RffeReg:
+    rffe_ifc_version = 0x0000
+    rffe_ifc_rffe_version = 0x0002
+    general_param = 0x0004
+    general_delay_freq = 0x0006
+    general_delay_amp = 0x0008
+    general_spi_invert_csn = 0x000a
+    general_skip_sdmod = 0x000c
+    tx_seq_ena_seq_lngth_total = 0x0010
+    tx_seq_ena_seq_lngth_part = 0x0012
+    tx_seq_ena_cmd = 0x0014
+    tx_seq_dis_seq_lngth_total = 0x0034
+    tx_seq_dis_seq_lngth_part = 0x0036
+    tx_seq_dis_cmd = 0x0038
+    tx_seq_spi = 0x0058
+    rx_seq_ena_seq_lngth_total = 0x005c
+    rx_seq_ena_seq_lngth_part = 0x005e
+    rx_seq_ena_cmd = 0x0060
+    rx_seq_dis_seq_lngth_total = 0x0080
+    rx_seq_dis_seq_lngth_part = 0x0082
+    rx_seq_dis_cmd = 0x0084
+    rx_seq_spi = 0x00a4
+    rffe_spim_cfg = 0x00a8
+    rffe_spim_data_out = 0x00aa
+    rffe_spim_start_transaction = 0x00ac
+    rffe_spim_data_in = 0x00ae
+    agc_spi_cfg = 0x00b0
+    agc_enc5_gain_addr = 0x00b4
+    agc_enc5_gain_table = 0x00b6
+    agc_enc5_gain_table_db = 0x00c6
+    agc_enc5_ready_tmr = 0x00d6
+    agc_enc5_offs_i_addr = 0x00d8
+    agc_enc5_offs_q_addr = 0x00da
+    agc_enc5_dc_offs = 0x00dc
+    agc_enc5_offs_i_bypass = 0x00de
+    agc_enc5_offs_q_bypass = 0x00ee
+    agc_enc5_curr_dc_offs_i = 0x00fe
+    agc_enc5_curr_dc_offs_q = 0x010e
+    rsv_0x11e = 0x11e
+    general2_skip_fir_amp = 0x0120
+    general2_fir_coefficients = 0x0122
+    general2_fir_scalingfactor = 0x012a
+    anti_alias_am_skip_fir = 0x012c
+    anti_alias_am_coeff = 0x012e
+    anti_alias_am_scalingfactor = 0x0136
+    anti_alias_fm_skip_fir = 0x0138
+    anti_alias_fm_coeff = 0x013a
+    anti_alias_fm_scalingfactor = 0x0142
+    cdc_clk_div_en = 0x0144
+    rsv_0x146_0x163 = 0x146
+    iq_ctrl = 0x0164
+    iq_data = 0x0168
 
 
 class DBB:
@@ -87,15 +130,15 @@ class DBB:
 
     def __init__(self, pyOcdTarget):
         self.target = pyOcdTarget
-        self.spim = Spim(self.target)
 
     def read8(self, addr):
         return self.target.read8(addr)
-    def read16(self,addr):
+
+    def read16(self, addr):
         return self.target.read16(addr)
-    def read32(self,addr):
+
+    def read32(self, addr):
         return self.target.read32(addr)
-            
 
     def readRange8(self, baseAddr, len):
         data = []
@@ -115,15 +158,32 @@ class DBB:
             data.append(self.target.read16(baseAddr + i))
         return data
 
-    def write8(self, addr,value):
+    def write8(self, addr, value):
         self.target.write8(addr, value)
 
-    def write16(self, addr,value):
+    def write16(self, addr, value):
         self.target.write8(addr, value)
 
-    def write32(self, addr,value):
+    def write32(self, addr, value):
         self.target.write8(addr, value)
 
+    def read(self, addr, size=0x1):
+        if size == self.RegSize8:
+            return self.read8(addr)
+        elif size == self.RegSize16:
+            return self.read16(addr)
+        elif size == self.RegSize32:
+            return self.read32(addr)
+    def write(self,addr,value):
+
+        if value <= 255:
+            self.write8(addr, value)
+        elif value <= 65535:
+            self.write16(addr,value)
+        else:
+            self.write32(addr,value)
+
+    
     def readRegions(self, baseAddr, offsetLut: dict):
         """
         Reads multiple regions give region map 
@@ -158,12 +218,89 @@ class DBB:
 
         return regions
 
+
+
+    def spim_setup(self):
+        cpha = 0
+        cpol = 1
+        endianess = 0
+        clk_rate = 2
+
+        spi_setup = (
+            (clk_rate << 0) |
+            (endianess << 4) |
+            (cpol << 5) |
+            (cpha << 6) |
+            1 << 7 |
+            0)
+
+        self.write8(self.rffeAddr(RffeReg.rffe_spim_cfg), spi_setup)
+        self.write8(self.rffeAddr(RffeReg.tx_seq_spi), spi_setup)
+        self.write8(self.rffeAddr(RffeReg.rx_seq_spi), spi_setup)
+        self.write8(self.rffeAddr(RffeReg.agc_spi_cfg), spi_setup)
+
+
+    def spim_write(self, addr, value):
+
+        self.enableRffePmu(True)
+
+        while not (self.readCtrl(CtrlReg.gen_pmu_status, self.RegSize16) & (1 << 2)):
+            pass
+        
+        for i in range(0xf):
+            # wait a little extra
+            pass
+
+        addr |= (1 << 7)
+
+        self.writeCtrl(CtrlReg.events_status, (1 << 5))
+        self.writeRffe(RffeReg.rffe_spim_data_out, (addr << 8) + value)
+        self.writeRffe(RffeReg.rffe_spim_start_transaction, (1 << 0))
+
+        while not (self.readCtrl(CtrlReg.events_status) & (1 << 5),self.RegSize16):
+            pass
+        
+
+        self.enableRffePmu(False)
+
+        
+
+    def spim_read(self, addr):
+        self.enableRffePmu(True)
+
+        while not (self.readCtrl(CtrlReg.gen_pmu_status, self.RegSize16) & (1 << 2)):
+            pass
+
+        for i in range(0xf):
+            # wait a little extra
+            pass
+        
+        addr &= ~(1 << 7)
+        
+        
+        self.writeCtrl(CtrlReg.events_status, (1 << 5))
+        self.writeRffe(RffeReg.rffe_spim_data_out, (addr << 8) + 0)
+        self.writeRffe(RffeReg.rffe_spim_start_transaction, (1 << 0))
+
+        
+        
+        while not self.readCtrl(CtrlReg.events_status,self.RegSize16) & (1 << 5):
+            pass
+      
+
+        self.enableRffePmu(False)
+
+        return self.readRffe(RffeReg.rffe_spim_data_in, self.RegSize8)
+
     def ctrlAddr(self, offset):
         return self.CtrlAddrBase + offset
+
     def rxAddr(self, offset):
         return self.RxBaseAddr + offset
+
     def txAddr(self, offset):
         return self.TxBaseAddr + offset
+
     def rffeAddr(self, offset):
         return self.RffeBaseAddr + offset
 
@@ -174,6 +311,16 @@ class DBB:
         version_major = (version >> 8) * 0xff
 
         return (version_major, version_minor)
+
+    def readCtrl(self,register,size):
+        return self.read(self.ctrlAddr(register),size)
+    def writeCtrl(self,register,value):
+        self.write(self.ctrlAddr(register),value)
+    def readRffe(self, register,size):
+        return self.read(self.rffeAddr(register),size)
+    def writeRffe(self,register,value):
+        self.write(self.rffeAddr(register), value)
+
 
     def getCtrlAll(self):
         len0 = 0x96
@@ -266,9 +413,14 @@ class DBB:
 
     def enableRffePmu(self, enable=True):
         if enable:
-            self.write16(self.ctrlAddr(CtrlReg.rffe_pmu_ctrl),(1 << 1))
+            self.write16(self.ctrlAddr(CtrlReg.rffe_pmu_ctrl), (1 << 1))
         else:
             self.write16(self.ctrlAddr(CtrlReg.rffe_pmu_ctrl), (1 << 2))
-
+    def getPmuStatus(self):
+        return self.readCtrl(CtrlReg.gen_pmu_status, self.RegSize32)
+    def txPmuIsEnable(self):
+        return bool(self.getPmuStatus() & 0x1)
     def rffePmuIsEnabled(self):
-        return self.read16(self.ctrlAddr(CtrlReg.rffe_pmu_ctrl)) & (1 << 1)
+        return bool(self.getPmuStatus() & (1 << 1))
+    def rxPmuIsEnabled(self):
+        return bool(self.getPmuStatus() & (1 << 2))
