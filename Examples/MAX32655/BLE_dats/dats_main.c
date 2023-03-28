12d8cb1ed5ae6a4c9cc0adc7d0d4b47440f4c9f5
Eddie <eddie.amaya@analog.com>
2023-03-28 09:47:30 -0500

    string terminate pointer buffer since were treating it as string

--- Examples/MAX32655/BLE_dats/dats_main.c
+++ Examples/MAX32655/BLE_dats/dats_main.c
@@ -423,7 +423,10 @@ uint8_t datsWpWriteCback(dmConnId_t connId, uint16_t handle, uint8_t operation, 
                          uint16_t len, uint8_t *pValue, attsAttr_t *pAttr)
 {
     if (len < 64) {
+        // Terminate string
+        pValue[len] = '\0';
         /* print received data if not a speed test message */
+        pValue[len] = '\0';
         APP_TRACE_INFO0((const char *)pValue);
 
         /* send back some data */
