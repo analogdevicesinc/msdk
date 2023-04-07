233c7dd9eee7e1e213266ce803eefe90ab571173
Tahsin Türker Mutlugün <Tahsin.Mutlugun@analog.com>
2023-04-07 20:37:18 +0300

    Add Examples for Building and Using Static Libraries (#476)
    - Added Library_Generate (build project as a static library) and Library_Use (link an external static library) examples.

---
+++ Examples/MAX32572/Library_Generate/gpiolib.c
@@ -0,0 +1,61 @@
+/**
+ * @file    gpiolib
+ * @brief   Example static library implementation.
+ * @details GPIO library source.
+ */
+
+/******************************************************************************
+ * Copyright (C) 2023 Maxim Integrated Products, Inc., All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Maxim Integrated
+ * Products, Inc. shall not be used except as stated in the Maxim Integrated
+ * Products, Inc. Branding Policy.
+ *
+ * The mere transfer of this software does not imply any licenses
+ * of trade secrets, proprietary technology, copyrights, patents,
+ * trademarks, maskwork rights, or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * ownership rights.
+ *
+ ******************************************************************************/
+
+#include "mxc_errors.h"
+#include "gpio.h"
+
+void gpio_set(const mxc_gpio_cfg_t *gpio)
+{
+    if (gpio->func == MXC_GPIO_FUNC_OUT)
+        MXC_GPIO_OutSet(gpio->port, gpio->mask);
+}
+
+void gpio_clear(const mxc_gpio_cfg_t *gpio)
+{
+    if (gpio->func == MXC_GPIO_FUNC_OUT)
+        MXC_GPIO_OutClr(gpio->port, gpio->mask);
+}
+
+int gpio_get(const mxc_gpio_cfg_t *gpio)
+{
+    if (gpio->func == MXC_GPIO_FUNC_IN)
+        return MXC_GPIO_InGet(gpio->port, gpio->mask);
+
+    return MXC_GPIO_OutGet(gpio->port, gpio->mask);
+}
