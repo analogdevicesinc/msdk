202bf9e7e4bcca5baadfccf0bac6c211dadaa505
Sadik Ozer <46590392+ozersa@users.noreply.github.com>
2023-04-10 23:22:07 +0300

    MAX32675:  Add Pin Definitions for I2C1 (#544)
    Signed-off-by: Sadik.Ozer <sadik.ozer@analog.com>

--- Libraries/PeriphDrivers/Source/SYS/pins_me16.c
+++ Libraries/PeriphDrivers/Source/SYS/pins_me16.c
@@ -48,7 +48,9 @@ const mxc_gpio_cfg_t gpio_cfg_extclk = { MXC_GPIO0, (MXC_GPIO_PIN_10), MXC_GPIO_
 const mxc_gpio_cfg_t gpio_cfg_i2c0 = { MXC_GPIO0, (MXC_GPIO_PIN_6 | MXC_GPIO_PIN_7),
                                        MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_PULL_UP,
                                        MXC_GPIO_VSSEL_VDDIO };
-// NOTE: I2C1 pins inaccessible
+const mxc_gpio_cfg_t gpio_cfg_i2c1 = { MXC_GPIO0, (MXC_GPIO_PIN_12 | MXC_GPIO_PIN_13),
+                                       MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_PULL_UP,
+                                       MXC_GPIO_VSSEL_VDDIO };
 const mxc_gpio_cfg_t gpio_cfg_i2c2 = { MXC_GPIO0, (MXC_GPIO_PIN_18 | MXC_GPIO_PIN_19),
                                        MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_PULL_UP,
                                        MXC_GPIO_VSSEL_VDDIO };
