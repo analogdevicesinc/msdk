f1e5896de9191bd94f334ee3af9417e503e1a5f8
Bobby <bobby@bobbycounts.com>
2023-04-11 15:49:36 -0500

    Fix clang formating

--- Libraries/PeriphDrivers/Source/GPIO/gpio_reva.c
+++ Libraries/PeriphDrivers/Source/GPIO/gpio_reva.c
@@ -155,7 +155,7 @@ int MXC_GPIO_RevA_SetAF(mxc_gpio_reva_regs_t *port, mxc_gpio_func_t func, uint32
 {
     //This is required for new devices going forward.
     port->inen |= mask;
-    
+
     //Switch to I/O mode first
     port->en0_set = mask;
 
