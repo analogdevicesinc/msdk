d892205701c094799688bf17761da32e21290bf6
Eddie <eddie.amaya@analog.com>
2023-04-21 09:53:49 -0500

    Update external flash erase elfs and internal flash erase script

--- .github/workflows/scripts/mass_erase_board.sh
+++ .github/workflows/scripts/mass_erase_board.sh
@@ -23,6 +23,30 @@ function erase_with_openocd() {
         wait $openocd_dapLink_pid
     fi
 }
+
+#****************************************************************************************************
+# Function accepts parameters: device, CMSIS-DAP serial #
+function flash_with_openocd() {
+    # mass erase and flash
+    set +e
+    $OPENOCD -f $OPENOCD_TCL_PATH/interface/cmsis-dap.cfg -f $OPENOCD_TCL_PATH/target/$1.cfg -s $OPENOCD_TCL_PATH -c "cmsis_dap_serial  $2" -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -c "init; reset halt;max32xxx mass_erase 0" -c "program ext_flc_erase_$1.elf verify reset exit" >/dev/null &
+    openocd_dapLink_pid=$!
+    # wait for openocd to finish
+    while kill -0 $openocd_dapLink_pid; do
+        sleep 1
+        # we can add a timeout here if we want
+    done
+    set -e
+
+    # Check the return value to see if we received an error
+    if [ "$?" -ne "0" ]; then
+        printf "> Verify failed , flashibng again \r\n"
+        # Reprogram the device if the verify failed
+        $OPENOCD -f $OPENOCD_TCL_PATH/interface/cmsis-dap.cfg -f $OPENOCD_TCL_PATH/target/$1.cfg -s $OPENOCD_TCL_PATH -c "cmsis_dap_serial  $2" -c "gdb_port 3333" -c "telnet_port 4444" -c "tcl_port 6666" -c "init; reset halt;max32xxx mass_erase 0" -c "program ext_flc_erase_$1.elf verify reset exit" >/dev/null &
+        openocd_dapLink_pid=$!
+    fi
+}
+
 if [ $(hostname) == "wall-e" ]; then
     FILE=/home/$USER/Workspace/Resource_Share/boards_config.json
     # WALL-E  paths
@@ -40,6 +64,13 @@ fi
 dut_serial=$(/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['$2']['daplink'])")
 
 #  flash device with an application to mass erase external flash
-
+if [[ $1 != "max32690" ]]; then
+    # flash device with an application to mass erase external flash
+    echo "Flashing ext flash erase application on $1 with ID:$dut_serial"
+    flash_with_openocd $1 $dut_serial
+    echo "External flash erased"
+fi
+sleep 2
 # mass erase internal flash
 erase_with_openocd $1 $dut_serial
+echo "Internal flash erased"
