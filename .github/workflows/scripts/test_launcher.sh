db89edc32c029e0ea01f0a4a80830586bff3f7cc
Eddie <eddie.amaya@analog.com>
2023-04-21 15:22:04 -0500

    Remove necessary fi

--- .github/workflows/scripts/test_launcher.sh
+++ .github/workflows/scripts/test_launcher.sh
@@ -407,9 +407,8 @@ function run_all_not_conencted_tests() {
 
     done # end non connected tests
 
-
-
 }
+
 #****************************************************************************************************
 function run_datcs_conencted_tests() {
     echo
@@ -486,17 +485,17 @@ function run_ota_test() {
     fi
     echo "****************************************************************************************************"
     # ME18 evkit does not have external flash
-    
+
     # #make sure all files have correct settings
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas
     git restore .
-    
+
     cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/Bootloader
     git restore .
 
     # change advertising names
     change_advertising_names
-    
+
     # change the target the OTAC embedded firmware in the client is built for
     cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac
     #appends TARGET , TARGET_UC and TARGET_LC to the make commands and sets them to $DUT_NAME_UPPER and $DUT_NAME_LOWER
@@ -559,18 +558,18 @@ function run_ota_test() {
         let "numOfFailedTests+=$testResult"
         if [[ "$INTERNAL_FLASH_TEST" -ne "0" ]]; then
             failedTestList+="| BLE_ota_cs_int ($DUT_NAME_UPPER) "
-             # test failed, save elfs datc/s
+            # test failed, save elfs datc/s
             cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac/build
             cp $MAIN_DEVICE_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$MAIN_DEVICE_NAME_LOWER"_"$DUT_NAME_LOWER"_BLE_otacs_client_int.elf"
             cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas/build
             cp $DUT_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$DUT_NAME_LOWER"_BLE_otacs_server_int.elf"
         else
             failedTestList+="| BLE_ota_cs_ext ($DUT_NAME_UPPER) "
-             # test failed, save elfs datc/s
+            # test failed, save elfs datc/s
             cd $MSDK_DIR/Examples/$MAIN_DEVICE_NAME_UPPER/BLE_otac/build
             cp $MAIN_DEVICE_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$MAIN_DEVICE_NAME_LOWER"_"$DUT_NAME_LOWER"_BLE_otacs_client_ext.elf"
             cd $MSDK_DIR/Examples/$DUT_NAME_UPPER/BLE_otas/build
-            cp $DUT_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$DUT_NAME_LOWER"_BLE_otacs_server_ext.elf"    
+            cp $DUT_NAME_LOWER.elf $EXAMPLE_TEST_PATH/results/failed_elfs/$DUT_NAME_LOWER"_BLE_otacs_server_ext.elf"
         fi
 
     fi
@@ -580,9 +579,8 @@ function run_ota_test() {
     erase_with_openocd $DUT_NAME_LOWER $DUT_ID
     #   erase_with_openocd $MAIN_DEVICE_NAME_LOWER $MAIN_DEVICE_ID
 
-
     erase_all_devices
-   
+
 }
 
 #****************************************************************************************************
@@ -604,7 +602,6 @@ git checkout ME17B1-new
 cd MAX32655/build/gcc
 make clean
 make -j
-
 
 if [ $CURRENT_TEST == "all" ]; then
     echo
@@ -637,7 +634,7 @@ elif [ $CURRENT_TEST == "ota" ]; then
     # run_ota_test 1 # arg 1 = internal flash
     # if [[ $DUT_NAME_UPPER != "MAX32690" ]]; then
     # run_ota_test 0 # arg 0= external flash
-    fi
+    #fi
     echo
 else
     echo
@@ -645,7 +642,6 @@ else
     run_single_not_conencted_tests $CURRENT_TEST
     echo
 fi
-
 
 if [ $CURRENT_TEST == "all" ]; then
     echo "=============================================================================="
@@ -662,5 +658,3 @@ if [ $CURRENT_TEST == "all" ]; then
     echo
 fi
 exit $numOfFailedTests
-
-
