8143c0f17aba3ef8e9577700675b972666a8ec56
sanjayjaroli <109093637+sanjayjaroli@users.noreply.github.com>
2023-04-24 13:54:55 -0700

    Updated 2023 Copyright Notices for MAXUSB

--- Libraries/MAXUSB/include/dbg_log/dbg_log.h
+++ Libraries/MAXUSB/include/dbg_log/dbg_log.h
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright (C) 2016 Maxim Integrated Products, Inc., All Rights Reserved.
+/******************************************************************************
+ * Copyright (C) 2023 Maxim Integrated Products, Inc., All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -29,8 +29,7 @@
  * property whatsoever. Maxim Integrated Products, Inc. retains all
  * ownership rights.
  *
- *******************************************************************************
- */
+ ******************************************************************************/
 
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
