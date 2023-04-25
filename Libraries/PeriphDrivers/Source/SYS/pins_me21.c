61b323179959e0047dbfe6dae068557b9b025fda
sanjayjaroli <109093637+sanjayjaroli@users.noreply.github.com>
2023-04-25 00:11:26 -0700

    fixed ss2 and clang

--- Libraries/PeriphDrivers/Source/SYS/pins_me21.c
+++ Libraries/PeriphDrivers/Source/SYS/pins_me21.c
@@ -90,26 +90,22 @@ const mxc_gpio_cfg_t gpio_cfg_i2s0 = {
 };
 
 const mxc_gpio_cfg_t gpio_cfg_spi0 = {
-    MXC_GPIO0, (MXC_GPIO_PIN_2 | MXC_GPIO_PIN_3 | MXC_GPIO_PIN_4),
+    MXC_GPIO0, (MXC_GPIO_PIN_2 | MXC_GPIO_PIN_3 | MXC_GPIO_PIN_4 | MXC_GPIO_PIN_5),
     MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_NONE
 };
 
 //SPI chip selected defined separately below
-const mxc_gpio_cfg_t gpio_cfg_spi0_ss0 = {
-    MXC_GPIO0, (MXC_GPIO_PIN_5), MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_NONE
-};
+const mxc_gpio_cfg_t gpio_cfg_spi0_ss0 = { MXC_GPIO0, MXC_GPIO_PIN_5, MXC_GPIO_FUNC_ALT1,
+                                           MXC_GPIO_PAD_NONE };
 
-const mxc_gpio_cfg_t gpio_cfg_spi0_ss1 = {
-    MXC_GPIO0, (MXC_GPIO_PIN_6), MXC_GPIO_FUNC_ALT3, MXC_GPIO_PAD_NONE
-};
+const mxc_gpio_cfg_t gpio_cfg_spi0_ss1 = { MXC_GPIO0, MXC_GPIO_PIN_6, MXC_GPIO_FUNC_ALT3,
+                                           MXC_GPIO_PAD_NONE };
 
-const mxc_gpio_cfg_t gpio_cfg_spi0_ss2 = {
-    MXC_GPIO0, (MXC_GPIO_PIN_7), MXC_GPIO_FUNC_ALT3, MXC_GPIO_PAD_NONE
-};
+const mxc_gpio_cfg_t gpio_cfg_spi0_ss2 = { MXC_GPIO0, MXC_GPIO_PIN_7, MXC_GPIO_FUNC_ALT3,
+                                           MXC_GPIO_PAD_NONE };
 
-const mxc_gpio_cfg_t gpio_cfg_spi0_ss2 = {
-    MXC_GPIO0, (MXC_GPIO_PIN_23), MXC_GPIO_FUNC_ALT3, MXC_GPIO_PAD_NONE
-};
+const mxc_gpio_cfg_t gpio_cfg_spi0_ss3 = { MXC_GPIO0, MXC_GPIO_PIN_23, MXC_GPIO_FUNC_ALT3,
+                                           MXC_GPIO_PAD_NONE };
 
 // NOTE: SPI1 defined here with SS0 only
 const mxc_gpio_cfg_t gpio_cfg_spi1 = {
