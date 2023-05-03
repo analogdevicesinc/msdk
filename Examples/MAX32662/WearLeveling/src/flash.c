086dde4d06e1ea5244acd5371bd85debeae83f2c
Scheiffler <Jacob.Scheiffler@analog.com>
2023-05-03 13:57:26 -0500

    MAX32662: Updating WearLeveling example to match latest version.

--- Examples/MAX32662/WearLeveling/src/flash.c
+++ Examples/MAX32662/WearLeveling/src/flash.c
@@ -44,36 +44,48 @@
 #include "gcr_regs.h"
 
 /***** Functions *****/
+static int flash_write4(uint32_t startaddr, uint32_t length, uint32_t *data, bool verify);
 
+//******************************************************************************
 int flash_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer,
                lfs_size_t size)
 {
     uint32_t first_block = *(uint32_t *)c->context;
     uint32_t startaddr = MXC_FLASH_PAGE_ADDR((first_block + block)) + off;
     uint8_t *data = (uint8_t *)buffer;
+
+    // Copy data from flash into the data buffer
     for (uint8_t *ptr = (uint8_t *)startaddr; ptr < (uint8_t *)(startaddr + size); ptr++, data++) {
         *data = *ptr;
     }
+
     return LFS_ERR_OK;
 }
-//******************************************************************************
 
+//******************************************************************************
 int flash_write(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer,
                 lfs_size_t size)
 {
     uint32_t first_block = *(uint32_t *)c->context;
+
+    // Get starting address of the write
     uint32_t startaddr = MXC_FLASH_PAGE_ADDR((first_block + block)) + off;
     uint32_t *data = (uint32_t *)buffer;
+
+    // Write data to flash 4 bytes at a time
     return flash_write4(startaddr, size / c->prog_size, data, FALSE);
 }
 
 //******************************************************************************
-
 int flash_erase(const struct lfs_config *c, lfs_block_t block)
 {
     uint32_t first_block = *(uint32_t *)c->context;
+
+    // Get address of filesystem block
     int addr = MXC_FLASH_PAGE_ADDR((first_block + block));
     LOGF("Erasing page at address %08x\n", addr);
+
+    // Erase filesystem block
     int error_status = MXC_FLC_PageErase(addr);
     if (error_status != E_NO_ERROR) {
         return error_status;
@@ -82,7 +94,6 @@ int flash_erase(const struct lfs_config *c, lfs_block_t block)
 }
 
 //******************************************************************************
-
 int flash_sync(const struct lfs_config *c)
 {
     // Not provided by the SDK
@@ -90,11 +101,11 @@ int flash_sync(const struct lfs_config *c)
 }
 
 //******************************************************************************
-
 int flash_verify(uint32_t address, uint32_t length, uint8_t *data)
 {
     volatile uint8_t *ptr;
 
+    // Scan through section of flash and check if it matches the data buffer
     for (ptr = (uint8_t *)address; ptr < (uint8_t *)(address + length); ptr++, data++) {
         if (*ptr != *data) {
             printf("Verify failed at 0x%x (0x%x != 0x%x)\n", (unsigned int)ptr, (unsigned int)*ptr,
@@ -107,11 +118,11 @@ int flash_verify(uint32_t address, uint32_t length, uint8_t *data)
 }
 
 //******************************************************************************
-
 int check_mem(uint32_t startaddr, uint32_t length, uint32_t data)
 {
     uint32_t *ptr;
 
+    // Scan section of flash to see if it matches the expected value
     for (ptr = (uint32_t *)startaddr; ptr < (uint32_t *)(startaddr + length); ptr++) {
         if (*ptr != data) {
             return 0;
@@ -122,9 +133,9 @@ int check_mem(uint32_t startaddr, uint32_t length, uint32_t data)
 }
 
 //******************************************************************************
-
 int check_erased(uint32_t startaddr, uint32_t length)
 {
+    // Scan through section of flash to see if it matches the erased value (0xFFFFFFFF)
     return check_mem(startaddr, length, 0xFFFFFFFF);
 }
 
@@ -135,6 +146,7 @@ int flash_write4(uint32_t startaddr, uint32_t length, uint32_t *data, bool verif
 
     MXC_ICC_Disable();
 
+    // Write data to flash 4 bytes at a time
     for (uint32_t testaddr = startaddr; i < length; testaddr += 4) {
         // Write a word
         int error_status = MXC_FLC_Write32(testaddr, data[i]);
