2dfdff46d06ad4b3fe5fe41e1a98f8ecc8e82d26
Jake Carter <jake.carter@analog.com>
2023-05-03 18:02:28 -0500

    Github web editor ftw

--- Examples/MAX32690/BLE_fcc/main.c
+++ Examples/MAX32690/BLE_fcc/main.c
@@ -419,7 +419,7 @@ static void mainWsfInit(void)
     /* +12 for message headroom, +ISO Data Load, +4 for header. */
     const uint16_t dataBufSize =
         12 + HCI_ISO_DL_MAX_LEN + mainLlRtCfg.maxAclLen + 4 + BB_DATA_PDU_TAILROOM;
-#if (BT_VER > 9)
+
     /* Use single pool for data buffers. */
 #if (BT_VER > 9)
     WSF_ASSERT(mainLlRtCfg.maxAclLen == mainLlRtCfg.maxIsoSduLen);
