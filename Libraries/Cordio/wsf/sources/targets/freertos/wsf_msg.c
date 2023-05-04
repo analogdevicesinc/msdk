0933c956a95843c1c3bd2a474066be1556426081
Eddie <eddie.amaya@analog.com>
2023-05-04 13:42:45 -0500

    Initializes message buffer to null character in freeRTOS target

--- Libraries/Cordio/wsf/sources/targets/freertos/wsf_msg.c
+++ Libraries/Cordio/wsf/sources/targets/freertos/wsf_msg.c
@@ -69,6 +69,7 @@ void *WsfMsgAlloc(uint16_t len)
   wsfMsg_t  *pMsg;
 
   pMsg = WsfBufAlloc(len + sizeof(wsfMsg_t));
+  memset(pMsg, '\0', len + sizeof(wsfMsg_t));
 
   /* hide header */
   if (pMsg != NULL) {
