6924a9c46c6086b2cb38e34a77b9eae0f556482b
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2023-05-08 09:18:39 -0500

    Revert "clang-format bot reformatting."
    This reverts commit dfa15a03901c641c2a496fb82ca58ebb5a7f9106.

--- Libraries/MiscDrivers/BarcodeDecoder/zbar/libiconv/aliases.h
+++ Libraries/MiscDrivers/BarcodeDecoder/zbar/libiconv/aliases.h
@@ -2,43 +2,47 @@
 /* Command-line: gperf -m 10 aliases.gperf  */
 /* Computed positions: -k'1,3-11,$' */
 
-#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) && ('%' == 37) && ('&' == 38) &&  \
-      ('\'' == 39) && ('(' == 40) && (')' == 41) && ('*' == 42) && ('+' == 43) && (',' == 44) && \
-      ('-' == 45) && ('.' == 46) && ('/' == 47) && ('0' == 48) && ('1' == 49) && ('2' == 50) &&  \
-      ('3' == 51) && ('4' == 52) && ('5' == 53) && ('6' == 54) && ('7' == 55) && ('8' == 56) &&  \
-      ('9' == 57) && (':' == 58) && (';' == 59) && ('<' == 60) && ('=' == 61) && ('>' == 62) &&  \
-      ('?' == 63) && ('A' == 65) && ('B' == 66) && ('C' == 67) && ('D' == 68) && ('E' == 69) &&  \
-      ('F' == 70) && ('G' == 71) && ('H' == 72) && ('I' == 73) && ('J' == 74) && ('K' == 75) &&  \
-      ('L' == 76) && ('M' == 77) && ('N' == 78) && ('O' == 79) && ('P' == 80) && ('Q' == 81) &&  \
-      ('R' == 82) && ('S' == 83) && ('T' == 84) && ('U' == 85) && ('V' == 86) && ('W' == 87) &&  \
-      ('X' == 88) && ('Y' == 89) && ('Z' == 90) && ('[' == 91) && ('\\' == 92) && (']' == 93) && \
-      ('^' == 94) && ('_' == 95) && ('a' == 97) && ('b' == 98) && ('c' == 99) && ('d' == 100) && \
-      ('e' == 101) && ('f' == 102) && ('g' == 103) && ('h' == 104) && ('i' == 105) &&            \
-      ('j' == 106) && ('k' == 107) && ('l' == 108) && ('m' == 109) && ('n' == 110) &&            \
-      ('o' == 111) && ('p' == 112) && ('q' == 113) && ('r' == 114) && ('s' == 115) &&            \
-      ('t' == 116) && ('u' == 117) && ('v' == 118) && ('w' == 119) && ('x' == 120) &&            \
-      ('y' == 121) && ('z' == 122) && ('{' == 123) && ('|' == 124) && ('}' == 125) &&            \
-      ('~' == 126))
+#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
+      && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40) \
+      && (')' == 41) && ('*' == 42) && ('+' == 43) && (',' == 44) \
+      && ('-' == 45) && ('.' == 46) && ('/' == 47) && ('0' == 48) \
+      && ('1' == 49) && ('2' == 50) && ('3' == 51) && ('4' == 52) \
+      && ('5' == 53) && ('6' == 54) && ('7' == 55) && ('8' == 56) \
+      && ('9' == 57) && (':' == 58) && (';' == 59) && ('<' == 60) \
+      && ('=' == 61) && ('>' == 62) && ('?' == 63) && ('A' == 65) \
+      && ('B' == 66) && ('C' == 67) && ('D' == 68) && ('E' == 69) \
+      && ('F' == 70) && ('G' == 71) && ('H' == 72) && ('I' == 73) \
+      && ('J' == 74) && ('K' == 75) && ('L' == 76) && ('M' == 77) \
+      && ('N' == 78) && ('O' == 79) && ('P' == 80) && ('Q' == 81) \
+      && ('R' == 82) && ('S' == 83) && ('T' == 84) && ('U' == 85) \
+      && ('V' == 86) && ('W' == 87) && ('X' == 88) && ('Y' == 89) \
+      && ('Z' == 90) && ('[' == 91) && ('\\' == 92) && (']' == 93) \
+      && ('^' == 94) && ('_' == 95) && ('a' == 97) && ('b' == 98) \
+      && ('c' == 99) && ('d' == 100) && ('e' == 101) && ('f' == 102) \
+      && ('g' == 103) && ('h' == 104) && ('i' == 105) && ('j' == 106) \
+      && ('k' == 107) && ('l' == 108) && ('m' == 109) && ('n' == 110) \
+      && ('o' == 111) && ('p' == 112) && ('q' == 113) && ('r' == 114) \
+      && ('s' == 115) && ('t' == 116) && ('u' == 117) && ('v' == 118) \
+      && ('w' == 119) && ('x' == 120) && ('y' == 121) && ('z' == 122) \
+      && ('{' == 123) && ('|' == 124) && ('}' == 125) && ('~' == 126))
 /* The character set is not based on ISO-646.  */
-#error \
-    "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gperf@gnu.org>."
+#error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gperf@gnu.org>."
 #endif
+
 
 #define MIN_WORD_LENGTH 2
 #define MAX_WORD_LENGTH 45
 #define MIN_HASH_VALUE 15
 #define MAX_HASH_VALUE 921
 
-struct alias {
-    const char *name;
-    unsigned int encoding_index;
-};
+struct alias { const char *name; unsigned int encoding_index; };
 
-static const struct alias aliases[] = {
-    { "ISO8859-1", ei_iso8859_1 },
-    { "UTF-8", ei_utf8 },
-    { "SJIS", ei_sjis },
-    { "BIG-5", ei_ces_big5 },
+static const struct alias aliases[] =
+  {
+	{"ISO8859-1", 				ei_iso8859_1},
+	{"UTF-8", 					ei_utf8},
+	{"SJIS",	 				ei_sjis},
+	{"BIG-5", 					ei_ces_big5},
 #if 0
     {"L6", 						ei_iso8859_10},
     {"L1", 						ei_iso8859_1},
@@ -386,23 +390,24 @@ static const struct alias aliases[] = {
     {"JOHAB", 					ei_johab},
     {"CSHALFWIDTHKATAKANA", 	ei_jisx0201}
 #endif
-};
+  };
 
-const struct alias *aliases_lookup(register const char *str, register size_t len)
+const struct alias *aliases_lookup (register const char *str, register size_t len)
 {
-    int i;
-    int number_of_item;
-    register const char *s;
+	int i;
+	int number_of_item;
+	register const char *s;
 
-    number_of_item = sizeof(aliases) / sizeof(aliases[0]);
+	number_of_item = sizeof(aliases) / sizeof(aliases[0]);
 
-    if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH) {
-        for (i = 0; i < number_of_item; i++) {
-            s = aliases[i].name;
-            if (*str == *s && !strcmp(str + 1, s + 1))
-                return &aliases[i];
-        }
-    }
+	if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
+	{
+		for (i = 0; i < number_of_item; i++) {
+			s = aliases[i].name;
+			if (*str == *s && !strcmp (str + 1, s + 1))
+			  return &aliases[i];
+		}
+	}
 
-    return 0;
+	return 0;
 }
