e58ca646cdd1fe620c1d6bb3fd39d5269ba1fcdf
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2023-05-30 14:34:13 -0500

    feat(workflow): Add generic scopes based on directory locations

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,7 +11,7 @@ module.exports = {
      * Any rules defined here will override rules from @commitlint/config-conventional
      */
     rules: {
-        "scope-enum": [2, "always", ["BLE", "MAX32520", "MAX32570", "MAX32572", "MAX32650", "MAX32655", "MAX32660", "MAX32662", "MAX32665", "MAX32670", "MAX32672", "MAX32675", "MAX32680", "MAX32690", "MAX78000", "MAX78002", "Register", "Tools", "Workflow", "VS Code", "Installer", "IAR", "Keil", "ignore"]],
+        "scope-enum": [2, "always", ["Documentation", "Examples", "Tools", "BLE", "Boards", "CMSIS", "MiscDrivers", "PeriphDrivers", "Third-Party", "ignore", "workflow"]],
         "scope-empty": [2, "never"],        
         "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"]],
         "subject-case": [2, "always", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
