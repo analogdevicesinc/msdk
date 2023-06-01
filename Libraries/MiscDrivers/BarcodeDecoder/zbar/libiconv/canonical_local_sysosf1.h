3d4e0ab7a7df2af6ff61f1270d3777e597968da6
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2023-06-01 15:53:49 -0500

    Added ifdef to prevent timer blocking code execution when production test is occuring (#605)
    * gitignore update
    
    * added ifdef to prevent timer blocking in prod tests
    
    * clang-format bot reformatting.
    
    ---------
    
    Co-authored-by: EricB-ADI <EricB-ADI@users.noreply.github.com>

--- Libraries/MiscDrivers/BarcodeDecoder/zbar/libiconv/canonical_local_sysosf1.h
+++ Libraries/MiscDrivers/BarcodeDecoder/zbar/libiconv/canonical_local_sysosf1.h
@@ -1,2 +1,2 @@
-  (int)(long)&((struct stringpool_t *)0)->stringpool_str193,
-  (int)(long)&((struct stringpool_t *)0)->stringpool_str457,
+(int)(long)&((struct stringpool_t *)0)->stringpool_str193,
+    (int)(long)&((struct stringpool_t *)0)->stringpool_str457,
--- Libraries/MiscDrivers/BarcodeDecoder/zbar/libiconv/dec_kanji.h
+++ Libraries/MiscDrivers/BarcodeDecoder/zbar/libiconv/dec_kanji.h
@@ -21,50 +21,50 @@
  * DEC-KANJI
  */
 
-static int
-dec_kanji_mbtowc (conv_t conv, ucs4_t *pwc, const unsigned char *s, size_t n)
+static int dec_kanji_mbtowc(conv_t conv, ucs4_t *pwc, const unsigned char *s, size_t n)
 {
-  unsigned char c = *s;
-  /* Code set 0 (ASCII or JIS X 0201-1976 Roman) */
-  if (c < 0x80)
-    return ascii_mbtowc(conv,pwc,s,n);
-  /* Code set 1 (JIS X 0208) */
-  if (c >= 0xa1 && c < 0xf5) {
-    if (n < 2)
-      return RET_TOOFEW(0);
-    {
-      unsigned char c2 = s[1];
-      if (c2 >= 0xa1 && c2 < 0xff) {
-        unsigned char buf[2];
-        buf[0] = c-0x80; buf[1] = c2-0x80;
-        return jisx0208_mbtowc(conv,pwc,buf,2);
-      }
+    unsigned char c = *s;
+    /* Code set 0 (ASCII or JIS X 0201-1976 Roman) */
+    if (c < 0x80)
+        return ascii_mbtowc(conv, pwc, s, n);
+    /* Code set 1 (JIS X 0208) */
+    if (c >= 0xa1 && c < 0xf5) {
+        if (n < 2)
+            return RET_TOOFEW(0);
+        {
+            unsigned char c2 = s[1];
+            if (c2 >= 0xa1 && c2 < 0xff) {
+                unsigned char buf[2];
+                buf[0] = c - 0x80;
+                buf[1] = c2 - 0x80;
+                return jisx0208_mbtowc(conv, pwc, buf, 2);
+            }
+        }
     }
-  }
-  return RET_ILSEQ;
+    return RET_ILSEQ;
 }
 
-static int
-dec_kanji_wctomb (conv_t conv, unsigned char *r, ucs4_t wc, size_t n)
+static int dec_kanji_wctomb(conv_t conv, unsigned char *r, ucs4_t wc, size_t n)
 {
-  unsigned char buf[2];
-  int ret;
+    unsigned char buf[2];
+    int ret;
 
-  /* Code set 0 (ASCII or JIS X 0201-1976 Roman) */
-  ret = ascii_wctomb(conv,r,wc,n);
-  if (ret != RET_ILUNI)
-    return ret;
+    /* Code set 0 (ASCII or JIS X 0201-1976 Roman) */
+    ret = ascii_wctomb(conv, r, wc, n);
+    if (ret != RET_ILUNI)
+        return ret;
 
-  /* Code set 1 (JIS X 0208) */
-  ret = jisx0208_wctomb(conv,buf,wc,2);
-  if (ret != RET_ILUNI) {
-    if (ret != 2) abort();
-    if (n < 2)
-      return RET_TOOSMALL;
-    r[0] = buf[0]+0x80;
-    r[1] = buf[1]+0x80;
-    return 2;
-  }
+    /* Code set 1 (JIS X 0208) */
+    ret = jisx0208_wctomb(conv, buf, wc, 2);
+    if (ret != RET_ILUNI) {
+        if (ret != 2)
+            abort();
+        if (n < 2)
+            return RET_TOOSMALL;
+        r[0] = buf[0] + 0x80;
+        r[1] = buf[1] + 0x80;
+        return 2;
+    }
 
-  return RET_ILUNI;
+    return RET_ILUNI;
 }
