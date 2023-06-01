dfee3c8185a5afe4cdb7b0cd585841c1fd1741fa
Eddie <62710807+EdwinFairchild@users.noreply.github.com>
2023-06-01 15:07:51 -0500

    MiscDrivers: Adds volatile qualifier to timeout variable used in ext flash (#588)
    Prevents the timeout variable from being optimized out.

--- Libraries/MiscDrivers/ExtMemory/w25.c
+++ Libraries/MiscDrivers/ExtMemory/w25.c
@@ -374,7 +374,7 @@ int Ext_Flash_Program_Page(uint32_t address, uint8_t *tx_buf, uint32_t tx_len,
                            Ext_Flash_DataLine_t d_line)
 {
     int err = EF_E_SUCCESS;
-    int timeout = 0;
+    volatile int timeout = 0;
     uint8_t cmd[4] = { 0 };
     uint32_t len = 0;
     uint32_t next_page = 0;
@@ -470,7 +470,7 @@ int Ext_Flash_Bulk_Erase(void)
 {
     int err = EF_E_SUCCESS;
     uint8_t cmd = W25_CMD_BULK_ERASE;
-    int timeout = 0;
+    volatile int timeout = 0;
 
     if (flash_busy()) {
         return EF_E_BUSY;
@@ -500,7 +500,7 @@ int Ext_Flash_Erase(uint32_t address, Ext_Flash_Erase_t size)
 {
     int err = EF_E_SUCCESS;
     uint8_t cmd[4] = { 0 };
-    int timeout = 0;
+    volatile int timeout = 0;
 
     if (flash_busy()) {
         return EF_E_BUSY;
