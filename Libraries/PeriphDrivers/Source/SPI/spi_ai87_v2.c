4a990ec0b1776092bf32fa322c23b69ad67ba5e8
sihyung-maxim <sihyung-maxim@users.noreply.github.com>
2023-06-20 16:30:01 +0000

    clang-format bot reformatting.

--- Libraries/PeriphDrivers/Source/SPI/spi_ai87_v2.c
+++ Libraries/PeriphDrivers/Source/SPI/spi_ai87_v2.c
@@ -713,9 +713,12 @@ int MXC_SPI_ControllerTransactionDMAB(mxc_spi_regs_t *spi, uint8_t *tx_buffer, u
     }
 }
 
-int MXC_SPI_CTransmitB(mxc_spi_regs_t *spi, uint8_t *tx_buffer, uint32_t tx_fr_len, uint8_t *rx_buffer, uint32_t rx_fr_len, uint8_t deassert, mxc_spi_target_t *target)
+int MXC_SPI_CTransmitB(mxc_spi_regs_t *spi, uint8_t *tx_buffer, uint32_t tx_fr_len,
+                       uint8_t *rx_buffer, uint32_t rx_fr_len, uint8_t deassert,
+                       mxc_spi_target_t *target)
 {
-    return MXC_SPI_RevA2_CTransmitB((mxc_spi_reva_regs_t *)spi, tx_buffer, tx_fr_len, rx_buffer, rx_fr_len, deassert, target);
+    return MXC_SPI_RevA2_CTransmitB((mxc_spi_reva_regs_t *)spi, tx_buffer, tx_fr_len, rx_buffer,
+                                    rx_fr_len, deassert, target);
 }
 
 int MXC_SPI_SlaveTransaction(mxc_spi_req_t *req)
@@ -770,21 +773,21 @@ int MXC_SPI_GetDataSize(mxc_spi_regs_t *spi)
 int MXC_SPI_SetWidth(mxc_spi_regs_t *spi, mxc_spi_width_t spiWidth)
 {
     switch (spiWidth) {
-        case SPI_WIDTH_3WIRE:
-            return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_3WIRE);
+    case SPI_WIDTH_3WIRE:
+        return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_3WIRE);
 
-        case SPI_WIDTH_STANDARD:
-            return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_STANDARD);
+    case SPI_WIDTH_STANDARD:
+        return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_STANDARD);
 
-        case SPI_WIDTH_DUAL:
-            return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_DUAL);
-            break;
+    case SPI_WIDTH_DUAL:
+        return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_DUAL);
+        break;
 
-        case SPI_WIDTH_QUAD:
-            return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_QUAD);
+    case SPI_WIDTH_QUAD:
+        return MXC_SPI_SetModeIF(spi, MXC_SPI_INTERFACE_QUAD);
 
-        default:
-            return E_BAD_PARAM;
+    default:
+        return E_BAD_PARAM;
     }
 }
 
@@ -794,21 +797,21 @@ mxc_spi_width_t MXC_SPI_GetWidth(mxc_spi_regs_t *spi)
 
     mode = MXC_SPI_GetModeIF(spi);
 
-    switch(mode) {
-        case MXC_SPI_INTERFACE_STANDARD:
-            return SPI_WIDTH_STANDARD;
+    switch (mode) {
+    case MXC_SPI_INTERFACE_STANDARD:
+        return SPI_WIDTH_STANDARD;
 
-        case MXC_SPI_INTERFACE_QUAD:
-            return SPI_WIDTH_QUAD;
+    case MXC_SPI_INTERFACE_QUAD:
+        return SPI_WIDTH_QUAD;
 
-        case MXC_SPI_INTERFACE_DUAL:
-            return SPI_WIDTH_DUAL;
+    case MXC_SPI_INTERFACE_DUAL:
+        return SPI_WIDTH_DUAL;
 
-        case MXC_SPI_INTERFACE_3WIRE:
-            return SPI_WIDTH_3WIRE;
+    case MXC_SPI_INTERFACE_3WIRE:
+        return SPI_WIDTH_3WIRE;
 
-        default:
-            return SPI_WIDTH_STANDARD;
+    default:
+        return SPI_WIDTH_STANDARD;
     }
 }
 
