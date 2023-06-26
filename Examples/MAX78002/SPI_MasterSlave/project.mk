2ad90135cc062885673d74429005cebc121aae47
Sihyung Woo <sihyung.woo@analog.com>
2023-06-26 10:04:32 -0500

    fix(Examples): Fix SPI library build selection p2

--- Examples/MAX78002/SPI_MasterSlave/project.mk
+++ Examples/MAX78002/SPI_MasterSlave/project.mk
@@ -10,6 +10,5 @@
 # Add your config here!
 
 # SPI v2 does not support Target (L. Slave) Transaction functions yet.
-# Set the MXC_SPI_LEGACY to 1 to build the previous SPI library.
-MXC_SPI_LEGACY=1
-export MXC_SPI_LEGACY
+# Set the MXC_SPI_BUILD_LEGACY to 1 to build the previous SPI library.
+MXC_SPI_BUILD_LEGACY=1
