a1094211b17fbfc11101ce273cb480abaa00b3ca
Jake Carter <jake.carter@analog.com>
2023-07-17 15:58:36 -0500

    Remove dependencies on 'tr' for upper/lowercase target name parsing

--- Libraries/CMSIS/Device/Maxim/MAX32680/Source/GCC/max32680.mk
+++ Libraries/CMSIS/Device/Maxim/MAX32680/Source/GCC/max32680.mk
@@ -35,8 +35,8 @@ ifeq "$(CMSIS_ROOT)" ""
 $(error CMSIS_ROOT must be specified)
 endif
 
-TARGET_UC:=$(shell echo $(TARGET) | tr a-z A-Z)
-TARGET_LC:=$(shell echo $(TARGET) | tr A-Z a-z)
+TARGET_UC := $(subst m,M,$(subst a,A,$(subst x,X,$(TARGET))))
+TARGET_LC := $(subst M,m,$(subst A,a,$(subst X,x,$(TARGET))))
 
 # The build directory
 ifeq "$(BUILD_DIR)" ""
