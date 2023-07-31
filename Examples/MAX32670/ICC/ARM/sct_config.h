be54c37457635a41603a5c4954f3e079eb22beac
SRV-JenkinsIDE <SRV-JenkinsIDE@maximintegrated.com>
2023-07-31 13:08:28 -0500

    After CLANG cleanup on all the sct_config.h

--- Examples/MAX32670/ICC/ARM/sct_config.h
+++ Examples/MAX32670/ICC/ARM/sct_config.h
@@ -2,7 +2,7 @@
 #define __SCT_CONFIG_H
 
 ; /*-Memory Regions-*/
-#define _BIN_START_  0x1000A000
-#define _BINARY_RISCV_BIN_START 0x10075000  
+#define _BIN_START_ 0x1000A000
+#define _BINARY_RISCV_BIN_START 0x10075000
 
 #endif
