dd3ae6dff39dd4eb173800f941147723b0bccba4
Jake Carter <jake.carter@analog.com>
2023-07-31 23:49:12 -0500

    Fix some bugs on parallel builds

--- Libraries/CMSIS/Device/Maxim/GCC/gcc_riscv.mk
+++ Libraries/CMSIS/Device/Maxim/GCC/gcc_riscv.mk
@@ -568,10 +568,12 @@ debug:
 # that come from the build system and compiler itself.  This generates a
 # "project_defines.h" header file inside the build directory that can be
 # force included by VS Code to improve the intellisense engine.
+$(BUILD_DIR)/_empty_tmp_file.c: $(BUILD_DIR)
+	@echo "" > $(BUILD_DIR)/_empty_tmp_file.c
+
 .PHONY: project_defines
 project_defines: $(BUILD_DIR)/project_defines.h
-$(BUILD_DIR)/project_defines.h: $(BUILD_DIR)
-	@echo "" > $(BUILD_DIR)/_empty_tmp_file.c
+$(BUILD_DIR)/project_defines.h: $(BUILD_DIR)/_empty_tmp_file.c
 	@echo "// This is a generated file that's used to detect definitions that have been set by the compiler and build system." > $@
 	@$(CC) -E -P -dD $(BUILD_DIR)/_empty_tmp_file.c $(CFLAGS) >> $@
 ifeq "$(_OS)" "windows"
