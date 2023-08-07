ece94852a89df1453fee37ecceee231b6d1fd87a
Afshin Niktash <48036488+aniktash@users.noreply.github.com>
2023-08-07 16:03:09 -0700

    feat(Examples): Speedup FTHR TFT update for facial-recognition demo for MAX78000 (#694)

--- Examples/MAX78000/CNN/facial_recognition/src/tft_utils.c
+++ Examples/MAX78000/CNN/facial_recognition/src/tft_utils.c
@@ -41,12 +41,72 @@
 void draw_obj_rect(float *xy, uint32_t w, uint32_t h)
 {
 #ifdef TFT_ENABLE
-    int x1 = w * xy[0] + X_START;
-    int y1 = h * xy[1] + Y_START;
-    int x2 = w * xy[2] + X_START - 1;
-    int y2 = h * xy[3] + Y_START - 1;
+
+    int x1 = h * (xy[1]) + X_START;
+    int y1 = w * (xy[2]) + Y_START - 1;
+    int x2 = h * (xy[3]) + X_START - 1;
+    int y2 = w * (xy[0]) + Y_START;
 
     // Draw rectangle around detected face
     MXC_TFT_Rectangle(x1, y1, x2, y2, FRAME_ORANGE);
 #endif
 }
+
+//--------------------------------------------------------------------
+#ifdef TFT_ENABLE
+
+int dma_channel;
+int g_dma_channel_tft = 1;
+uint8_t *rx_data = NULL;
+void setup_dma_tft(uint32_t *src_ptr)
+{
+    printf("TFT DMA setup\n");
+    // TFT DMA
+    MXC_DMA->ch[g_dma_channel_tft].status = MXC_F_DMA_STATUS_CTZ_IF; // Clear CTZ status flag
+    MXC_DMA->ch[g_dma_channel_tft].dst = (uint32_t)rx_data; // Cast Pointer
+    MXC_DMA->ch[g_dma_channel_tft].src = (uint32_t)src_ptr;
+    MXC_DMA->ch[g_dma_channel_tft].cnt = IMAGE_XRES * IMAGE_YRES;
+
+    MXC_DMA->ch[g_dma_channel_tft].ctrl =
+        ((0x1 << MXC_F_DMA_CTRL_CTZ_IE_POS) + (0x0 << MXC_F_DMA_CTRL_DIS_IE_POS) +
+         (0x1 << MXC_F_DMA_CTRL_BURST_SIZE_POS) + (0x0 << MXC_F_DMA_CTRL_DSTINC_POS) +
+         (0x1 << MXC_F_DMA_CTRL_DSTWD_POS) + (0x1 << MXC_F_DMA_CTRL_SRCINC_POS) +
+         (0x1 << MXC_F_DMA_CTRL_SRCWD_POS) + (0x0 << MXC_F_DMA_CTRL_TO_CLKDIV_POS) +
+         (0x0 << MXC_F_DMA_CTRL_TO_WAIT_POS) + (0x2F << MXC_F_DMA_CTRL_REQUEST_POS) + // SPI0 -> TFT
+         (0x0 << MXC_F_DMA_CTRL_PRI_POS) + // High Priority
+         (0x0 << MXC_F_DMA_CTRL_RLDEN_POS) // Disable Reload
+        );
+
+    MXC_SPI0->ctrl0 &= ~(MXC_F_SPI_CTRL0_EN);
+    MXC_SETFIELD(MXC_SPI0->ctrl1, MXC_F_SPI_CTRL1_TX_NUM_CHAR,
+                 (IMAGE_XRES * IMAGE_YRES) << MXC_F_SPI_CTRL1_TX_NUM_CHAR_POS);
+    MXC_SPI0->dma |= (MXC_F_SPI_DMA_TX_FLUSH | MXC_F_SPI_DMA_RX_FLUSH);
+
+    // Clear SPI master done flag
+    MXC_SPI0->intfl = MXC_F_SPI_INTFL_MST_DONE;
+    MXC_SETFIELD(MXC_SPI0->dma, MXC_F_SPI_DMA_TX_THD_VAL, 0x10 << MXC_F_SPI_DMA_TX_THD_VAL_POS);
+    MXC_SPI0->dma |= (MXC_F_SPI_DMA_TX_FIFO_EN);
+    MXC_SPI0->dma |= (MXC_F_SPI_DMA_DMA_TX_EN);
+    MXC_SPI0->ctrl0 |= (MXC_F_SPI_CTRL0_EN);
+}
+
+void start_tft_dma(uint32_t *src_ptr)
+{
+    while ((MXC_DMA->ch[g_dma_channel_tft].status & MXC_F_DMA_STATUS_STATUS)) {
+        ;
+    }
+
+    if (MXC_DMA->ch[g_dma_channel_tft].status & MXC_F_DMA_STATUS_CTZ_IF) {
+        MXC_DMA->ch[g_dma_channel_tft].status = MXC_F_DMA_STATUS_CTZ_IF;
+    }
+
+    MXC_DMA->ch[g_dma_channel_tft].cnt = IMAGE_XRES * IMAGE_YRES;
+    MXC_DMA->ch[g_dma_channel_tft].src = (uint32_t)src_ptr;
+
+    // Enable DMA channel
+    MXC_DMA->ch[g_dma_channel_tft].ctrl += (0x1 << MXC_F_DMA_CTRL_EN_POS);
+    // Start DMA
+    MXC_SPI0->ctrl0 |= MXC_F_SPI_CTRL0_START;
+}
+
+#endif
