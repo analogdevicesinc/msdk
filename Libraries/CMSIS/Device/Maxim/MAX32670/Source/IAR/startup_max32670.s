8ea6477a35de052ee95e8e654bc8c4cf91da8067
ftariqAnalogTx <faisal.tariq@analog.com>
2023-08-07 17:38:28 -0500

    updated the license header comment in IAR/startup_max32670.s

--- Libraries/CMSIS/Device/Maxim/MAX32670/Source/IAR/startup_max32670.s
+++ Libraries/CMSIS/Device/Maxim/MAX32670/Source/IAR/startup_max32670.s
@@ -1,48 +1,35 @@
-;*******************************************************************************
-;* Copyright (C) 2018 Maxim Integrated Products, Inc., All Rights Reserved.
-;*
-;* Permission is hereby granted, free of charge, to any person obtaining a
-;* copy of this software and associated documentation files (the "Software"),
-;* to deal in the Software without restriction, including without limitation
-;* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-;* and/or sell copies of the Software, and to permit persons to whom the
-;* Software is furnished to do so, subject to the following conditions:
-;*
-;* The above copyright notice and this permission notice shall be included
-;* in all copies or substantial portions of the Software.
-;*
-;* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-;* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-;* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-;* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
-;* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-;* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-;* OTHER DEALINGS IN THE SOFTWARE.
-;*
-;* Except as contained in this notice, the name of Maxim Integrated
-;* Products, Inc. shall not be used except as stated in the Maxim Integrated
-;* Products, Inc. Branding Policy.
-;*
-;* The mere transfer of this software does not imply any licenses
-;* of trade secrets, proprietary technology, copyrights, patents,
-;* trademarks, maskwork rights, or any other form of intellectual
-;* property whatsoever. Maxim Integrated Products, Inc. retains all
-;* ownership rights.
-;*
-;* Description        : MAX32655 device vector table for IAR EWARM toolchain.
-;*                      - Sets the initial SP
-;*                      - Sets the initial PC == _iar_program_start,
-;*                      - Set the vector table entries with the exceptions ISR
-;*                        address, all set as PUBWEAK. User may override any ISR
-;*                        defined as PUBWEAK.
-;*                      - Branches to main in the C library (which eventually
-;*                        calls SystemInit() and main()).
-;*                      After Reset the Cortex-M4 processor is in Thread mode,
-;*                      priority is Privileged, and the Stack is set to Main.
-;* $Date: 2019-07-30 14:08:36 -0500 (Tue, 30 Jul 2019) $
-;* $Revision: 45136 $
-;*
-;*******************************************************************************
+;------------------------------------------------------------------------------
+; Copyright (C) 2023 Maxim Integrated Products, Inc., All Rights Reserved.
+;
+; Permission is hereby granted, free of charge, to any person obtaining a
+; copy of this software and associated documentation files (the "Software"),
+; to deal in the Software without restriction, including without limitation
+; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+; and/or sell copies of the Software, and to permit persons to whom the
+; Software is furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included
+; in all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+; OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+; IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+; OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+; OTHER DEALINGS IN THE SOFTWARE.
+;
+; Except as contained in this notice, the name of Maxim Integrated
+; Products, Inc. shall not be used except as stated in the Maxim Integrated
+; Products, Inc. Branding Policy.
+;
+; The mere transfer of this software does not imply any licenses
+; of trade secrets, proprietary technology, copyrights, patents,
+; trademarks, maskwork rights, or any other form of intellectual
+; property whatsoever. Maxim Integrated Products, Inc. retains all
+; ownership rights.
+;
+;------------------------------------------------------------------------------
     MODULE  ?cstartup
 
     ;; Forward declaration of sections.
