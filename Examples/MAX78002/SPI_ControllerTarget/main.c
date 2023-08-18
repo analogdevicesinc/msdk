0cd86ff2ea6145ec9ef93e173317abf9def79a81
Sihyung Woo <sihyung.woo@analog.com>
2023-08-18 14:42:40 -0500

    fix(Examples): Fix build errors with SPI_ControllerTarget

--- Examples/MAX78002/SPI_ControllerTarget/main.c
+++ Examples/MAX78002/SPI_ControllerTarget/main.c
@@ -70,8 +70,8 @@
 /***** Globals *****/
 uint8_t controller_rx[DATA_LEN];
 uint8_t controller_tx[DATA_LEN];
-uint8_t target_rx[DATA_LEN];
-uint8_t target_tx[DATA_LEN];
+uint8_t ts_rx[DATA_LEN];
+uint8_t ts_tx[DATA_LEN];
 uint8_t TX_DMA_CH, RX_DMA_CH;
 
 /***** Functions *****/
@@ -95,15 +95,20 @@ void DMA_RX_IRQHandler(void)
 int main(void)
 {
     int error;
-    mxc_spi_init_t controller_init; // L. Master
-    mxc_spi_init_t target_init; // L. Slave
-    mxc_spi_target_t target;
+    // Controller (L. Master)
+    mxc_spi_pins_t controller_pins;
+    mxc_spi_cfg_t controller_cfg;
     mxc_spi_req_t controller_req;
+    mxc_spi_ts_t ts;
+
+    // Target (L. Slave)
+    mxc_spi_pins_t target_pins;
+    mxc_spi_cfg_t target_cfg;
     mxc_spi_req_t target_req;
 
     printf("\n************************ SPI Controller-Target Example ************************\n");
     printf("This example sends data between two SPI peripherals in the MAX78002.\n");
-    printf("SPI%d is configured as the target (L. Slave) and SPI%d is configured\n",
+    printf("SPI%d is configured as the ts (L. Slave) and SPI%d is configured\n",
            MXC_SPI_GET_IDX(SPI_TARGET), MXC_SPI_GET_IDX(SPI_CONTROLLER));
     printf("as the controller (L. Master). Each SPI peripheral sends %d bytes\n", DATA_LEN);
     printf("on the SPI bus. If the data received by each SPI instance matches the\n");
@@ -119,99 +124,107 @@ int main(void)
     /***** Initialize data buffers *****/
     for (int i = 0; i < DATA_LEN; i++) {
         controller_tx[i] = i;
-        target_tx[i] = i;
+        ts_tx[i] = i;
     }
 
     memset(controller_rx, 0x0, DATA_LEN * sizeof(uint8_t));
-    memset(target_rx, 0x0, DATA_LEN * sizeof(uint8_t));
+    memset(ts_rx, 0x0, DATA_LEN * sizeof(uint8_t));
 
     /***** Configure Controller (L. Master) *****/
-    controller_init.spi = SPI_CONTROLLER;
-    controller_init.freq = SPI_SPEED;
-    controller_init.spi_pins = NULL; // Use default, predefined pins
-    controller_init.if_mode = MXC_SPI_INTERFACE_STANDARD;
-    controller_init.type = MXC_SPI_TYPE_CONTROLLER; // L. Master
-    controller_init.clk_mode = MXC_SPI_CLKMODE_0; // CPOL: 0, CPHA: 0
-    controller_init.frame_size = DATA_SIZE;
-    controller_init.use_dma = false;
+
+
+
+    controller_cfg.spi = SPI_CONTROLLER;
+    controller_cfg.clk_mode = MXC_SPI_CLKMODE_0; // CPOL: 0, CPHA: 0
+    controller_cfg.frame_size = DATA_SIZE;
+    controller_cfg.use_dma_tx = false;
+    controller_cfg.use_dma_rx = false;
 
     // Target Select Settings
 #if CUSTOM_TARGET
-    // Example to select a custom target.
-    mxc_gpio_cfg_t target_pins;
-    target_pins.port = MXC_GPIO0;
-    target_pins.mask = MXC_GPIO_PIN_12;
-    target_pins.func = MXC_GPIO_FUNC_OUT;
-    target_pins.pad = MXC_GPIO_PAD_PULL_UP;
-    target_pins.vssel = MXC_GPIO_VSSEL_VDDIO; // Set custom target pin to VDDIOH (3.3V).
+    // Example to select a custom ts.
+    mxc_gpio_cfg_t ts_pins;
+    ts_pins.port = MXC_GPIO0;
+    ts_pins.mask = MXC_GPIO_PIN_12;
+    ts_pins.func = MXC_GPIO_FUNC_OUT;
+    ts_pins.pad = MXC_GPIO_PAD_PULL_UP;
+    ts_pins.vssel = MXC_GPIO_VSSEL_VDDIO; // Set custom ts pin to VDDIOH (3.3V).
 
-    controller_init.ts_control =
-        MXC_SPI_TSCONTROL_SW_DRV; // SPI Driver will handle deassertion for TS pins.
-    controller_init.target.pins = target_pins;
-    controller_init.target.active_polarity = 0;
-    controller_init.vssel = MXC_GPIO_VSSEL_VDDIO; // Set SPI pins to VDDIOH (3.3V).
+    // Initialize HW TS0 pin.
+    controller_pins.ts0 = false;
+    controller_pins.ts1 = false;
+    controller_pins.ts2 = false;
+    controller_pins.vddioh = false;
 
-    // Select target for transaction.
-    target.pins = target_pins; // Custom pins
+    // Select ts for transaction.
+    ts.pins = ts_pins; // Custom pins
+    ts.active_pol = 0;
 #else
-    controller_init.ts_control = MXC_SPI_TSCONTROL_HW_AUTO; // HW will deassert/assert TS pins.
-    controller_init.target.active_polarity = 0;
-    controller_init.target.init_mask = 1 << SPI_CONTROLLER_TSIDX; // Initialize Target Select 0 pin.
-    controller_init.vssel = MXC_GPIO_VSSEL_VDDIO;
+    // Initialize HW TS0 pin.
+    controller_pins.ts0 = true;
+    controller_pins.ts1 = false;
+    controller_pins.ts2 = false;
+    controller_pins.vddioh = false;
 
-    // Select target for transaction.
-    target.index = SPI_CONTROLLER_TSIDX;
+    // Select TSn pin for transaction.
+    ts.index = SPI_CONTROLLER_TSIDX;
+    ts.active_pol = 0;
 #endif
 
-    error = MXC_SPI_Init_v2(&controller_init);
+    error = MXC_SPI_Init(SPI_CONTROLLER, MXC_SPI_TYPE_CONTROLLER, MXC_SPI_INTERFACE_STANDARD, 0, 0b000, SPI_SPEED, controller_pins);
     if (error != E_NO_ERROR) {
         printf("\nSPI CONTROLLER INITIALIZATION ERROR\n");
+        while (1) {}
+    }
+
+    error = MXC_SPI_Config(&controller_cfg);
+    if (error != E_NO_ERROR) {
+        printf("\nSPI CONTROLLER CONFIGURATION ERROR\n");
         while (1) {}
     }
 
     // Setup Controller Request.
     controller_req.spi = SPI_CONTROLLER;
     controller_req.tx_buffer = (uint8_t *)controller_tx;
-    controller_req.tx_fr_len = DATA_LEN;
+    controller_req.tx_length_frames = DATA_LEN;
     controller_req.rx_buffer = (uint8_t *)controller_rx;
-    controller_req.rx_fr_len = DATA_LEN;
+    controller_req.rx_length_frames = DATA_LEN;
     controller_req.deassert = 1;
     controller_req.callback = NULL;
-    controller_req.target_sel = &target;
+    controller_req.ts = &ts;
 
     /***** Configure Target (L. Slave) *****/
-    target_init.spi = SPI_TARGET;
-    target_init.freq = SPI_SPEED;
-    target_init.spi_pins = NULL; // Use default, predefined pins
-    target_init.if_mode = MXC_SPI_INTERFACE_STANDARD;
-    target_init.type = MXC_SPI_TYPE_TARGET; // L. Slave
-    target_init.clk_mode = MXC_SPI_CLKMODE_0; // CPOL: 0, CPHA: 0
-    target_init.frame_size = DATA_SIZE;
-    target_init.ts_control =
-        MXC_SPI_TSCONTROL_HW_AUTO; // Target transactions only supports HW_AUTO mode
-    target_init.target.active_polarity = 0;
-    target_init.target.init_mask = 1 << SPI_TARGET_TSIDX; // Initialize Target Select 0 pin.
-    target_init.vssel = MXC_GPIO_VSSEL_VDDIO;
+    target_cfg.spi = SPI_TARGET;
+    target_cfg.clk_mode = MXC_SPI_CLKMODE_0; // CPOL: 0, CPHA: 0
+    target_cfg.frame_size = DATA_SIZE;
 
 #if DMA
-    target_init.use_dma = true;
-    target_init.dma = MXC_DMA;
+    target_cfg.use_dma_tx = true;
+    target_cfg.use_dma_rx = true;
+    target_cfg.dma = MXC_DMA;
 #else
-    target_init.use_dma = false;
+    target_cfg.use_dma_tx = false;
+    target_cfg.use_dma_rx = false;
 #endif
 
-    error = MXC_SPI_Init_v2(&target_init);
+    error = MXC_SPI_Init(SPI_TARGET, MXC_SPI_TYPE_TARGET, MXC_SPI_INTERFACE_STANDARD, 0, 0, SPI_SPEED, target_pins);
     if (error != E_NO_ERROR) {
         printf("\nSPI TARGET INITIALIZATION ERROR\n");
         while (1) {}
     }
 
+    error = MXC_SPI_Config(&target_cfg);
+    if (error != E_NO_ERROR) {
+        printf("\nSPI TARGET CONFIGURATION ERROR\n");
+        while (1) {}
+    }
+
     // Setup Target Request.
     target_req.spi = SPI_TARGET;
-    target_req.tx_buffer = (uint8_t *)target_tx;
-    target_req.tx_fr_len = DATA_LEN;
-    target_req.rx_buffer = (uint8_t *)target_rx;
-    target_req.rx_fr_len = DATA_LEN;
+    target_req.tx_buffer = (uint8_t *)ts_tx;
+    target_req.tx_length_frames = DATA_LEN;
+    target_req.rx_buffer = (uint8_t *)ts_rx;
+    target_req.rx_length_frames = DATA_LEN;
     target_req.deassert = 1;
     target_req.callback = NULL;
 
@@ -242,11 +255,11 @@ int main(void)
     MXC_SPI_ControllerTransaction(&controller_req);
 
     /***** Verify Results *****/
-    if (memcmp(target_rx, controller_tx, sizeof(controller_tx)) != 0) { // Controller->Target
+    if (memcmp(ts_rx, controller_tx, sizeof(controller_tx)) != 0) { // Controller->Target
         printf("\nTarget failed to receive data.\n");
         LED_On(1);
         return E_COMM_ERR;
-    } else if (memcmp(controller_rx, target_tx, sizeof(target_tx)) != 0) { // Target->Controller
+    } else if (memcmp(controller_rx, ts_tx, sizeof(ts_tx)) != 0) { // Target->Controller
         printf("\nController failed to receive data.\n");
         LED_On(1);
         return E_COMM_ERR;
