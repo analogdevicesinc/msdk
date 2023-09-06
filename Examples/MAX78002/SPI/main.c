08bb7f1987f33b4f66bd1acb7973d1801556f019
Jake Carter <jake.carter@analog.com>
2023-09-06 14:58:14 -0500

    clang-format

--- Examples/MAX78002/SPI/main.c
+++ Examples/MAX78002/SPI/main.c
@@ -242,7 +242,7 @@ int main(void)
         }
 
 #else // TSCONTROL_HW_AUTO
-        ts0.index = 0;  // Select TS0 HW pin.
+        ts0.index = 0; // Select TS0 HW pin.
         ts0.active_pol = 0;
 #endif
 
