250865b4f3df181517aaaa6d322cde35ec5fcdea
Erman Okman <Erman.Okman@maximintegrated.com>
2023-09-19 12:41:52 +0300

    cats and dogs demos updated with new cats dogs model parameters trained using dynamic augmentation

--- Examples/MAX78000/CNN/cats-dogs/sampleoutput.h
+++ Examples/MAX78000/CNN/cats-dogs/sampleoutput.h
@@ -1,4 +1,5 @@
-#define SAMPLE_OUTPUT                                                          \
-    {                                                                          \
-        0x50401000, 0xffffffff, 0x00000002, 0x000091b7, 0xffff6de1, 0x00000000 \
-    }
+// This file was @generated automatically
+
+#define SAMPLE_OUTPUT { \
+  0x50401000, 0xffffffff, 0x00000002, 0xffff9810, 0x0000775e, 0x00000000 \
+}
