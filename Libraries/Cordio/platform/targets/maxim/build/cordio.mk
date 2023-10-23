07a7b12f61bd06179bf1ce1969b0925c67432d0a
Eddie <eddie.amaya@analog.com>
2023-10-23 11:27:17 -0500

    Makes  default PAL_NVM_SIZE a multiple of MXC_FLASH_PAGE_SIZE

--- Libraries/Cordio/platform/targets/maxim/build/cordio.mk
+++ Libraries/Cordio/platform/targets/maxim/build/cordio.mk
@@ -64,7 +64,7 @@ PLATFORM        := maxim
 RTOS            ?= baremetal
 
 # Used for storing pairing/bonding information
-PAL_NVM_SIZE	?= 0x2000
+PAL_NVM_SIZE	?= 0x4000
 
 CFG_DEV         := BT_VER=$(BT_VER)
 CFG_DEV         += SCH_CHECK_LIST_INTEGRITY=1
