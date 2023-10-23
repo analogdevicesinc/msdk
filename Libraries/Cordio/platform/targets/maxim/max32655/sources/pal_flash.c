9561b56189c9a7f7ffbcefbe3f92bd27e54f59f3
Eddie <eddie.amaya@analog.com>
2023-10-23 09:25:21 -0500

    Fix PalFlashEraseSector underflowing size

--- Libraries/Cordio/platform/targets/maxim/max32655/sources/pal_flash.c
+++ Libraries/Cordio/platform/targets/maxim/max32655/sources/pal_flash.c
@@ -186,7 +186,7 @@ void PalFlashWrite(void *pBuf, uint32_t size, uint32_t dstAddr)
 /*!
  *  \brief  Erase sector.
  *
- *  \param[in] size       Data size in bytes to be erased.
+ *  \param[in] size       Data size in sectors to be erased.
  *  \param[in] startAddr  Word aligned address.
  *
  *  \return None.
@@ -206,7 +206,7 @@ void PalFlashEraseSector(uint32_t size, uint32_t startAddr)
     MXC_FLC_PageErase(startAddr);
     WsfCsExit();
     startAddr += MXC_FLASH_PAGE_SIZE;
-    size -= MXC_FLASH_PAGE_SIZE;
+    size --;
   }
 }
 
