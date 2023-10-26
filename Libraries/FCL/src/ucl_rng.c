b0e42e2459eb949e3011bace7ab172491e312df5
Sihyung Woo <sihyung.woo@analog.com>
2023-10-26 15:08:24 -0500

    Fix linter in UCL

--- Libraries/FCL/src/ucl_rng.c
+++ Libraries/FCL/src/ucl_rng.c
@@ -1,49 +1,49 @@
 /******************************************************************************
  *
- * Copyright 2023 Analog Devices, Inc.
+ * Copyright 2023 Analog Devices,Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License,Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing,software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  ******************************************************************************
  *
- * Copyright (C) 2023 Maxim Integrated Products, Inc., All Rights Reserved.
+ * Copyright (C) 2023 Maxim Integrated Products,Inc.,All Rights Reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
+ * Permission is hereby granted,free of charge,to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * to deal in the Software without restriction,including without limitation
+ * the rights to use,copy,modify,merge,publish,distribute,sublicense,
+ * and/or sell copies of the Software,and to permit persons to whom the
+ * Software is furnished to do so,subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
- * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND,EXPRESS
+ * OR IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM,DAMAGES
+ * OR OTHER LIABILITY,WHETHER IN AN ACTION OF CONTRACT,TORT OR OTHERWISE,
+ * ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  *
- * Except as contained in this notice, the name of Maxim Integrated
- * Products, Inc. shall not be used except as stated in the Maxim Integrated
- * Products, Inc. Branding Policy.
+ * Except as contained in this notice,the name of Maxim Integrated
+ * Products,Inc. shall not be used except as stated in the Maxim Integrated
+ * Products,Inc. Branding Policy.
  *
  * The mere transfer of this software does not imply any licenses
- * of trade secrets, proprietary technology, copyrights, patents,
- * trademarks, maskwork rights, or any other form of intellectual
- * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * of trade secrets,proprietary technology,copyrights,patents,
+ * trademarks,maskwork rights,or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products,Inc. retains all
  * ownership rights.
  *
  ******************************************************************************/
@@ -60,13 +60,13 @@
 #endif
 
 
-//this is not secure for ECDSA signatures, as being pseudo random number generator
+//this is not secure for ECDSA signatures,as being pseudo random number generator
 //this is for test and demo only
-int ucl_rng_read(u8 *rand, u32 rand_byteLen)
+int ucl_rng_read(u8 *rand,u32 rand_byteLen)
 {
-    int msgi, j;
-    static u8 pseudo[16] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x00, 0x11, 0x22, 0x33, 0x44};
-    u8 output[32], input[16];
+    int msgi,j;
+    static u8 pseudo[16] = {0x11,0x22,0x33,0x44,0x55,0x00,0x11,0x22,0x33,0x44,0x55,0x00,0x11,0x22,0x33,0x44};
+    u8 output[32],input[16];
     u8 blocksize;
     blocksize = 16;
      
@@ -76,7 +76,7 @@ int ucl_rng_read(u8 *rand, u32 rand_byteLen)
             input[j] = pseudo[j];
         }
 
-        ucl_sha256(output, input, blocksize);
+        ucl_sha256(output,input,blocksize);
 
         for (j = 0; j < blocksize; j++) {
             pseudo[j] = output[j];
--- Libraries/FCL/src/ucl_sha512.c
+++ Libraries/FCL/src/ucl_sha512.c
@@ -1,49 +1,49 @@
 /******************************************************************************
  *
- * Copyright 2023 Analog Devices, Inc.
+ * Copyright 2023 Analog Devices,Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License,Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing,software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  ******************************************************************************
  *
- * Copyright (C) 2023 Maxim Integrated Products, Inc., All Rights Reserved.
+ * Copyright (C) 2023 Maxim Integrated Products,Inc.,All Rights Reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
+ * Permission is hereby granted,free of charge,to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * to deal in the Software without restriction,including without limitation
+ * the rights to use,copy,modify,merge,publish,distribute,sublicense,
+ * and/or sell copies of the Software,and to permit persons to whom the
+ * Software is furnished to do so,subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
- * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND,EXPRESS
+ * OR IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM,DAMAGES
+ * OR OTHER LIABILITY,WHETHER IN AN ACTION OF CONTRACT,TORT OR OTHERWISE,
+ * ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  *
- * Except as contained in this notice, the name of Maxim Integrated
- * Products, Inc. shall not be used except as stated in the Maxim Integrated
- * Products, Inc. Branding Policy.
+ * Except as contained in this notice,the name of Maxim Integrated
+ * Products,Inc. shall not be used except as stated in the Maxim Integrated
+ * Products,Inc. Branding Policy.
  *
  * The mere transfer of this software does not imply any licenses
- * of trade secrets, proprietary technology, copyrights, patents,
- * trademarks, maskwork rights, or any other form of intellectual
- * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * of trade secrets,proprietary technology,copyrights,patents,
+ * trademarks,maskwork rights,or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products,Inc. retains all
  * ownership rights.
  *
  ******************************************************************************/
@@ -58,7 +58,7 @@
 #include <ucl/bignum_ecdsa_generic_api.h>
 
 
-void _wsb_ll2b(u8 *dst, u64 src)
+void _wsb_ll2b(u8 *dst,u64 src)
 {
     dst[7] = src & 0xFF;
     src >>= 8;
@@ -77,17 +77,17 @@ void _wsb_ll2b(u8 *dst, u64 src)
     dst[0] = src & 0xFF;
 }
 
-void swapcpy_ll2b(u8 *dst, const u64 *src, u32 wordlen)
+void swapcpy_ll2b(u8 *dst,const u64 *src,u32 wordlen)
 {
     int i;
 
     for (i = 0; i < (int)wordlen; i++) {
-        _wsb_ll2b(dst, src[i]);
+        _wsb_ll2b(dst,src[i]);
         dst += 8;
     }
 }
 
-void swapcpy_b2b64(u8 *dst, u8 *src, u32 wordlen)
+void swapcpy_b2b64(u8 *dst,u8 *src,u32 wordlen)
 {
     u8 tmp;
     int i;
@@ -131,9 +131,9 @@ int ucl_sha512_init(ucl_sha512_ctx_t *ctx)
     return UCL_OK;
 }
 
-int ucl_sha512_core(ucl_sha512_ctx_t *ctx, u8 *data, u32 dataLen)
+int ucl_sha512_core(ucl_sha512_ctx_t *ctx,u8 *data,u32 dataLen)
 {
-    u32 indexh, partLen, i;
+    u32 indexh,partLen,i;
 
     if (ctx == NULL) {
         return UCL_INVALID_INPUT;
@@ -149,13 +149,13 @@ int ucl_sha512_core(ucl_sha512_ctx_t *ctx, u8 *data, u32 dataLen)
     partLen = 128 - indexh;
 
     if (dataLen >= partLen) {
-        memcpy(&ctx->buffer[indexh], data, partLen);
-        swapcpy_b2b64(ctx->buffer, ctx->buffer, 16);
-        sha512_stone(ctx->state, (u64 *) ctx->buffer);
+        memcpy(&ctx->buffer[indexh],data,partLen);
+        swapcpy_b2b64(ctx->buffer,ctx->buffer,16);
+        sha512_stone(ctx->state,(u64 *) ctx->buffer);
 
         for (i = partLen; i + 127 < dataLen; i += 128) {
-            swapcpy_b2b64(ctx->buffer, &data[i], 16);
-            sha512_stone(ctx->state, (u64 *) ctx->buffer);
+            swapcpy_b2b64(ctx->buffer,&data[i],16);
+            sha512_stone(ctx->state,(u64 *) ctx->buffer);
         }
 
         indexh = 0;
@@ -163,18 +163,18 @@ int ucl_sha512_core(ucl_sha512_ctx_t *ctx, u8 *data, u32 dataLen)
         i = 0;
     }
 
-    memcpy(&ctx->buffer[indexh], &data[i], dataLen - i);
+    memcpy(&ctx->buffer[indexh],&data[i],dataLen - i);
     return UCL_OK;
 }
 
-int ucl_sha512_finish(u8 *hash, ucl_sha512_ctx_t *ctx)
+int ucl_sha512_finish(u8 *hash,ucl_sha512_ctx_t *ctx)
 {
     u8 bits[16];
-    u64 indexh, padLen;
+    u64 indexh,padLen;
     u8 padding[128];
 
     padding[0] = 0x80;
-    memset(padding + 1, 0, 127);
+    memset(padding + 1,0,127);
 
     if (hash == NULL) {
         return UCL_INVALID_OUTPUT;
@@ -184,18 +184,18 @@ int ucl_sha512_finish(u8 *hash, ucl_sha512_ctx_t *ctx)
         return UCL_INVALID_INPUT;
     }
 
-    swapcpy_ll2b(bits, ctx->count, 2);
+    swapcpy_ll2b(bits,ctx->count,2);
     indexh = (u32)((ctx->count[1] >> 3) & 0x7f);
     padLen = (indexh < 112) ? (112 - indexh) : (240 - indexh);
 
-    ucl_sha512_core(ctx, padding, (u32)padLen);
-    ucl_sha512_core(ctx, bits, 16);
-    swapcpy_ll2b(hash, ctx->state, 8);
+    ucl_sha512_core(ctx,padding,(u32)padLen);
+    ucl_sha512_core(ctx,bits,16);
+    swapcpy_ll2b(hash,ctx->state,8);
 
     return UCL_OK;
 }
 
-int ucl_sha512(u8 *hash, u8 *message, u32 byteLength)
+int ucl_sha512(u8 *hash,u8 *message,u32 byteLength)
 {
     ucl_sha512_ctx_t ctx;
 
@@ -204,8 +204,8 @@ int ucl_sha512(u8 *hash, u8 *message, u32 byteLength)
     }
 
     ucl_sha512_init(&ctx);
-    ucl_sha512_core(&ctx, message, byteLength);
-    ucl_sha512_finish(hash, &ctx);
+    ucl_sha512_core(&ctx,message,byteLength);
+    ucl_sha512_finish(hash,&ctx);
     return UCL_OK;
 }
 
