18020ecbdc44bb5ca60c174230c060b5903c10a0
Jake Carter <jhcarter.co@gmail.com>
2023-11-07 17:44:34 -0600

    Add copyrights

--- Libraries/MiscDrivers/Display/fonts/fonts.h
+++ Libraries/MiscDrivers/Display/fonts/fonts.h
@@ -1,3 +1,20 @@
+/******************************************************************************
+ *
+ * Copyright 2023 Analog Devices, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
 /**
  * @file fonts.h
@@ -13,44 +30,45 @@
 
 #ifdef FONT_LiberationSans12x12
 extern const unsigned char Liberation_Sans12x12[];
-#ifndef _font_table
-#define _font_table Liberation_Sans12x12
+#ifndef _font_char_table
+#define _font_char_table Liberation_Sans12x12
 #endif
 #endif
 
 #ifdef FONT_LiberationSans16x16
 extern const unsigned char Liberation_Sans16x16[];
-#ifndef _font_table
-#define _font_table Liberation_Sans16x16
+#ifndef _font_char_table
+#define _font_char_table Liberation_Sans16x16
 #endif
 #endif
 
 #ifdef FONT_LiberationSans19x19
 extern const unsigned char Liberation_Sans19x19[];
-#ifndef _font_table
-#define _font_table Liberation_Sans19x19
+#ifndef _font_char_table
+#define _font_char_table Liberation_Sans19x19
 #endif
 #endif
 
 #ifdef FONT_LiberationSans24x24
 extern const unsigned char Liberation_Sans24x24[];
-#ifndef _font_table
-#define _font_table Liberation_Sans24x24
+#ifndef _font_char_table
+#define _font_char_table Liberation_Sans24x24
 #endif
 #endif
 
 #ifdef FONT_LiberationSans28x28
 extern const unsigned char Liberation_Sans28x28[];
-#ifndef _font_table
-#define _font_table Liberation_Sans28x28
+#ifndef _font_char_table
+#define _font_char_table Liberation_Sans28x28
 #endif
 #endif
 
 /**
  * @brief font table macro that can be used to access the selected font.  When multiple
  * fonts are enabled, the font table will point to the first font in the list.
+ * 
  */
-#define font_table _font_table
+#define font_char_table _font_char_table
 
 #endif
 
