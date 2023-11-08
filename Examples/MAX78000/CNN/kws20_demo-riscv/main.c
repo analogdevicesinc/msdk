40e8cc9e65ce86d27b5fc09e4b9a28b6ff296faa
Jake Carter <jhcarter.co@gmail.com>
2023-11-07 18:21:38 -0600

    Restore kws20_demo-riscv project, use new fonts

--- Examples/MAX78000/CNN/kws20_demo-riscv/main.c
+++ Examples/MAX78000/CNN/kws20_demo-riscv/main.c
@@ -246,6 +246,6 @@ void TFT_End(uint16_t words)
     MXC_TFT_ClearScreen();
     TFT_Print(buff, 70, 30, font_2, snprintf(buff, sizeof(buff), "Demo Stopped!"));
     TFT_Print(buff, 10, 60, font_1, snprintf(buff, sizeof(buff), "Number of words: %d ", words));
-    TFT_Print(buff, 0, 180, font_1, snprintf(buff, sizeof(buff), "PRESS RESET TO TRY AGAIN!"));
+    TFT_Print(buff, 20, 180, font_1, snprintf(buff, sizeof(buff), "PRESS RESET TO TRY AGAIN!"));
 }
 #endif
--- Examples/MAX78000/CNN/kws20_demo-riscv/project.mk
+++ Examples/MAX78000/CNN/kws20_demo-riscv/project.mk
@@ -5,27 +5,21 @@
 # For instructions on how to use this system, see
 # https://analog-devices-msdk.github.io/msdk/USERGUIDE/#build-system
 
-# Uncomment the line below to build for the MAX78000FTHR
-#BOARD=FTHR_RevA
+# **********************************************************
 
-# Set a higher optimization level.  The increased performance
-# is required for the CameraIF DMA code to work within the
-# timing requirements of the Parallel Camera Interface.
-MXC_OPTIMIZE_CFLAGS = -O2
+ifeq "$(BOARD)" ""
+BOARD = EvKit_V1
+endif
 
 # Place build files specific to EvKit_V1 here.
 ifeq "$(BOARD)" "EvKit_V1"
 PROJ_CFLAGS+=-DENABLE_TFT
-IPATH += TFT/evkit/
-VPATH += TFT/evkit/
 endif
 
 # Place build files specific to FTHR_RevA here.
 ifeq "$(BOARD)" "FTHR_RevA"
 # Only Enable if 2.4" TFT is connected to Feather
-#PROJ_CFLAGS+=-DENABLE_TFT
-IPATH += TFT/fthr
-VPATH += TFT/fthr
+# PROJ_CFLAGS+=-DENABLE_TFT
 FONTS = LiberationSans16x16
 endif
 
