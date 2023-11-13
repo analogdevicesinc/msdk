e3694866698a720403e3ece58af690baf8d3a197
Sihyung Woo <sihyung.woo@analog.com>
2023-11-13 11:17:55 -0600

    Remove unsupported files from ME55EVKIT board.mk

--- Libraries/Boards/MAX32572/EvKit_V1/board.mk
+++ Libraries/Boards/MAX32572/EvKit_V1/board.mk
@@ -44,12 +44,9 @@ SRCS += stdio.c
 SRCS += led.c
 SRCS += pb.c
 SRCS += tsc2046.c
-# SRCS += tft.c
+SRCS += tft_ssd2119.c
 
-# PROJ_CFLAGS+=-DEXT_FLASH_MX25
-
-# MISC_DRIVERS_DIR ?= $(MAXIM_PATH)/Libraries/MiscDrivers
-MISC_DRIVERS_DIR ?= $(LIBS_DIR)/MiscDrivers
+MISC_DRIVERS_DIR ?= $(MAXIM_PATH)/Libraries/MiscDrivers
 
 # Where to find BSP source files
 VPATH += $(BOARD_DIR)/Source
