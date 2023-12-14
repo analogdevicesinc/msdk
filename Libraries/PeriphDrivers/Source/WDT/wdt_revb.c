942782ade8d68bd58ee16e476830289d7bcc5f7a
Furkan Akkiz <hasanfurkan.akkiz@analog.com>
2023-12-14 12:28:09 +0300

    Fix unused variable warning
    clock_source_num parameter causes warning, fixed it.
    
    Signed-off-by: Furkan Akkiz <hasanfurkan.akkiz@analog.com>

--- Libraries/PeriphDrivers/Source/WDT/wdt_revb.c
+++ Libraries/PeriphDrivers/Source/WDT/wdt_revb.c
@@ -158,6 +158,7 @@ void MXC_WDT_RevB_ClearIntFlag(mxc_wdt_revb_regs_t *wdt)
 void MXC_WDT_RevB_SetClockSource(mxc_wdt_revb_regs_t *wdt, int clock_source)
 {
     const uint8_t clock_source_num = 8; // Max number of clock sources for Rev B WDT
+    (void)clock_source_num;
 
     MXC_ASSERT((clock_source < clock_source_num) && (clock_source >= 0));
     MXC_SETFIELD(wdt->clksel, MXC_F_WDT_REVB_CLKSEL_SOURCE,
