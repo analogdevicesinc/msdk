f19ebab4a9b57864d2cf0a5f4ec5ea4e97b8d504
Kenan Balci <kenan.balci@analog.com>
2024-01-19 13:41:43 +0300

    feat(Examples): Add 3 Wire SPI example for MAX32665

---
+++ Examples/MAX32665/SPI_3Wire/project.mk
@@ -0,0 +1,15 @@
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://analog-devices-msdk.github.io/msdk/USERGUIDE/#build-system
+
+# **********************************************************
+
+# Add your config here!
+
+# If you have secure version of MCU (MAX32666), set SBT=1 to generate signed binary
+# For more information on how sing process works, see
+# https://www.analog.com/en/education/education-library/videos/6313214207112.html
+SBT=0
