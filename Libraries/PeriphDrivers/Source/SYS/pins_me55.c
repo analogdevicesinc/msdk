8a56b2fcf22a687a71051e82a99ed473a029c881
Sihyung Woo <sihyung.woo@analog.com>
2024-01-24 10:52:43 -0600

    fix(PeriphDrivers): Remove whitespace to fix linter error

--- Libraries/PeriphDrivers/Source/SYS/pins_me55.c
+++ Libraries/PeriphDrivers/Source/SYS/pins_me55.c
@@ -1,6 +1,6 @@
 /**
- * @file mxc_pins.c
- * @brief      This file contains constant pin configurations for the peripherals.
+ * @file    mxc_pins.c
+ * @brief   This file contains constant pin configurations for the peripherals.
  */
 
 /******************************************************************************
@@ -185,5 +185,5 @@ const mxc_gpio_cfg_t gpio_cfg_spixf = { MXC_GPIO1, (MXC_GPIO_PIN_22 | MXC_GPIO_P
 // SKBD Pin Definitions
 const mxc_gpio_cfg_t gpio_cfg_skbd_P2 = { MXC_GPIO1, ( MXC_GPIO_PIN_14 | MXC_GPIO_PIN_15 | MXC_GPIO_PIN_16 |
                                           MXC_GPIO_PIN_17 | MXC_GPIO_PIN_18| MXC_GPIO_PIN_19 | MXC_GPIO_PIN_20 |
-                                          MXC_GPIO_PIN_21), MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_PULL_UP, 
+                                          MXC_GPIO_PIN_21), MXC_GPIO_FUNC_ALT1, MXC_GPIO_PAD_PULL_UP,
                                           MXC_GPIO_VSSEL_VDDIO, MXC_GPIO_DRVSTR_0 };
