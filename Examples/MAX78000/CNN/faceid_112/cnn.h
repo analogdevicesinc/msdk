f431de8aaa71ddb4d0f4c80b39d4c287b4326369
Jake Carter <jhcarter.co@gmail.com>
2024-01-25 16:57:05 -0600

    clang-format faceid_112

--- Examples/MAX78000/CNN/faceid_112/cnn.h
+++ Examples/MAX78000/CNN/faceid_112/cnn.h
@@ -1,9 +1,23 @@
-/**************************************************************************************************
-* Copyright (C) 2019-2021 Maxim Integrated Products, Inc. All Rights Reserved.
-*
-* Maxim Integrated Products, Inc. Default Copyright Notice:
-* https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
-**************************************************************************************************/
+/******************************************************************************
+ *
+ * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. All Rights Reserved.
+ * (now owned by Analog Devices, Inc.),
+ * Copyright (C) 2023-2024 Analog Devices, Inc. All Rights Reserved. This software
+ * is proprietary to Analog Devices, Inc. and its licensors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
 /*
  * This header file was automatically @generated for the faceid_112 network from a template.
@@ -62,9 +76,10 @@ typedef int16_t q15_t;
 #define SYS_COMPLETE LED_Off(0)
 
 /* Run software SoftMax on unloaded data */
-void softmax_q17p14_q15(const q31_t * vec_in, const uint16_t dim_vec, q15_t * p_out);
+void softmax_q17p14_q15(const q31_t *vec_in, const uint16_t dim_vec, q15_t *p_out);
 /* Shift the input, then calculate SoftMax */
-void softmax_shift_q17p14_q15(q31_t * vec_in, const uint16_t dim_vec, uint8_t in_shift, q15_t * p_out);
+void softmax_shift_q17p14_q15(q31_t *vec_in, const uint16_t dim_vec, uint8_t in_shift,
+                              q15_t *p_out);
 
 /* Stopwatch - holds the runtime when accelerator finishes */
 extern volatile uint32_t cnn_time;
