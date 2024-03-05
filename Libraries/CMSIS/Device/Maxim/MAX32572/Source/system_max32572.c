3774bf7f239bf931f609c2b3225edac495053635
Woo <Sihyung.Woo@analog.com>
2024-03-05 11:59:49 -0600

    Add SRAM code section in ME55 linkers, Fix SFCC build error

--- Libraries/CMSIS/Device/Maxim/MAX32572/Source/system_max32572.c
+++ Libraries/CMSIS/Device/Maxim/MAX32572/Source/system_max32572.c
@@ -22,10 +22,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "max32572.h"
+#include "mxc_device.h"
 #include "gcr_regs.h"
 #include "mxc_sys.h"
 #include "usbhs_regs.h"
+#include "sfcc.h"
 
 extern void (*const __isr_vector[])(void);
 
