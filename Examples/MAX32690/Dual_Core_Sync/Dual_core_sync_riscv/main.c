d13c3b2ced3056b080e0a01254507b6f8e7f625b
Woo <Sihyung.Woo@analog.com>
2024-03-06 10:42:01 -0600

    Run msdkgen on ME18 Dual Core examples

--- Examples/MAX32690/Dual_Core_Sync/Dual_core_sync_riscv/main.c
+++ Examples/MAX32690/Dual_Core_Sync/Dual_core_sync_riscv/main.c
@@ -1,7 +1,6 @@
 /******************************************************************************
  *
- * Copyright (C) 2024 Analog Devices, Inc. All Rights Reserved. This software
- * is proprietary to Analog Devices, Inc. and its licensors.
+ * Copyright (C) 2024 Analog Devices, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
