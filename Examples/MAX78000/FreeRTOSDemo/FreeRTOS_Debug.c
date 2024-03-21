0386d1e91cb277b131a0f3157102adb64ea1895c
Brandon-Hurst <brandon.hurst@analog.com>
2024-03-20 17:20:39 -0700

    Format FreeRTOS files

--- Examples/MAX78000/FreeRTOSDemo/FreeRTOS_Debug.c
+++ Examples/MAX78000/FreeRTOSDemo/FreeRTOS_Debug.c
@@ -112,22 +112,20 @@ void vApplicationStackOverflowHook(TaskHandle_t xTask, signed char *pcTaskName)
 }
 
 /* The prototype shows it is a naked function - in effect this is just an
-assembly function. */\
+assembly function. */
 void HardFault_Handler(void) __attribute__((naked, aligned(8)));
 
 /* The fault handler implementation calls a function called
 prvGetRegistersFromStack(). */
 void HardFault_Handler(void)
 {
-    __asm volatile
-    (
-        " tst lr, #4                                                \n"
-        " ite eq                                                    \n"
-        " mrseq r0, msp                                             \n"
-        " mrsne r0, psp                                             \n"
-        " ldr r1, [r0, #24]                                         \n"
-        " ldr r2, handler2_address_const                            \n"
-        " bx r2                                                     \n"
-        " handler2_address_const: .word prvGetRegistersFromStack    \n");
+    __asm volatile(" tst lr, #4                                                \n"
+                   " ite eq                                                    \n"
+                   " mrseq r0, msp                                             \n"
+                   " mrsne r0, psp                                             \n"
+                   " ldr r1, [r0, #24]                                         \n"
+                   " ldr r2, handler2_address_const                            \n"
+                   " bx r2                                                     \n"
+                   " handler2_address_const: .word prvGetRegistersFromStack    \n");
 }
 #endif
