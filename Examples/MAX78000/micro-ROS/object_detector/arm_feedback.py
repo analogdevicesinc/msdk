5d15a962883f2b167fbba01f002a8e7fff9721e9
Jake Carter <jhcarter.co@gmail.com>
2024-03-26 17:56:12 -0600

    Reduce drop height compensation by 20%

--- Examples/MAX78000/micro-ROS/object_detector/arm_feedback.py
+++ Examples/MAX78000/micro-ROS/object_detector/arm_feedback.py
@@ -501,7 +501,7 @@ class PolygonSubscriber(Node):
             elif self.state == 6: # Move back to home
                 global height_drop
                 global drop_angle
-                height_drop = (deepcopy(present_kinematics_pose[2]) * 1.3)
+                height_drop = (deepcopy(present_kinematics_pose[2]) * 1.1)
                 drop_angle = deepcopy(present_kinematics_pose[3:6])
                 goal_joint_angle = home
 
