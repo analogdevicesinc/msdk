a5782360bc3775fc042b96581d5c9228cbad06a4
Brandon Hurst <82959533+Brandon-Hurst@users.noreply.github.com>
2024-03-28 13:41:09 -0600

    feat(Examples): Add RTOS Debug Features to MAX78000 FreeRTOSDemo (#962)

--- Examples/MAX78000/FreeRTOSDemo/FreeRTOSConfig.h
+++ Examples/MAX78000/FreeRTOSDemo/FreeRTOSConfig.h
@@ -24,6 +24,27 @@
 #include <stdint.h>
 #include "max78000.h"
 
+// MSDK Debug flags
+#if defined(DEBUG) && DEBUG == 1
+// RTOS Stats Timer Functions
+extern void ConfigTimerForStats(void);
+extern uint32_t GetTimerForStats(void);
+#define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() (ConfigTimerForStats())
+#define portGET_RUN_TIME_COUNTER_VALUE() (GetTimerForStats())
+#define configGENERATE_RUN_TIME_STATS 1
+
+// RTOS Assert & other debug configuration
+#define configASSERT(x)           \
+    if ((x) == 0) {               \
+        taskDISABLE_INTERRUPTS(); \
+        for (;;) {}               \
+    }
+#define configENABLE_BACKWARD_COMPATIBILITY 1
+#define configQUEUE_REGISTRY_SIZE 10
+#define configRECORD_STACK_HIGH_ADDRESS 1
+
+#endif
+
 /*-----------------------------------------------------------
  * Application specific definitions.
  *
