079fe434224cccefa48bff358892cf8e95b71a6b
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-04-12 13:25:31 -0500

    52174ec453d3fb6a21b802807051cdf4f596fb02

--- Examples/MAX32690/Bluetooth/BLE_FreeRTOS/freertos_tickless.c
+++ Examples/MAX32690/Bluetooth/BLE_FreeRTOS/freertos_tickless.c
@@ -233,10 +233,10 @@ void vPortSuppressTicksAndSleep(TickType_t xExpectedIdleTime)
         if (schTimerActive) {
             /* Stop the BLE scheduler timer */
             PalTimerStop();
-
-            /* Shutdown BB hardware */
-            PalBbDisable();
         }
+
+        /* Shutdown BB hardware */
+        PalBbDisable();
 
         LED_Off(SLEEP_LED);
         LED_Off(DEEPSLEEP_LED);
@@ -246,12 +246,11 @@ void vPortSuppressTicksAndSleep(TickType_t xExpectedIdleTime)
         LED_On(DEEPSLEEP_LED);
         LED_On(SLEEP_LED);
 
+        /* Enable and restore the BB hardware */
+        PalBbEnable();
+        PalBbRestore();
+
         if (schTimerActive) {
-            /* Enable and restore the BB hardware */
-            PalBbEnable();
-
-            PalBbRestore();
-
             /* Restore the BB counter */
             MXC_WUT_RestoreBBClock(BB_CLK_RATE_HZ);
 
