92678b94a118a0aae51f3d77a7dbf1993a292ef4
Sadik Ozer <46590392+ozersa@users.noreply.github.com>
2024-05-04 03:47:21 +0300

    fix(PeriphDrivers): Fix MAX32675 timer warning (#1007)
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- Libraries/PeriphDrivers/Source/TMR/tmr_me16.c
+++ Libraries/PeriphDrivers/Source/TMR/tmr_me16.c
@@ -25,14 +25,14 @@
 
 int MXC_TMR_Init(mxc_tmr_regs_t *tmr, mxc_tmr_cfg_t *cfg, bool init_pins)
 {
-    uint8_t tmr_id;
+    uint8_t tmr_id = MXC_TMR_GET_IDX(tmr);
     uint8_t clockSource = MXC_TMR_CLK0;
 
     if (cfg == NULL) {
         return E_NULL_PTR;
     }
 
-    MXC_ASSERT((tmr_id = MXC_TMR_GET_IDX(tmr)) >= 0);
+    MXC_ASSERT(tmr_id >= 0);
 
     switch (cfg->clock) {
     case MXC_TMR_EXT_CLK:
