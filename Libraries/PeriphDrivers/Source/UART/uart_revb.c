713a45c4b67989f9800ee4502edc25c257ecfe8d
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-05-10 08:35:03 -0500

    removed extra newline from revb driver

--- Libraries/PeriphDrivers/Source/UART/uart_revb.c
+++ Libraries/PeriphDrivers/Source/UART/uart_revb.c
@@ -755,18 +755,12 @@ int MXC_UART_RevB_AsyncHandler(mxc_uart_revb_regs_t *uart)
     /* Unexpected interrupt */
     if (!AsyncTxRequests[uart_num] && !AsyncRxRequests[uart_num]) {
         MXC_UART_ClearFlags((mxc_uart_regs_t *)uart, uart->int_fl);
-
         return E_INVALID;
     }
 
     if (flags & MXC_UART_REVB_ERRINT_FL & uart->int_en) {
         MXC_UART_AsyncStop((mxc_uart_regs_t *)uart);
         MXC_UART_AsyncCallback((mxc_uart_regs_t *)uart, E_COMM_ERR);
-
-        return E_INVALID;
-    }
-
-    req = (mxc_uart_req_t *)AsyncTxRequests[uart_num];
     if ((req != NULL) && (req->txLen)) {
         numToWrite = MXC_UART_GetTXFIFOAvailable((mxc_uart_regs_t *)(req->uart));
         numToWrite = numToWrite > (req->txLen - req->txCnt) ? req->txLen - req->txCnt : numToWrite;
