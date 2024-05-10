c25f62c0ff0e22f20cde9ae6269f7d8fa20b5019
Woo <Sihyung.Woo@analog.com>
2024-05-10 16:28:27 -0500

    Revert to original

--- Libraries/PeriphDrivers/Include/MAX78002/spi.h
+++ Libraries/PeriphDrivers/Include/MAX78002/spi.h
@@ -593,7 +593,6 @@ unsigned int MXC_SPI_GetTXThreshold(mxc_spi_regs_t *spi);
 unsigned int MXC_SPI_GetRXThreshold(mxc_spi_regs_t *spi);
 
 //////>>> Previous Implementation
-
 /**
  * @brief   Sets the number of bits per character
  *
