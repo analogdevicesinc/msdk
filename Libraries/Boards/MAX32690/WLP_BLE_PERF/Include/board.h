38d570c6e711ad666dc07fa8c32b96252d906206
Kevin Gillespie <Kevin.Gillespie@analog.com>
2024-05-24 11:39:53 -0500

    Adding WLP_BLE_PERF board.

---
+++ Libraries/Boards/MAX32690/WLP_BLE_PERF/Include/board.h
@@ -0,0 +1,107 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. All Rights Reserved.
+ * (now owned by Analog Devices, Inc.),
+ * Copyright (C) 2023 Analog Devices, Inc. All Rights Reserved. This software
+ * is proprietary to Analog Devices, Inc. and its licensors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+/**
+ * @file    board.h
+ * @brief   Board support package API.
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifndef LIBRARIES_BOARDS_MAX32690_WLP_V1_INCLUDE_BOARD_H_
+#define LIBRARIES_BOARDS_MAX32690_WLP_V1_INCLUDE_BOARD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef CONSOLE_UART
+#define CONSOLE_UART 1
+#endif
+
+#ifndef CONSOLE_BAUD
+#define CONSOLE_BAUD 115200 /// Console baud rate
+#endif
+
+#ifndef HCI_UART
+#define HCI_UART 0
+#endif
+
+#ifndef TERMINAL_UART
+#define TERMINAL_UART CONSOLE_UART
+#endif
+
+#ifndef USER_UART
+#define USER_UART 1
+#endif
+
+#ifdef LED_OFF
+#undef LED_OFF
+#endif
+#define LED_OFF 1 /// Override inactive state of LEDs
+
+#ifdef LED_ON
+#undef LED_ON
+#endif
+#define LED_ON 0 /// Override active state of LEDs
+
+
+#define WLP_VBLE_PERF
+
+/**
+ *  A reference to LED2 (GREEN LED in the RGB LED) of the board.
+ *  Can be used with the LED_On, LED_Off, and LED_Toggle functions.
+ */
+#define LED0 0
+#define LED_GREEN LED0
+#define LED_RED LED0
+
+/**
+ * \brief   Initialize the BSP and board interfaces.
+ * \returns #E_NO_ERROR if everything is successful
+ */
+int Board_Init(void);
+
+/**
+ * \brief   Initialize or reinitialize the console. This may be necessary if the
+ *          system clock rate is changed.
+ * \returns #E_NO_ERROR if everything is successful
+ */
+int Console_Init(void);
+
+/**
+ * \brief   Shutdown the console.
+ * \returns #E_NO_ERROR if everything is successful
+ */
+int Console_Shutdown(void);
+
+/**
+ * \brief   Attempt to prepare the console for sleep.
+ * \returns #E_NO_ERROR if ready to sleep, #E_BUSY if not ready for sleep.
+ */
+int Console_PrepForSleep(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBRARIES_BOARDS_MAX32690_WLP_V1_INCLUDE_BOARD_H_
---
+++ Libraries/Boards/MAX32690/WLP_BLE_PERF/board.mk
@@ -0,0 +1,44 @@
+###############################################################################
+ #
+ # Copyright (C) 2022-2023 Maxim Integrated Products, Inc. All Rights Reserved.
+ # (now owned by Analog Devices, Inc.),
+ # Copyright (C) 2023 Analog Devices, Inc. All Rights Reserved. This software
+ # is proprietary to Analog Devices, Inc. and its licensors.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+ ##############################################################################
+
+ifeq "$(BOARD_DIR)" ""
+$(error BOARD_DIR must be set)
+endif
+
+# Source files for this test (add path to VPATH below)
+SRCS += board.c
+SRCS += stdio.c
+SRCS += led.c
+SRCS += pb.c
+
+MISC_DRIVERS_DIR ?= $(MAXIM_PATH)/Libraries/MiscDrivers
+
+# Where to find BSP source files
+VPATH += $(BOARD_DIR)/Source
+VPATH += $(MISC_DRIVERS_DIR)
+VPATH += $(MISC_DRIVERS_DIR)/LED
+VPATH += $(MISC_DRIVERS_DIR)/PushButton
+
+# Where to find BSP header files
+IPATH += $(BOARD_DIR)/Include
+IPATH += $(MISC_DRIVERS_DIR)
+IPATH += $(MISC_DRIVERS_DIR)/LED
+IPATH += $(MISC_DRIVERS_DIR)/PushButton
