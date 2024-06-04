2a3d743c56370b0462d06e68d5bac477a97d213d
Woo <Sihyung.Woo@analog.com>
2024-06-04 15:28:09 -0500

    Revert MPC register names to match what ARM has

--- Libraries/CMSIS/Device/Maxim/MAX32657/Include/mpc_regs.h
+++ Libraries/CMSIS/Device/Maxim/MAX32657/Include/mpc_regs.h
@@ -74,24 +74,24 @@ extern "C" {
 typedef struct {
     __IO uint32_t ctrl;                 /**< <tt>\b 0x0000:</tt> MPC CTRL Register */
     __R  uint32_t rsv_0x4_0xf[3];
-    __I  uint32_t blkmax;               /**< <tt>\b 0x0010:</tt> MPC BLKMAX Register */
-    __I  uint32_t blkctrl;              /**< <tt>\b 0x0014:</tt> MPC BLKCTRL Register */
-    __IO uint32_t blkidx;               /**< <tt>\b 0x0018:</tt> MPC BLKIDX Register */
-    __IO uint32_t blklut;               /**< <tt>\b 0x001C:</tt> MPC BLKLUT Register */
-    __I  uint32_t intfl;                /**< <tt>\b 0x0020:</tt> MPC INTFL Register */
-    __O  uint32_t intclr;               /**< <tt>\b 0x0024:</tt> MPC INTCLR Register */
-    __IO uint32_t inten;                /**< <tt>\b 0x0028:</tt> MPC INTEN Register */
-    __I  uint32_t intinfo0;             /**< <tt>\b 0x002C:</tt> MPC INTINFO0 Register */
-    __I  uint32_t intinfo1;             /**< <tt>\b 0x0030:</tt> MPC INTINFO1 Register */
-    __O  uint32_t intset;               /**< <tt>\b 0x0034:</tt> MPC INTSET Register */
+    __I  uint32_t blk_max;              /**< <tt>\b 0x0010:</tt> MPC BLK_MAX Register */
+    __I  uint32_t blk_cfg;              /**< <tt>\b 0x0014:</tt> MPC BLK_CFG Register */
+    __IO uint32_t blk_idx;              /**< <tt>\b 0x0018:</tt> MPC BLK_IDX Register */
+    __IO uint32_t blk_lut;              /**< <tt>\b 0x001C:</tt> MPC BLK_LUT Register */
+    __I  uint32_t int_stat;             /**< <tt>\b 0x0020:</tt> MPC INT_STAT Register */
+    __O  uint32_t int_clear;            /**< <tt>\b 0x0024:</tt> MPC INT_CLEAR Register */
+    __IO uint32_t int_en;               /**< <tt>\b 0x0028:</tt> MPC INT_EN Register */
+    __I  uint32_t int_info1;            /**< <tt>\b 0x002C:</tt> MPC INT_INFO1 Register */
+    __I  uint32_t int_info2;            /**< <tt>\b 0x0030:</tt> MPC INT_INFO2 Register */
+    __O  uint32_t int_set;              /**< <tt>\b 0x0034:</tt> MPC INT_SET Register */
     __R  uint32_t rsv_0x38_0xfcf[998];
-    __I  uint32_t periphid4;            /**< <tt>\b 0x0FD0:</tt> MPC PERIPHID4 Register */
+    __I  uint32_t pidr4;                /**< <tt>\b 0x0FD0:</tt> MPC PIDR4 Register */
     __R  uint32_t rsv_0xfd4_0xfdf[3];
-    __I  uint32_t periphid0;            /**< <tt>\b 0x0FE0:</tt> MPC PERIPHID0 Register */
-    __I  uint32_t periphid1;            /**< <tt>\b 0x0FE4:</tt> MPC PERIPHID1 Register */
-    __I  uint32_t periphid2;            /**< <tt>\b 0x0FE8:</tt> MPC PERIPHID2 Register */
-    __I  uint32_t periphid3;            /**< <tt>\b 0x0FEC:</tt> MPC PERIPHID3 Register */
-    __I  uint32_t compid[4];            /**< <tt>\b 0x0FF0:</tt> MPC COMPID Register */
+    __I  uint32_t pidr0;                /**< <tt>\b 0x0FE0:</tt> MPC PIDR0 Register */
+    __I  uint32_t pidr1;                /**< <tt>\b 0x0FE4:</tt> MPC PIDR1 Register */
+    __I  uint32_t pidr2;                /**< <tt>\b 0x0FE8:</tt> MPC PIDR2 Register */
+    __I  uint32_t pidr3;                /**< <tt>\b 0x0FEC:</tt> MPC PIDR3 Register */
+    __I  uint32_t cidr[4];              /**< <tt>\b 0x0FF0:</tt> MPC CIDR Register */
 } mxc_mpc_regs_t;
 
 /* Register offsets for module MPC */
@@ -102,22 +102,22 @@ typedef struct {
  * @{
  */
 #define MXC_R_MPC_CTRL                     ((uint32_t)0x00000000UL) /**< Offset from MPC Base Address: <tt> 0x0000</tt> */
-#define MXC_R_MPC_BLKMAX                   ((uint32_t)0x00000010UL) /**< Offset from MPC Base Address: <tt> 0x0010</tt> */
-#define MXC_R_MPC_BLKCTRL                  ((uint32_t)0x00000014UL) /**< Offset from MPC Base Address: <tt> 0x0014</tt> */
-#define MXC_R_MPC_BLKIDX                   ((uint32_t)0x00000018UL) /**< Offset from MPC Base Address: <tt> 0x0018</tt> */
-#define MXC_R_MPC_BLKLUT                   ((uint32_t)0x0000001CUL) /**< Offset from MPC Base Address: <tt> 0x001C</tt> */
-#define MXC_R_MPC_INTFL                    ((uint32_t)0x00000020UL) /**< Offset from MPC Base Address: <tt> 0x0020</tt> */
-#define MXC_R_MPC_INTCLR                   ((uint32_t)0x00000024UL) /**< Offset from MPC Base Address: <tt> 0x0024</tt> */
-#define MXC_R_MPC_INTEN                    ((uint32_t)0x00000028UL) /**< Offset from MPC Base Address: <tt> 0x0028</tt> */
-#define MXC_R_MPC_INTINFO0                 ((uint32_t)0x0000002CUL) /**< Offset from MPC Base Address: <tt> 0x002C</tt> */
-#define MXC_R_MPC_INTINFO1                 ((uint32_t)0x00000030UL) /**< Offset from MPC Base Address: <tt> 0x0030</tt> */
-#define MXC_R_MPC_INTSET                   ((uint32_t)0x00000034UL) /**< Offset from MPC Base Address: <tt> 0x0034</tt> */
-#define MXC_R_MPC_PERIPHID4                ((uint32_t)0x00000FD0UL) /**< Offset from MPC Base Address: <tt> 0x0FD0</tt> */
-#define MXC_R_MPC_PERIPHID0                ((uint32_t)0x00000FE0UL) /**< Offset from MPC Base Address: <tt> 0x0FE0</tt> */
-#define MXC_R_MPC_PERIPHID1                ((uint32_t)0x00000FE4UL) /**< Offset from MPC Base Address: <tt> 0x0FE4</tt> */
-#define MXC_R_MPC_PERIPHID2                ((uint32_t)0x00000FE8UL) /**< Offset from MPC Base Address: <tt> 0x0FE8</tt> */
-#define MXC_R_MPC_PERIPHID3                ((uint32_t)0x00000FECUL) /**< Offset from MPC Base Address: <tt> 0x0FEC</tt> */
-#define MXC_R_MPC_COMPID                   ((uint32_t)0x00000FF0UL) /**< Offset from MPC Base Address: <tt> 0x0FF0</tt> */
+#define MXC_R_MPC_BLK_MAX                  ((uint32_t)0x00000010UL) /**< Offset from MPC Base Address: <tt> 0x0010</tt> */
+#define MXC_R_MPC_BLK_CFG                  ((uint32_t)0x00000014UL) /**< Offset from MPC Base Address: <tt> 0x0014</tt> */
+#define MXC_R_MPC_BLK_IDX                  ((uint32_t)0x00000018UL) /**< Offset from MPC Base Address: <tt> 0x0018</tt> */
+#define MXC_R_MPC_BLK_LUT                  ((uint32_t)0x0000001CUL) /**< Offset from MPC Base Address: <tt> 0x001C</tt> */
+#define MXC_R_MPC_INT_STAT                 ((uint32_t)0x00000020UL) /**< Offset from MPC Base Address: <tt> 0x0020</tt> */
+#define MXC_R_MPC_INT_CLEAR                ((uint32_t)0x00000024UL) /**< Offset from MPC Base Address: <tt> 0x0024</tt> */
+#define MXC_R_MPC_INT_EN                   ((uint32_t)0x00000028UL) /**< Offset from MPC Base Address: <tt> 0x0028</tt> */
+#define MXC_R_MPC_INT_INFO1                ((uint32_t)0x0000002CUL) /**< Offset from MPC Base Address: <tt> 0x002C</tt> */
+#define MXC_R_MPC_INT_INFO2                ((uint32_t)0x00000030UL) /**< Offset from MPC Base Address: <tt> 0x0030</tt> */
+#define MXC_R_MPC_INT_SET                  ((uint32_t)0x00000034UL) /**< Offset from MPC Base Address: <tt> 0x0034</tt> */
+#define MXC_R_MPC_PIDR4                    ((uint32_t)0x00000FD0UL) /**< Offset from MPC Base Address: <tt> 0x0FD0</tt> */
+#define MXC_R_MPC_PIDR0                    ((uint32_t)0x00000FE0UL) /**< Offset from MPC Base Address: <tt> 0x0FE0</tt> */
+#define MXC_R_MPC_PIDR1                    ((uint32_t)0x00000FE4UL) /**< Offset from MPC Base Address: <tt> 0x0FE4</tt> */
+#define MXC_R_MPC_PIDR2                    ((uint32_t)0x00000FE8UL) /**< Offset from MPC Base Address: <tt> 0x0FE8</tt> */
+#define MXC_R_MPC_PIDR3                    ((uint32_t)0x00000FECUL) /**< Offset from MPC Base Address: <tt> 0x0FEC</tt> */
+#define MXC_R_MPC_CIDR                     ((uint32_t)0x00000FF0UL) /**< Offset from MPC Base Address: <tt> 0x0FF0</tt> */
 /**@} end of group mpc_registers */
 
 /**
@@ -145,122 +145,122 @@ typedef struct {
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_BLKMAX MPC_BLKMAX
+ * @defgroup MPC_BLK_MAX MPC_BLK_MAX
  * @brief    Maximum value of block-based index register.
  * @{
  */
-#define MXC_F_MPC_BLKMAX_VAL_POS                       0 /**< BLKMAX_VAL Position */
-#define MXC_F_MPC_BLKMAX_VAL                           ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_BLKMAX_VAL_POS)) /**< BLKMAX_VAL Mask */
+#define MXC_F_MPC_BLK_MAX_VAL_POS                      0 /**< BLK_MAX_VAL Position */
+#define MXC_F_MPC_BLK_MAX_VAL                          ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_BLK_MAX_VAL_POS)) /**< BLK_MAX_VAL Mask */
 
-/**@} end of group MPC_BLKMAX_Register */
+/**@} end of group MPC_BLK_MAX_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_BLKCTRL MPC_BLKCTRL
+ * @defgroup MPC_BLK_CFG MPC_BLK_CFG
  * @brief    Block Control Register.
  * @{
  */
-#define MXC_F_MPC_BLKCTRL_SIZE_POS                     0 /**< BLKCTRL_SIZE Position */
-#define MXC_F_MPC_BLKCTRL_SIZE                         ((uint32_t)(0xFUL << MXC_F_MPC_BLKCTRL_SIZE_POS)) /**< BLKCTRL_SIZE Mask */
+#define MXC_F_MPC_BLK_CFG_SIZE_POS                     0 /**< BLK_CFG_SIZE Position */
+#define MXC_F_MPC_BLK_CFG_SIZE                         ((uint32_t)(0xFUL << MXC_F_MPC_BLK_CFG_SIZE_POS)) /**< BLK_CFG_SIZE Mask */
 
-#define MXC_F_MPC_BLKCTRL_INIT_ST_POS                  31 /**< BLKCTRL_INIT_ST Position */
-#define MXC_F_MPC_BLKCTRL_INIT_ST                      ((uint32_t)(0x1UL << MXC_F_MPC_BLKCTRL_INIT_ST_POS)) /**< BLKCTRL_INIT_ST Mask */
+#define MXC_F_MPC_BLK_CFG_INIT_ST_POS                  31 /**< BLK_CFG_INIT_ST Position */
+#define MXC_F_MPC_BLK_CFG_INIT_ST                      ((uint32_t)(0x1UL << MXC_F_MPC_BLK_CFG_INIT_ST_POS)) /**< BLK_CFG_INIT_ST Mask */
 
-/**@} end of group MPC_BLKCTRL_Register */
+/**@} end of group MPC_BLK_CFG_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_BLKIDX MPC_BLKIDX
+ * @defgroup MPC_BLK_IDX MPC_BLK_IDX
  * @brief    Block Index Register.
  * @{
  */
-#define MXC_F_MPC_BLKIDX_IDX_POS                       0 /**< BLKIDX_IDX Position */
-#define MXC_F_MPC_BLKIDX_IDX                           ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_BLKIDX_IDX_POS)) /**< BLKIDX_IDX Mask */
+#define MXC_F_MPC_BLK_IDX_IDX_POS                      0 /**< BLK_IDX_IDX Position */
+#define MXC_F_MPC_BLK_IDX_IDX                          ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_BLK_IDX_IDX_POS)) /**< BLK_IDX_IDX Mask */
 
-/**@} end of group MPC_BLKIDX_Register */
+/**@} end of group MPC_BLK_IDX_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_BLKLUT MPC_BLKLUT
+ * @defgroup MPC_BLK_LUT MPC_BLK_LUT
  * @brief    Block-based gating Look Up Table Register.
  * @{
  */
-#define MXC_F_MPC_BLKLUT_ACCESS_POS                    0 /**< BLKLUT_ACCESS Position */
-#define MXC_F_MPC_BLKLUT_ACCESS                        ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_BLKLUT_ACCESS_POS)) /**< BLKLUT_ACCESS Mask */
+#define MXC_F_MPC_BLK_LUT_ACCESS_POS                   0 /**< BLK_LUT_ACCESS Position */
+#define MXC_F_MPC_BLK_LUT_ACCESS                       ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_BLK_LUT_ACCESS_POS)) /**< BLK_LUT_ACCESS Mask */
 
-/**@} end of group MPC_BLKLUT_Register */
+/**@} end of group MPC_BLK_LUT_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_INTFL MPC_INTFL
+ * @defgroup MPC_INT_STAT MPC_INT_STAT
  * @brief    Interrupt Flag Register.
  * @{
  */
-#define MXC_F_MPC_INTFL_MPC_POS                        0 /**< INTFL_MPC Position */
-#define MXC_F_MPC_INTFL_MPC                            ((uint32_t)(0x1UL << MXC_F_MPC_INTFL_MPC_POS)) /**< INTFL_MPC Mask */
+#define MXC_F_MPC_INT_STAT_MPC_IRQ_POS                 0 /**< INT_STAT_MPC_IRQ Position */
+#define MXC_F_MPC_INT_STAT_MPC_IRQ                     ((uint32_t)(0x1UL << MXC_F_MPC_INT_STAT_MPC_IRQ_POS)) /**< INT_STAT_MPC_IRQ Mask */
 
-/**@} end of group MPC_INTFL_Register */
+/**@} end of group MPC_INT_STAT_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_INTCLR MPC_INTCLR
+ * @defgroup MPC_INT_CLEAR MPC_INT_CLEAR
  * @brief    Interrupt Clear Register.
  * @{
  */
-#define MXC_F_MPC_INTCLR_MPC_POS                       0 /**< INTCLR_MPC Position */
-#define MXC_F_MPC_INTCLR_MPC                           ((uint32_t)(0x1UL << MXC_F_MPC_INTCLR_MPC_POS)) /**< INTCLR_MPC Mask */
+#define MXC_F_MPC_INT_CLEAR_MPC_IRQ_POS                0 /**< INT_CLEAR_MPC_IRQ Position */
+#define MXC_F_MPC_INT_CLEAR_MPC_IRQ                    ((uint32_t)(0x1UL << MXC_F_MPC_INT_CLEAR_MPC_IRQ_POS)) /**< INT_CLEAR_MPC_IRQ Mask */
 
-/**@} end of group MPC_INTCLR_Register */
+/**@} end of group MPC_INT_CLEAR_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_INTEN MPC_INTEN
+ * @defgroup MPC_INT_EN MPC_INT_EN
  * @brief    Interrupt Enable Register.
  * @{
  */
-#define MXC_F_MPC_INTEN_MPC_POS                        0 /**< INTEN_MPC Position */
-#define MXC_F_MPC_INTEN_MPC                            ((uint32_t)(0x1UL << MXC_F_MPC_INTEN_MPC_POS)) /**< INTEN_MPC Mask */
+#define MXC_F_MPC_INT_EN_MPC_IRQ_POS                   0 /**< INT_EN_MPC_IRQ Position */
+#define MXC_F_MPC_INT_EN_MPC_IRQ                       ((uint32_t)(0x1UL << MXC_F_MPC_INT_EN_MPC_IRQ_POS)) /**< INT_EN_MPC_IRQ Mask */
 
-/**@} end of group MPC_INTEN_Register */
-
-/**
- * @ingroup  mpc_registers
- * @defgroup MPC_INTINFO0 MPC_INTINFO0
- * @brief    Interrupt Info 0 Register.
- * @{
- */
-#define MXC_F_MPC_INTINFO0_HADDR_POS                   0 /**< INTINFO0_HADDR Position */
-#define MXC_F_MPC_INTINFO0_HADDR                       ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_INTINFO0_HADDR_POS)) /**< INTINFO0_HADDR Mask */
-
-/**@} end of group MPC_INTINFO0_Register */
+/**@} end of group MPC_INT_EN_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_INTINFO1 MPC_INTINFO1
+ * @defgroup MPC_INT_INFO1 MPC_INT_INFO1
  * @brief    Interrupt Info 1 Register.
  * @{
  */
-#define MXC_F_MPC_INTINFO1_HMASTER_POS                 0 /**< INTINFO1_HMASTER Position */
-#define MXC_F_MPC_INTINFO1_HMASTER                     ((uint32_t)(0xFFFFUL << MXC_F_MPC_INTINFO1_HMASTER_POS)) /**< INTINFO1_HMASTER Mask */
+#define MXC_F_MPC_INT_INFO1_HADDR_POS                  0 /**< INT_INFO1_HADDR Position */
+#define MXC_F_MPC_INT_INFO1_HADDR                      ((uint32_t)(0xFFFFFFFFUL << MXC_F_MPC_INT_INFO1_HADDR_POS)) /**< INT_INFO1_HADDR Mask */
 
-#define MXC_F_MPC_INTINFO1_HNONSEC_POS                 16 /**< INTINFO1_HNONSEC Position */
-#define MXC_F_MPC_INTINFO1_HNONSEC                     ((uint32_t)(0x1UL << MXC_F_MPC_INTINFO1_HNONSEC_POS)) /**< INTINFO1_HNONSEC Mask */
-
-#define MXC_F_MPC_INTINFO1_CFG_NS_POS                  17 /**< INTINFO1_CFG_NS Position */
-#define MXC_F_MPC_INTINFO1_CFG_NS                      ((uint32_t)(0x1UL << MXC_F_MPC_INTINFO1_CFG_NS_POS)) /**< INTINFO1_CFG_NS Mask */
-
-/**@} end of group MPC_INTINFO1_Register */
+/**@} end of group MPC_INT_INFO1_Register */
 
 /**
  * @ingroup  mpc_registers
- * @defgroup MPC_INTSET MPC_INTSET
+ * @defgroup MPC_INT_INFO2 MPC_INT_INFO2
+ * @brief    Interrupt Info 2 Register.
+ * @{
+ */
+#define MXC_F_MPC_INT_INFO2_HMASTER_POS                0 /**< INT_INFO2_HMASTER Position */
+#define MXC_F_MPC_INT_INFO2_HMASTER                    ((uint32_t)(0xFFFFUL << MXC_F_MPC_INT_INFO2_HMASTER_POS)) /**< INT_INFO2_HMASTER Mask */
+
+#define MXC_F_MPC_INT_INFO2_HNONSEC_POS                16 /**< INT_INFO2_HNONSEC Position */
+#define MXC_F_MPC_INT_INFO2_HNONSEC                    ((uint32_t)(0x1UL << MXC_F_MPC_INT_INFO2_HNONSEC_POS)) /**< INT_INFO2_HNONSEC Mask */
+
+#define MXC_F_MPC_INT_INFO2_CFG_NS_POS                 17 /**< INT_INFO2_CFG_NS Position */
+#define MXC_F_MPC_INT_INFO2_CFG_NS                     ((uint32_t)(0x1UL << MXC_F_MPC_INT_INFO2_CFG_NS_POS)) /**< INT_INFO2_CFG_NS Mask */
+
+/**@} end of group MPC_INT_INFO2_Register */
+
+/**
+ * @ingroup  mpc_registers
+ * @defgroup MPC_INT_SET MPC_INT_SET
  * @brief    Interrupt Set Debug Register.
  * @{
  */
-#define MXC_F_MPC_INTSET_MPC_POS                       0 /**< INTSET_MPC Position */
-#define MXC_F_MPC_INTSET_MPC                           ((uint32_t)(0xFFFFUL << MXC_F_MPC_INTSET_MPC_POS)) /**< INTSET_MPC Mask */
+#define MXC_F_MPC_INT_SET_MPC_IRQ_POS                  0 /**< INT_SET_MPC_IRQ Position */
+#define MXC_F_MPC_INT_SET_MPC_IRQ                      ((uint32_t)(0x1UL << MXC_F_MPC_INT_SET_MPC_IRQ_POS)) /**< INT_SET_MPC_IRQ Mask */
 
-/**@} end of group MPC_INTSET_Register */
+/**@} end of group MPC_INT_SET_Register */
 
 #ifdef __cplusplus
 }
