91aa6e28b0b0436394edaf9d802c093ef4f9c061
EricB-ADI <eric.busch@analogdevices.com>
2024-08-21 15:36:20 -0500

    Revert "test(workflow): Update BLE Examples WF (#1018)"
    This reverts commit e56d5a7135d2b788b0109ba9df5e69d3ecd6cba0.

---
+++ .github/workflows/scripts/local_BLE_Examples_Test.sh
@@ -0,0 +1,249 @@
+#!/usr/bin/env bash
+
+echo
+echo "#####################################################################################################################"
+echo "# This script is used to test the BLE_Examples_Test.yml locally at wall-e.                                          #"
+echo "# local_BLE_Examples_Test.sh <0/1 for max32655> <0/1 for max32665> <0/1 for max32690 devkit> <0/1 for max32690 WLP> #"
+echo "#####################################################################################################################"
+
+echo
+echo $0 $@
+
+if [[ $# -ne 4 ]]; then
+    echo
+    echo "Invalid arguments."
+    echo "local_BLE_Examples_Test.sh <0/1 for max32655> <0/1 for max32665> <0/1 for max32690 devkit> <0/1 for max32690 WLP>"
+    exit 1
+fi
+
+host=`hostname`
+if [ $host == "wall-e" ]; then
+    MSDK=/home/$USER/Workspace/yc/msdk_open
+else
+    MSDK=/home/$USER/Workspace/msdk_open
+fi
+
+echo
+echo "MSDK=$MSDK"
+
+DO_MAX32655=$1
+DO_MAX32665=$2
+DO_MAX32690_EVKIT=$3
+DO_MAX32690_WLP=$4
+
+#--------------------------------------------------------------------------------------------------
+# For MAX32655
+if [[ $DO_MAX32655 -eq 1 ]]; then
+    echo
+    echo "-----------------------------------------------------------------------------------------"
+    echo "TEST MAX32655"
+
+    echo 
+    echo "Lock the used recourse files."
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32690_board_w1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32690_board_A5
+
+    touch /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32690_EvKit.txt
+
+    set -e
+
+    cd $MSDK
+    echo "PWD="`pwd`
+
+    cd .github/workflows/scripts
+    chmod +x test_launcher.sh
+    FILE=/home/$USER/Workspace/Resource_Share/boards_config.json
+    if [ $(hostname) == "wall-e" ]; then
+        dut_uart=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board2']['uart0'])"`
+        dut_serial=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board2']['daplink'])"`
+    else
+        dut_uart=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board_y2']['uart0'])"`
+        dut_serial=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32655_board_y2']['daplink'])"`
+    fi
+    ./test_launcher.sh max32655 $dut_uart $dut_serial
+ 
+    echo 
+    echo "Release the used resource files."
+    echo
+    ls -hal ~/Workspace/Resource_Share
+
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32690_board_A5
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32690_board_w1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board1
+
+    echo 
+    echo "-----------------------------------------------------------------------------------------"
+    echo "FINISH TEST MAX32655"
+fi
+
+#--------------------------------------------------------------------------------------------------
+# For MAX32665
+if [[ $DO_MAX32665 -eq 1 ]]; then
+    echo
+    echo "-----------------------------------------------------------------------------------------"
+    echo "TEST MAX32665"
+
+    echo "Lock the used recourse files."
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32690_board_w1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32690_board_A5
+
+    touch /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32665.txt
+
+    set +e
+
+    cd $MSDK
+    echo "PWD="`pwd`
+
+    cd .github/workflows/scripts
+    chmod +x test_launcher.sh
+    FILE=/home/$USER/Workspace/Resource_Share/boards_config.json
+    if [ $(hostname) == "wall-e" ]; then
+        dut_uart=`  /usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32665_board1']['uart1'])"`
+        dut_serial=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32665_board1']['daplink'])"`            
+    else
+        dut_uart=`  /usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32665_board_2']['uart1'])"`
+        dut_serial=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32665_board_2']['daplink'])"`            
+    fi
+
+    ./test_launcher.sh max32665 $dut_uart $dut_serial
+ 
+    set -e
+    
+    echo 
+    echo "Release the used resource files."
+    echo
+    ls -hal ~/Workspace/Resource_Share
+    
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32690_board_A5
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32690_board_w1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board1
+
+    echo 
+    echo "-----------------------------------------------------------------------------------------"
+    echo "FINISH TEST MAX32655"
+fi
+
+#--------------------------------------------------------------------------------------------------
+# For MAX32690_EVKIT
+if [[ $DO_MAX32690_EVKIT -eq 1 ]]; then
+    echo
+    echo "-----------------------------------------------------------------------------------------"
+    echo "TEST MAX32690 EVKIT"
+    echo 
+
+    echo "Lock the used recourse files."
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32690_board_w1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32690_board_A5
+
+    touch /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32690_EvKit.txt
+
+    set +e
+
+    cd $MSDK
+    echo "PWD="`pwd`
+
+    cd .github/workflows/scripts
+    chmod +x test_launcher.sh
+    FILE=/home/$USER/Workspace/Resource_Share/boards_config.json
+    if [ $(hostname) == "wall-e" ]; then
+        dut_uart=`  /usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32690_board_w1']['uart2'])"`
+        dut_serial=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32690_board_w1']['daplink'])"`            
+    else
+        dut_uart=`  /usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32690_board_3']['uart2'])"`
+        dut_serial=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['max32690_board_3']['daplink'])"`            
+    fi
+
+    ./test_launcher.sh max32690 $dut_uart $dut_serial
+ 
+    set -e
+
+    echo 
+    echo "Release the used resource files."
+    echo
+    ls -hal ~/Workspace/Resource_Share
+    
+    if [ -f /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32690_EvKit.txt ]; then
+        rm /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32690_EvKit.txt
+    fi
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32690_board_A5
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32690_board_w1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board1
+
+    echo 
+    echo "-----------------------------------------------------------------------------------------"
+    echo "FINISH TEST MAX32690 EVKIT"
+fi
+
+
+#--------------------------------------------------------------------------------------------------
+# For MAX32690_WLP
+if [[ $DO_MAX32690_WLP -eq 1 ]]; then
+    echo
+    echo "-----------------------------------------------------------------------------------------"
+    echo "TEST MAX32690 WLP"
+    echo
+
+    FILE=/home/$USER/Workspace/Resource_Share/boards_config.json
+
+    if [ $host == "wall-e" ]; then
+        max32690_wlp_brd=max32690_board_A5
+    else
+        max32690_wlp_brd=max32690_board_A3
+    fi
+
+    dut_uart=`  /usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['${max32690_wlp_brd}']['con_sn'])"`
+    dut_serial=`/usr/bin/python3 -c "import sys, json; print(json.load(open('$FILE'))['${max32690_wlp_brd}']['DAP_sn'])"`            
+
+    echo "Lock the used recourse files."    
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 max32690_board_w1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py -l -t 3600 ${max32690_wlp_brd}
+
+    touch /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32690_WLP.txt # indicate which jos is running
+    set +e
+
+    cd $MSDK
+    echo "PWD="`pwd`
+
+    cd .github/workflows/scripts
+    chmod +x test_launcher.sh
+    ./test_launcher.sh max32690 $dut_uart $dut_serial WLP_V1
+ 
+    set -e
+
+    echo 
+    echo "Release the used resource files."
+    echo
+    ls -hal ~/Workspace/Resource_Share
+    
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py ${max32690_wlp_brd}
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32655_board2
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32665_board1
+    python3 /home/$USER/Workspace/Resource_Share/Resource_Share.py max32690_board_w1
+
+    if [ -f /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32690_WLP.txt ]; then
+        rm /home/$USER/Workspace/Resource_Share/local-BLE_Examples_Test-MAX32690_WLP.txt
+    fi
+
+    echo 
+    echo "-----------------------------------------------------------------------------------------"
+    echo "FINISH TEST MAX32690 WLP"
+fi
