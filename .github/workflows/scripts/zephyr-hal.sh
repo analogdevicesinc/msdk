29e4c66c36b835e18104ea4521233b2cd464f34d
EricB-ADI <eric.busch@analogdevices.com>
2024-08-21 15:48:21 -0500

    Revert "chore(Other): Add zephyr related wrappers and system files in MSDK (#990)"
    This reverts commit 790ad991dece127f4a52f6cff189af970a488fa4.

--- .github/workflows/scripts/zephyr-hal.sh
+++ .github/workflows/scripts/zephyr-hal.sh
@@ -12,6 +12,9 @@ else
     hal_adi="./hal_adi"
 fi
 
+src="${msdk}/Libraries"
+dst="${hal_adi}/MAX/Libraries"
+
 root_dir=$(pwd)
 
 # Get SHA
@@ -21,35 +24,29 @@ cd ${root_dir}
 
 
 # Cleanup hal_adi
-rm -rf ${hal_adi}/MAX/
-
-# Create parent folder
-mkdir -p ${hal_adi}/MAX/Libraries/CMSIS
-mkdir -p ${hal_adi}/MAX/Libraries/PeriphDrivers
-
-# Copy zephyr wrappers, system files and cmakefiles
-cp -rf ${msdk}/Libraries/zephyr/MAX/*  ${hal_adi}/MAX/
+rm -rf ${dst}/CMSIS/*
+rm -rf ${dst}/PeriphDrivers/*
 
 # Copy CMSIS folder
-cp -rf ${msdk}/Libraries/CMSIS/Device  ${hal_adi}/MAX/Libraries/CMSIS/
-cp -rf ${msdk}/Libraries/CMSIS/Include ${hal_adi}/MAX/Libraries/CMSIS/
+cp -rf ${src}/CMSIS/Device  ${dst}/CMSIS/
+cp -rf ${src}/CMSIS/Include  ${dst}/CMSIS/
 
 # Copy PeriphDrivers folder
-cp -rf ${msdk}/Libraries/PeriphDrivers/Include ${hal_adi}/MAX/Libraries/PeriphDrivers/
-cp -rf ${msdk}/Libraries/PeriphDrivers/Source  ${hal_adi}/MAX/Libraries/PeriphDrivers/
+cp -rf ${src}/PeriphDrivers/Include  ${dst}/PeriphDrivers/
+cp -rf ${src}/PeriphDrivers/Source  ${dst}/PeriphDrivers/
 
 # Remove unneeded files
-rm -rf ${hal_adi}/MAX/Libraries/CMSIS/Device/Maxim/GCC
-rm -rf ${hal_adi}/MAX/Libraries/CMSIS/Device/Maxim/MAX*/Source/IAR
-rm -rf ${hal_adi}/MAX/Libraries/CMSIS/Device/Maxim/MAX*/Source/GCC
-rm -rf ${hal_adi}/MAX/Libraries/CMSIS/Device/Maxim/MAX*/Source/ARM
+rm -rf ${dst}/CMSIS/Device/Maxim/GCC
+rm -rf ${dst}/CMSIS/Device/Maxim/MAX*/Source/IAR
+rm -rf ${dst}/CMSIS/Device/Maxim/MAX*/Source/GCC
+rm -rf ${dst}/CMSIS/Device/Maxim/MAX*/Source/ARM
 
 # Check either dirty or clean
 cd ${hal_adi}
 if [[ -n $(git status -s) ]]; then
     echo "New change exist need to be pushed"
     # Set new SHA
-    echo "${msdk_head}" > MAX/msdk_sha
+    echo "${msdk_head}" > MAX/Libraries/msdk_sha
 else
     # No need to push to hal_adi
     echo "No any change in hal_adi"
