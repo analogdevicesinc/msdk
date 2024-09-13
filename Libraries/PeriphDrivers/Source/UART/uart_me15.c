ac3e34b2509d792118b0811ca8c2b79ffe23e8a3
Sadik Ozer <46590392+ozersa@users.noreply.github.com>
2024-09-13 22:28:51 +0300

    chore(Other, PeriphDrivers): Fix MAX32xxx UART build issue (#1175)
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- Libraries/PeriphDrivers/Source/UART/uart_me15.c
+++ Libraries/PeriphDrivers/Source/UART/uart_me15.c
@@ -45,9 +45,9 @@ int MXC_UART_AsyncStop(mxc_uart_regs_t *uart)
 
 int MXC_UART_Init(mxc_uart_regs_t *uart, unsigned int baud, mxc_uart_clock_t clock)
 {
-#ifndef MSDK_NO_GPIO_CLK_INIT
     int retval;
 
+#ifndef MSDK_NO_GPIO_CLK_INIT
     retval = MXC_UART_Shutdown(uart);
 
     if (retval) {
--- Libraries/PeriphDrivers/Source/UART/uart_me16.c
+++ Libraries/PeriphDrivers/Source/UART/uart_me16.c
@@ -48,8 +48,9 @@ int MXC_UART_AsyncStop(mxc_uart_regs_t *uart)
 
 int MXC_UART_Init(mxc_uart_regs_t *uart, unsigned int baud, mxc_uart_clock_t clock)
 {
+    int retval;
 #ifndef MSDK_NO_GPIO_CLK_INIT
-    int retval, error;
+    int error;
 
     retval = MXC_UART_Shutdown(uart);
     if (retval) {
