fd2fff039ec35bbd19863b8968f132e8654e46fa
Woo <Sihyung.Woo@analog.com>
2024-09-19 14:39:27 -0500

    Update oscillator trim settings

--- Libraries/CMSIS/Device/Maxim/MAX32657/Include/mcr_regs.h
+++ Libraries/CMSIS/Device/Maxim/MAX32657/Include/mcr_regs.h
@@ -78,11 +78,11 @@ typedef struct {
     __R  uint32_t rsv_0xc;
     __IO uint32_t ctrl;                 /**< <tt>\b 0x10:</tt> MCR CTRL Register */
     __R  uint32_t rsv_0x14_0x2f[7];
-    __IO uint32_t bbreg0;               /**< <tt>\b 0x30:</tt> MCR BBREG0 Register */
-    __IO uint32_t bbreg1;               /**< <tt>\b 0x34:</tt> MCR BBREG1 Register */
+    __IO uint32_t bypass0;              /**< <tt>\b 0x30:</tt> MCR BYPASS0 Register */
+    __IO uint32_t bypass1;              /**< <tt>\b 0x34:</tt> MCR BYPASS1 Register */
     __R  uint32_t rsv_0x38_0x3f[2];
-    __IO uint32_t bbdata0;              /**< <tt>\b 0x40:</tt> MCR BBDATA0 Register */
-    __IO uint32_t bbdata1;              /**< <tt>\b 0x44:</tt> MCR BBDATA1 Register */
+    __IO uint32_t data0;                /**< <tt>\b 0x40:</tt> MCR DATA0 Register */
+    __IO uint32_t data1;                /**< <tt>\b 0x44:</tt> MCR DATA1 Register */
 } mxc_mcr_regs_t;
 
 /* Register offsets for module MCR */
@@ -95,10 +95,10 @@ typedef struct {
 #define MXC_R_MCR_RST                      ((uint32_t)0x00000004UL) /**< Offset from MCR Base Address: <tt> 0x0004</tt> */
 #define MXC_R_MCR_OUTEN                    ((uint32_t)0x00000008UL) /**< Offset from MCR Base Address: <tt> 0x0008</tt> */
 #define MXC_R_MCR_CTRL                     ((uint32_t)0x00000010UL) /**< Offset from MCR Base Address: <tt> 0x0010</tt> */
-#define MXC_R_MCR_BBREG0                   ((uint32_t)0x00000030UL) /**< Offset from MCR Base Address: <tt> 0x0030</tt> */
-#define MXC_R_MCR_BBREG1                   ((uint32_t)0x00000034UL) /**< Offset from MCR Base Address: <tt> 0x0034</tt> */
-#define MXC_R_MCR_BBDATA0                  ((uint32_t)0x00000040UL) /**< Offset from MCR Base Address: <tt> 0x0040</tt> */
-#define MXC_R_MCR_BBDATA1                  ((uint32_t)0x00000044UL) /**< Offset from MCR Base Address: <tt> 0x0044</tt> */
+#define MXC_R_MCR_BYPASS0                  ((uint32_t)0x00000030UL) /**< Offset from MCR Base Address: <tt> 0x0030</tt> */
+#define MXC_R_MCR_BYPASS1                  ((uint32_t)0x00000034UL) /**< Offset from MCR Base Address: <tt> 0x0034</tt> */
+#define MXC_R_MCR_DATA0                    ((uint32_t)0x00000040UL) /**< Offset from MCR Base Address: <tt> 0x0040</tt> */
+#define MXC_R_MCR_DATA1                    ((uint32_t)0x00000044UL) /**< Offset from MCR Base Address: <tt> 0x0044</tt> */
 /**@} end of group mcr_registers */
 
 /**
@@ -109,9 +109,6 @@ typedef struct {
  */
 #define MXC_F_MCR_RST_BOOST_POS                        0 /**< RST_BOOST Position */
 #define MXC_F_MCR_RST_BOOST                            ((uint32_t)(0x1UL << MXC_F_MCR_RST_BOOST_POS)) /**< RST_BOOST Mask */
-
-#define MXC_F_MCR_RST_RSTZ_POS                         1 /**< RST_RSTZ Position */
-#define MXC_F_MCR_RST_RSTZ                             ((uint32_t)(0x1UL << MXC_F_MCR_RST_RSTZ_POS)) /**< RST_RSTZ Mask */
 
 /**@} end of group MCR_RST_Register */
 
@@ -132,20 +129,20 @@ typedef struct {
  * @brief    Control Register
  * @{
  */
-#define MXC_F_MCR_CTRL_CLKSEL_POS                      0 /**< CTRL_CLKSEL Position */
-#define MXC_F_MCR_CTRL_CLKSEL                          ((uint32_t)(0x3UL << MXC_F_MCR_CTRL_CLKSEL_POS)) /**< CTRL_CLKSEL Mask */
-#define MXC_V_MCR_CTRL_CLKSEL_ERTCO                    ((uint32_t)0x0UL) /**< CTRL_CLKSEL_ERTCO Value */
-#define MXC_S_MCR_CTRL_CLKSEL_ERTCO                    (MXC_V_MCR_CTRL_CLKSEL_ERTCO << MXC_F_MCR_CTRL_CLKSEL_POS) /**< CTRL_CLKSEL_ERTCO Setting */
-#define MXC_V_MCR_CTRL_CLKSEL_INRO                     ((uint32_t)0x1UL) /**< CTRL_CLKSEL_INRO Value */
-#define MXC_S_MCR_CTRL_CLKSEL_INRO                     (MXC_V_MCR_CTRL_CLKSEL_INRO << MXC_F_MCR_CTRL_CLKSEL_POS) /**< CTRL_CLKSEL_INRO Setting */
-#define MXC_V_MCR_CTRL_CLKSEL_EXTCLK                   ((uint32_t)0x2UL) /**< CTRL_CLKSEL_EXTCLK Value */
-#define MXC_S_MCR_CTRL_CLKSEL_EXTCLK                   (MXC_V_MCR_CTRL_CLKSEL_EXTCLK << MXC_F_MCR_CTRL_CLKSEL_POS) /**< CTRL_CLKSEL_EXTCLK Setting */
+#define MXC_F_MCR_CTRL_CLKSEL_32K_POS                  0 /**< CTRL_CLKSEL_32K Position */
+#define MXC_F_MCR_CTRL_CLKSEL_32K                      ((uint32_t)(0x3UL << MXC_F_MCR_CTRL_CLKSEL_32K_POS)) /**< CTRL_CLKSEL_32K Mask */
+#define MXC_V_MCR_CTRL_CLKSEL_32K_ERTCO                ((uint32_t)0x0UL) /**< CTRL_CLKSEL_32K_ERTCO Value */
+#define MXC_S_MCR_CTRL_CLKSEL_32K_ERTCO                (MXC_V_MCR_CTRL_CLKSEL_32K_ERTCO << MXC_F_MCR_CTRL_CLKSEL_32K_POS) /**< CTRL_CLKSEL_32K_ERTCO Setting */
+#define MXC_V_MCR_CTRL_CLKSEL_32K_INRO                 ((uint32_t)0x1UL) /**< CTRL_CLKSEL_32K_INRO Value */
+#define MXC_S_MCR_CTRL_CLKSEL_32K_INRO                 (MXC_V_MCR_CTRL_CLKSEL_32K_INRO << MXC_F_MCR_CTRL_CLKSEL_32K_POS) /**< CTRL_CLKSEL_32K_INRO Setting */
+#define MXC_V_MCR_CTRL_CLKSEL_32K_EXTCLK               ((uint32_t)0x2UL) /**< CTRL_CLKSEL_32K_EXTCLK Value */
+#define MXC_S_MCR_CTRL_CLKSEL_32K_EXTCLK               (MXC_V_MCR_CTRL_CLKSEL_32K_EXTCLK << MXC_F_MCR_CTRL_CLKSEL_32K_POS) /**< CTRL_CLKSEL_32K_EXTCLK Setting */
 
-#define MXC_F_MCR_CTRL_ERTCO_32KHZ_EN_POS              3 /**< CTRL_ERTCO_32KHZ_EN Position */
-#define MXC_F_MCR_CTRL_ERTCO_32KHZ_EN                  ((uint32_t)(0x1UL << MXC_F_MCR_CTRL_ERTCO_32KHZ_EN_POS)) /**< CTRL_ERTCO_32KHZ_EN Mask */
+#define MXC_F_MCR_CTRL_CLK32K_EN_POS                   3 /**< CTRL_CLK32K_EN Position */
+#define MXC_F_MCR_CTRL_CLK32K_EN                       ((uint32_t)(0x1UL << MXC_F_MCR_CTRL_CLK32K_EN_POS)) /**< CTRL_CLK32K_EN Mask */
 
-#define MXC_F_MCR_CTRL_ERTCO_EN_POS                    5 /**< CTRL_ERTCO_EN Position */
-#define MXC_F_MCR_CTRL_ERTCO_EN                        ((uint32_t)(0x1UL << MXC_F_MCR_CTRL_ERTCO_EN_POS)) /**< CTRL_ERTCO_EN Mask */
+#define MXC_F_MCR_CTRL_RTC4K_EN_POS                    5 /**< CTRL_RTC4K_EN Position */
+#define MXC_F_MCR_CTRL_RTC4K_EN                        ((uint32_t)(0x1UL << MXC_F_MCR_CTRL_RTC4K_EN_POS)) /**< CTRL_RTC4K_EN Mask */
 
 /**@} end of group MCR_CTRL_Register */
 
