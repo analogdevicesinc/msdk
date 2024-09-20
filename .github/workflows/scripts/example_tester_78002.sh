f931c56eb311d41a2bea0660e5f664bc44cc4a5b
JC-282-AD <Jason-X.Cai@analog.com>
2024-09-20 10:27:13 -0500

    feat(Examples): Add autotest to test Examples for MAX78002 (#1184)

--- .github/workflows/scripts/example_tester_78002.sh
+++ .github/workflows/scripts/example_tester_78002.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
 <<"CONFIGURATION"
-The following is the configuration for hardware (max32690): 
-remove JP7(RX_EN) and install JP8(TX_EN) headers
+The following is the configuration for hardware (max78002): 
+Close jumper (RX - P0.0) and (TX - P0.1) at Headers JP23 (UART 0 EN).
 Install headers JP9 and JP10 to SDA and SCL respectively.
-You must connect P1.8->P2.8 (SCL) and P1.7->P2.7 (SCL).
-Connect pins P2.12->P1.9 for UART test
-connect MISO (P2.27) and MOSI (P2.28) pins
+You must connect P0.10 to P0.16 (SCL) and P0.11 to P0.17 (SDA)
+Connect P0.12 to P1.1. Connect P0.13 to P1.0. for UART test
+Connect P0.21 (MOSI) to P0.22 (MISO).
 Apply an input voltage between 0 and 1.25V to pin labeled 0 of the JH6 (Analog) header. for ADC
 CONFIGURATION
 
@@ -15,12 +15,11 @@ CONFIGURATION
 baudRate=115200
 timeLimit=5
 timeLimitICC=30 #ICC needs around 20s to finish the test
-boardVersion=max32690
-#boardName=max32690-1
+boardVersion=max78002
+#boardName=max78002-1
 boardName=$1
 uartPort=$(resource_manager -g $boardName.console_port)
 target_uc=$(resource_manager -g $boardName.target)
-#MAXIM_PATH=/home/jcai/Workspace/msdk
 Path=$MAXIM_PATH/Examples/$target_uc
 
 # variable for testing purpose
@@ -31,18 +30,10 @@ result_HelloWorld='not tested'
 result_HelloWorld_Cpp='not tested'
 result_TRNG='not tested'
 result_I2C='not tested'
-result_SPI_POLLING='not tested'
-result_SPI_INTERRUPT='not tested'
-result_SPI_DMA='not tested'
-result_SPI_V2_POLLING='not tested' #this is only for max32690
-result_SPI_V2_INTERRUPT='not tested' #this is only for max32690
-result_SPI_V2_DMA='not tested' #this is only for max32690
+result_SPI='not tested'
 result_ICC='not tested'
-result_Hash='not tested'
 result_DMA='not tested'
 result_CRC='not tested'
-result_ADC_POLLING='not tested'
-result_ADC_INTERRUPT='not tested'
 result_ADC_DMA='not tested'
 result_Lib_Gen='not tested'
 result_Lib_Use='not tested'
@@ -61,12 +52,12 @@ function init() {
 	then
 		make -C $Path/$testName distclean
 
-		if [[ $1 = "SPI" || $1 = "SPI_v2" || $1 = "ADC" || $1 = "UART" ]];
-		then
-			make -j -C $Path/$testName METHOD=$2
+		if [[ $1 == "ADC" || $1 == "UART" ]]; then
+    		make -j -C "$Path/$testName" METHOD="$2"
 		else
-			make -j -C $Path/$testName
+    		make -j -C "$Path/$testName"
 		fi
+
 	fi
 
 	if [[ $testName != Library_Generate && $testName != Library_Use ]]
@@ -127,7 +118,6 @@ function test_UART() {
 	fi
 	
 	printf "Test result for UART_INTERRUPT: $result_UART_INTERRUPT\n"
-: '
 	init UART DMA
 
 	# start testing the output
@@ -140,7 +130,6 @@ function test_UART() {
 	fi
 	
 	printf "Test result for UART_DMA: $result_UART_DMA\n"
-'
 }
 
 function test_TRNG() {
@@ -177,88 +166,22 @@ function test_I2C() {
 }
 
 function test_SPI() {
-	init SPI MASTERSYNC
+	init SPI
 
 	# start testing the output
 	grep "16 Bits Transaction Successful" $tempFile
 	if [[ $? -eq 0 ]];
 	then
-		result_SPI_POLLING='pass'
+		result_SPI='pass'
 	else
-		result_SPI_POLLING='fail'
+		result_SPI='fail'
 	fi
 	
-	printf "Test result for SPI_POLLING: $result_SPI_POLLING\n"
-
-	init SPI MASTERASYNC
-
-	# start testing the output
-	grep "16 Bits Transaction Successful" $tempFile
-	if [[ $? -eq 0 ]];
-	then
-		result_SPI_INTERRUPT='pass'
-	else
-		result_SPI_INTERRUPT='fail'
-	fi
+	printf "Test result for SPI: $result_SPI\n"
 	
-	printf "Test result for SPI_INTERRUPT: $result_SPI_INTERRUPT\n"
-
-	init SPI MASTERDMA
-
-	# start testing the output
-	grep "16 Bits Transaction Successful" $tempFile
-	if [[ $? -eq 0 ]];
-	then
-		result_SPI_DMA='pass'
-	else
-		result_SPI_DMA='fail'
-	fi
-	
-	printf "Test result for SPI_DMA: $result_SPI_DMA\n"
 	
 }
 
-function test_SPI_V2() {
-	init SPI_v2 CONTROLLER_SYNC
-
-	# start testing the output
-	grep "16 Bits Transaction Successful" $tempFile
-	if [[ $? -eq 0 ]];
-	then
-		result_SPI_V2_POLLING='pass'
-	else
-		result_SPI_V2_POLLING='fail'
-	fi
-	
-	printf "Test result for SPI_v2_POLLING: $result_SPI_V2_POLLING\n"
-
-	init SPI_v2 CONTROLLER_ASYNC
-
-	# start testing the output
-	grep "16 Bits Transaction Successful" $tempFile
-	if [[ $? -eq 0 ]];
-	then
-		result_SPI_V2_INTERRUPT='pass'
-	else
-		result_SPI_V2_INTERRUPT='fail'
-	fi
-	
-	printf "Test result for SPI_v2_INTERRUPT: $result_SPI_V2_INTERRUPT\n"
-
-	init SPI_v2 CONTROLLER_DMA
-
-	# start testing the output
-	grep "16 Bits Transaction Successful" $tempFile
-	if [[ $? -eq 0 ]];
-	then
-		result_SPI_V2_DMA='pass'
-	else
-		result_SPI_V2_DMA='fail'
-	fi
-	
-	printf "Test result for SPI_v2_DMA: $result_SPI_V2_DMA\n"
-	
-}
 function test_ICC() {
 	init ICC
 
@@ -275,21 +198,6 @@ function test_ICC() {
 	
 }
 
-function test_Hash() {
-	init Hash
-
-	# start testing the output
-	grep "Example Succeeded" $tempFile
-	if [[ $? -eq 0 ]];
-	then
-		result_Hash='pass'
-	else
-		result_Hash='fail'
-	fi
-	
-	printf "Test result for Hash: $result_Hash\n"
-	
-}
 
 function test_DMA() {
 	init DMA
@@ -325,52 +233,6 @@ function test_CRC() {
 
 
 function test_ADC() {
-
-	init ADC POLLING
-
-	# start testing the output
-	result_ADC_POLLING='pass'
-	grep "Running Single Channel Example" $tempFile
-	if [[ $? -ne 0 ]];
-	then
-		result_ADC_POLLING='fail'
-	fi
-	
-	grep "Running Temperature Sensor Example" $tempFile
-	if [[ $? -ne 0 ]];
-	then
-		result_ADC_POLLING='fail'
-	fi
-
-	grep "Running Multi Channel Example" $tempFile
-	if [[ $? -ne 0 ]];
-	then
-		result_ADC_POLLING='fail'
-	fi
-	printf "Test result for ADC_POLLING: $result_ADC_POLLING\n"
-	
-	init ADC INTERRUPT
-
-	# start testing the output
-	result_ADC_INTERRUPT='pass'
-	grep "Running Single Channel Example" $tempFile
-	if [[ $? -ne 0 ]];
-	then
-		result_ADC_INTERRUPT='fail'
-	fi
-	
-	grep "Running Temperature Sensor Example" $tempFile
-	if [[ $? -ne 0 ]];
-	then
-		result_ADC_INTERRUPT='fail'
-	fi
-
-	grep "Running Multi Channel Example" $tempFile
-	if [[ $? -ne 0 ]];
-	then
-		result_ADC_INTERRUPT='fail'
-	fi
-	printf "Test result for ADC_INTERRUPT: $result_ADC_INTERRUPT\n"
 
 	init ADC DMA
 
@@ -437,21 +299,10 @@ function summary() {
 	printf "Test result for UART_DMA: $result_UART_DMA\n"
 	printf "Test result for TRNG: $result_TRNG\n"
 	printf "Test result for I2C: $result_I2C\n"
-	printf "Test result for SPI_POLLING: $result_SPI_POLLING\n"
-	printf "Test result for SPI_INTERRUPT: $result_SPI_INTERRUPT\n"
-	printf "Test result for SPI_DMA: $result_SPI_DMA\n"
-	if [[ $boardVersion = max32690 ]]
-	then
-		printf "Test result for SPI_v2_POLLING: $result_SPI_V2_POLLING\n"
-		printf "Test result for SPI_v2_INTERRUPT: $result_SPI_V2_INTERRUPT\n"
-		printf "Test result for SPI_v2_DMA: $result_SPI_V2_DMA\n"
-	fi
+	printf "Test result for SPI: $result_SPI\n"
 	printf "Test result for ICC: $result_ICC\n"
-	printf "Test result for Hash: $result_Hash\n"
 	printf "Test result for DMA: $result_DMA\n"
 	printf "Test result for CRC: $result_CRC\n"
-	printf "Test result for ADC_POLLING: $result_ADC_POLLING\n"
-	printf "Test result for ADC_INTERRUPT: $result_ADC_INTERRUPT\n"
 	printf "Test result for ADC_DMA: $result_ADC_DMA\n"
 	printf "Test result for Library_Generate: $result_Lib_Gen\n"
 	printf "Test result for Library_Use: $result_Lib_Use\n"
@@ -468,13 +319,8 @@ function main() {
 	test_Hello_World
 	test_Hello_World_Cpp
 	test_TRNG
-	#test_I2C
+	test_I2C
 	test_SPI
-	if [[ $boardVersion = max32690 ]]
-	then
-		test_SPI_V2
-	fi
-	test_Hash
 	test_DMA
 	test_CRC
 	test_ADC
@@ -494,18 +340,10 @@ if [[ $result_UART_INTERRUPT = "fail" ||
     $result_HelloWorld_Cpp = "fail" ||
     $result_TRNG = "fail" ||
     $result_I2C = "fail" ||
- 	$result_SPI_POLLING = "fail" ||
-    $result_SPI_INTERRUPT = "fail" ||
-    $result_SPI_DMA = "fail" ||
-    $result_SPI_V2_POLLING = "fail" ||   # Only for max32690
-    $result_SPI_V2_INTERRUPT = "fail" || # Only for max32690
-    $result_SPI_V2_DMA = "fail" ||       # Only for max32690
+ 	$result_SPI = "fail" ||
     $result_ICC = "fail" ||
-    $result_Hash = "fail" ||
     $result_DMA = "fail" ||
     $result_CRC = "fail" ||
-    $result_ADC_POLLING = "fail" ||
-    $result_ADC_INTERRUPT = "fail" ||
     $result_ADC_DMA = "fail" ||
     $result_Lib_Gen = "fail" ||
     $result_Lib_Use = "fail" ]]; then
