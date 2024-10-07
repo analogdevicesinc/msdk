a0f48e9407d950359b6557c98db57b5d0fdceacd
Anderson, Glen <Glen.Anderson@analog.com>
2024-10-07 14:36:22 -0400

    Refactored Documentation/build.py for new directory layout

--- Documentation/build.py
+++ Documentation/build.py
@@ -4,7 +4,6 @@
 import re
 import shutil
 import textwrap
-from dataclasses import dataclass
 from operator import attrgetter
 from pathlib import Path
 from subprocess import run
@@ -57,9 +56,8 @@ def generate_examples_md():
     print("Searching for shared examples...")
     for directory in examples_dir.glob("*"):
         if directory.name in target_excludelist:
-            print(f"Skipping {directory.name}... (in exclude-list)")
+            print(f"Skipping {directory.name}...  (in exclude-list)")
             continue
-        print(f"-> {directory.name}")
         target_micro = directory.name
         all_target_micros.add(target_micro)
         for main_file in directory.rglob("**/main.c"):
@@ -88,6 +86,7 @@ def generate_examples_md():
         if len(e.supported_parts) > 1:
             common_entries += f"| **{e.name}** | {e.description} | {'<br/>'.join(e.supported_parts)} |\n"
 
+<<<<<<< HEAD
     markdown_content = textwrap.dedent(
         """
         ### Common Examples
@@ -180,6 +179,117 @@ def build_doxygen_docs():
             run(f"doxygen {f.name}", cwd=f.parent, shell=True, check=False)
         else:
             print(f"-> {micro} Doxygen exists, skipping")
+        dest = here / "Libraries/PeriphDrivers/Documentation" / micro
+        # ^ Recreate directory structure so built links work
+        shutil.copytree(periph_docs_dir / micro, dest, dirs_exist_ok=True)
+
+
+def build_mkdocs():
+    # Run mkdocs build
+    # A subprocess is used because the Mkdocs Python API does not print any logging info
+    print("Building docs (this could take a few minutes)...")
+    run("mkdocs build", cwd=repo, shell=True, check=False)  # Build with CLI
+
+
+=======
+    markdown_content = f"""
+    ### Common Examples
+
+    The following common examples are supported across multiple microcontrollers.
+
+    | Example | Description | Supported Parts |
+    | ------- | ----------- | --------------- |
+    {common_entries}
+
+    """
+
+    # Generate markdown content for part-specific examples
+    all_target_micros = sorted(all_target_micros)
+    specific_examples = {}
+    for micro in all_target_micros:
+        specific_examples[micro] = [
+            e
+            for e in all_examples
+            if micro in e.supported_parts and len(e.supported_parts) == 1
+        ]
+
+    for target, examples in specific_examples.items():
+        markdown_content += f"""
+        ### {target.upper()} Examples
+
+        In addition to the [Common Examples](#common-examples), the following examples 
+        are available specifically for the {target.upper()}.
+
+        | Example | Description | MSDK Location |
+        | ------- | ----------- | ------------- |
+        """
+        for e in examples:
+            relative_path = e.folder.relative_to(repo).as_posix()
+            github = (
+                f"https://github.com/analogdevicesinc/msdk/tree/release/{relative_path}"
+            )
+            markdown_content += f"| **{e.name}** | {e.description} | _Local:_`{relative_path}`<br>_Github:_ [link]({github})\n"
+
+    # generate examples.md from the template
+    src_path = repo / "Documentation/user-guide/examples.md.tpl"
+    dst_path = repo / "Documentation/user-guide/examples.md"
+    dst_path.write_text(
+        src_path.read_text(encoding="utf-8").replace(
+            "{{EXAMPLES_LIST}}", markdown_content
+        ),
+        encoding="utf-8",
+    )
+
+
+def copy_sources():
+    print("Copying root markdown files")
+    for f in repo.glob("*.md"):
+        print(f"-> {f.name}")
+        if f.name != "README.md":
+            shutil.copy(
+                f, here
+            )  # Workaround for https://github.com/mkdocs/mkdocs/issues/3313
+
+    print("Copying CLI markdown files")
+    cli_path = repo / "Libraries/CLI"
+    cli_path.mkdir(exist_ok=True, parents=True)
+    shutil.copy(
+        cli_path / "README.md",
+        here / "Libraries/CLI/README.md",
+    )
+
+    print("Copying bluetooth markdown files")
+    src = repo / "Tools/Bluetooth"
+    dst = here / "Tools/Bluetooth"
+    for f in src.glob("*.md"):
+        print(f"-> {f.name}")
+        if not dst.exists():
+            dst.mkdir(parents=True, exist_ok=True)
+        shutil.copy(f, dst)
+
+    cordio_docs_dir = repo / "Libraries/Cordio/docs"
+    platform_docs_dir = repo / "Libraries/Cordio/platform/Documentation"
+
+    print("Copying Cordio markdown files")
+    shutil.copytree(cordio_docs_dir, here / "Libraries/Cordio/docs", dirs_exist_ok=True)
+
+    print("Copying platform markdown files")
+    shutil.copytree(
+        platform_docs_dir,
+        here / "Libraries/Cordio/platform/Documentation",
+        dirs_exist_ok=True,
+    )
+
+
+def build_doxygen_docs():
+    # Run Doxygen builds
+    periph_docs_dir = repo / "Libraries/PeriphDrivers/Documentation"
+    for f in periph_docs_dir.glob("*_Doxyfile"):
+        micro = f.name.split("_")[0].upper()  # max32xxx_Doxyfile -> MAX32XXX
+        if not (periph_docs_dir / micro).exists():
+            run(f"doxygen {f.name}", cwd=f.parent, shell=True, check=False)
+        else:
+            print(f"{micro} Doxygen exists, skipping")
         dest = here / "Libraries/PeriphDrivers/Documentation" / micro
         # ^ Recreate directory structure so built links work
         shutil.copytree(periph_docs_dir / micro, dest, dirs_exist_ok=True)
