b0bf19634292cebfc362992aac2670001f20e07a
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2024-10-08 19:13:50 -0500

    chore(Other): Sync `release` branch with `main` (#1202)
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Anıl Kara <anil.kara@analog.com>
    Signed-off-by: Maureen Helm <maureen.helm@analog.com>
    Signed-off-by: Furkan Akkiz <hasanfurkan.akkiz@analog.com>
    Signed-off-by: Tahsin Mutlugun <Tahsin.Mutlugun@analog.com>
    Signed-off-by: Michael Perkins <michael.perkins@analog.com>
    Signed-off-by: Kevin Townsend <kevin.townsend@analog.com>
    Co-authored-by: Lorne Smith <65424047+lorne-maxim@users.noreply.github.com>
    Co-authored-by: Jake Carter <jake.carter@analog.com>
    Co-authored-by: Sadik Ozer <46590392+ozersa@users.noreply.github.com>
    Co-authored-by: Jake Carter <jhcarter.co@gmail.com>
    Co-authored-by: ahmetyazicii <83782637+ahmetyazicii@users.noreply.github.com>
    Co-authored-by: Brent K <129296493+BrentK-ADI@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Omercan Kilic <134288264+kilicomercan@users.noreply.github.com>
    Co-authored-by: EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
    Co-authored-by: Jake-Carter <Jake-Carter@users.noreply.github.com>
    Co-authored-by: Anıl Kara <karaanil@users.noreply.github.com>
    Co-authored-by: Brandon Hurst <82959533+Brandon-Hurst@users.noreply.github.com>
    Co-authored-by: EricB-ADI <EricB-ADI@users.noreply.github.com>
    Co-authored-by: Victor Loginov <65688219+vicloginov@users.noreply.github.com>
    Co-authored-by: lorne-maxim <lorne-maxim@users.noreply.github.com>
    Co-authored-by: Sadik Ozer <sadik.ozer@analog.com>
    Co-authored-by: Maureen Helm <maureen.helm@analog.com>
    Co-authored-by: Furkan AKKIZ <94184469+hfakkiz@users.noreply.github.com>
    Co-authored-by: isztldav <isztl.david@gmail.com>
    Co-authored-by: JC-282-AD <Jason-X.Cai@analog.com>
    Co-authored-by: Oğuzhan Büyüksolak <oguzhan.buyuksolak@analog.com>
    Co-authored-by: JC-282-AD <JC-282-AD@users.noreply.github.com>
    Co-authored-by: Mehmet Ozgun Kanal <123065567+OzgunKanal@users.noreply.github.com>
    Co-authored-by: Ozgun <asdasad@asdasd.com>
    Co-authored-by: Noxi123 <61051098+Noxi123@users.noreply.github.com>
    Co-authored-by: Changmin Jeon <changmina141217@gmail.com>
    Co-authored-by: ersahatmacaa <146827243+ersahatmacaa@users.noreply.github.com>
    Co-authored-by: Kevin Gillespie <kevin.gillespie@maximintegrated.com>
    Co-authored-by: Kevin Gillespie <kevin.gillespie@analog.com>
    Co-authored-by: Kalvin McCallum <137926624+kmccalluADI@users.noreply.github.com>
    Co-authored-by: Omer Demir <Omer.Demir@analog.com>
    Co-authored-by: sihyung-maxim <sihyung-maxim@users.noreply.github.com>
    Co-authored-by: EricB-ADI <eric.busch@analogdevices.com>
    Co-authored-by: Tahsin Türker Mutlugün <Tahsin.Mutlugun@analog.com>
    Co-authored-by: Michael Perkins <157516771+perkinsmg@users.noreply.github.com>
    Co-authored-by: Kevin Townsend <kevin@ktownsend.com>
    Co-authored-by: Bobby <bobby@bobbycounts.com>
    Co-authored-by: Bobby Counts <bobby.counts@analog.com>
    Co-authored-by: Mert Ekren <mert.ekren@analog.com>
    Co-authored-by: Anıl Kara <anil.kara@analog.com>
    Co-authored-by: Ozan Durgut <49796358+ozan956@users.noreply.github.com>
    Co-authored-by: Cristian Cruz <71054319+crsz20@users.noreply.github.com>

--- Libraries/Cordio/controller/sources/ble/lhci/lhci_cmd_vs.c
+++ Libraries/Cordio/controller/sources/ble/lhci/lhci_cmd_vs.c
@@ -7,6 +7,8 @@
  *  Copyright (c) 2013-2019 Arm Ltd. All Rights Reserved.
  *
  *  Copyright (c) 2019-2020 Packetcraft, Inc.
+ *
+ *  Portions Copyright (c) 2024 Analog Devices, Inc.
  *  
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
