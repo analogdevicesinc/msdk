2e34472705532ace08eff8bfb6c5ab400daa5950
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2024-10-11 11:44:26 -0500

    fix(CMSIS,PeriphDrivers): Remove SPI TS3, fix GPIO pad controls, and fix build errors for MAX32657 (#1226)

--- Libraries/PeriphDrivers/Include/MAX32657/gpio.h
+++ Libraries/PeriphDrivers/Include/MAX32657/gpio.h
@@ -151,8 +151,6 @@ typedef enum {
  */
 typedef enum {
     MXC_GPIO_PAD_NONE, /**< No pull-up or pull-down */
-    MXC_GPIO_PAD_PULL_UP, /**< Set pad to strong pull-up */
-    MXC_GPIO_PAD_PULL_DOWN, /**< Set pad to strong pull-down */
     MXC_GPIO_PAD_WEAK_PULL_UP, /**< Set pad to weak pull-up */
     MXC_GPIO_PAD_WEAK_PULL_DOWN /**< Set pad to weak pull-down */
 } mxc_gpio_pad_t;
