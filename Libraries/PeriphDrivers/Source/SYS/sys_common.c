ed4479283fc18bf7919028e8fcc384774685a58e
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-10-23 11:42:39 -0500

    added basic api for get package and date code

---
+++ Libraries/PeriphDrivers/Source/SYS/sys_common.c
@@ -0,0 +1,123 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. (now owned by 
+ * Analog Devices, Inc.),
+ * Copyright (C) 2023-2024 Analog Devices, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+/**
+ * @file mxc_sys.c
+ * @brief      System layer driver.
+ * @details    This driver is used to control the system layer of the device.
+ */
+
+/* **** Includes **** */
+#include <stddef.h>
+#include "mxc_device.h"
+#include "mxc_assert.h"
+#include "mxc_sys.h"
+#include "flc.h"
+#include "mxc_delay.h"
+#include "gcr_regs.h"
+#include "fcr_regs.h"
+#include "mcr_regs.h"
+
+/**
+ * @ingroup mxc_sys
+ * @{
+ */
+
+/* **** Definitions **** */
+#define DAY_CODE_OFFSET 0x14
+#define MONTH_CODE_OFFSET 0x15
+#define YEAR_CODE_OFFSET 0x16
+#define PKG_CODE_OFFSET 0x17
+#define REG8_VAL(addr) (*(volatile uint8_t *)(MXC_INFO_MEM_BASE + addr))
+/* **** Globals **** */
+static mxc_sys_package_type_t pkg_type = MXC_SYS_PKG_UNSET;
+/* **** Functions **** */
+/* ************************************************************************** */
+mxc_sys_package_type_t MXC_SYS_GetPackageType(void)
+{
+    if(pkg_type != MXC_SYS_PKG_UNSET)
+    {
+        return pkg_type;
+    }
+
+
+    MXC_FLC_UnlockInfoBlock(MXC_INFO_MEM_BASE);
+
+    mxc_sys_date_t date_info;
+    MXC_SYS_GetTestDate(&date_info);
+
+    // Package codes were only introduced when test date was
+    mxc_sys_date_t date;
+    int err = MXC_SYS_GetTestDate(&date);
+
+    if(err != E_NO_ERROR)
+    {
+        pkg_type = MXC_SYS_PKG_UNSET;
+    }
+    else
+    {
+        const uint8_t maybe_pkg_type = REG8_VAL(PKG_CODE_OFFSET);
+        int err = MXC_SYS_SetPackageType(maybe_pkg_type);
+        MXC_ASSET(err == E_NO_ERROR);
+    }
+    
+
+
+    MXC_FLC_LockInfoBlock(MXC_INFO_MEM_BASE);
+
+    return pkg_type;
+}
+int MXC_SYS_SetPackageType(mxc_sys_package_type_t new_pkg_type)
+{
+    switch(new_pkg_type)
+    {
+        case MXC_SYS_PKG_TQFN:
+        case MXC_SYS_PKG_BGA:
+        case MXC_SYS_PKG_WLP:
+        case MXC_SYS_PKG_UNSET:
+            pkg_type = new_pkg_type;
+            return E_NO_ERROR;
+        default:
+            return E_BAD_PARAM;
+    }
+}
+
+
+int MXC_SYS_GetTestDate(mxc_sys_date_t *date_info)
+{   
+    MXC_ASSERT(date_info);
+    
+    date_info->day = REG8_VAL(DAY_CODE_OFFSET);
+    date_info->month = REG8_VAL(MONTH_CODE_OFFSET);
+    date_info->year = REG8_VAL(YEAR_CODE_OFFSET);
+
+    // Flash is cleared if not valid
+    // Sorry dev in 2255
+    if(date_info->year == 0xff || date_info->month == 0xff || date_info->day == 0xff)
+    {
+        return E_INVALID;
+    }
+    
+    
+    return E_NO_ERROR;
+
+}
+
+/**@} end of mxc_sys */
