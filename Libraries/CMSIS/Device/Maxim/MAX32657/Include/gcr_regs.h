371f6960ff4c2aea5bb9272b6f84a44f532954f9
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-11-11 09:25:14 -0600

    reggen ME30

--- Libraries/CMSIS/Device/Maxim/MAX32657/Include/gcr_regs.h
+++ Libraries/CMSIS/Device/Maxim/MAX32657/Include/gcr_regs.h
@@ -7,7 +7,9 @@
 
 /******************************************************************************
  *
- * Copyright (C) 2024 Analog Devices, Inc.
+ * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. (now owned by 
+ * Analog Devices, Inc.),
+ * Copyright (C) 2023-2024 Analog Devices, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +50,11 @@ extern "C" {
 #define __IO volatile
 #endif
 #ifndef __I
-#define __I  volatile const
+#ifdef __cplusplus
+#define __I volatile
+#else
+#define __I volatile const
+#endif
 #endif
 #ifndef __O
 #define __O  volatile
@@ -571,6 +577,14 @@ typedef struct {
 
 #define MXC_F_GCR_BTLELDOCTRL_BB_VSEL_POS              2 /**< BTLELDOCTRL_BB_VSEL Position */
 #define MXC_F_GCR_BTLELDOCTRL_BB_VSEL                  ((uint32_t)(0x3UL << MXC_F_GCR_BTLELDOCTRL_BB_VSEL_POS)) /**< BTLELDOCTRL_BB_VSEL Mask */
+#define MXC_V_GCR_BTLELDOCTRL_BB_VSEL_0_9              ((uint32_t)0x0UL) /**< BTLELDOCTRL_BB_VSEL_0_9 Value */
+#define MXC_S_GCR_BTLELDOCTRL_BB_VSEL_0_9              (MXC_V_GCR_BTLELDOCTRL_BB_VSEL_0_9 << MXC_F_GCR_BTLELDOCTRL_BB_VSEL_POS) /**< BTLELDOCTRL_BB_VSEL_0_9 Setting */
+#define MXC_V_GCR_BTLELDOCTRL_BB_VSEL_1_0              ((uint32_t)0x1UL) /**< BTLELDOCTRL_BB_VSEL_1_0 Value */
+#define MXC_S_GCR_BTLELDOCTRL_BB_VSEL_1_0              (MXC_V_GCR_BTLELDOCTRL_BB_VSEL_1_0 << MXC_F_GCR_BTLELDOCTRL_BB_VSEL_POS) /**< BTLELDOCTRL_BB_VSEL_1_0 Setting */
+#define MXC_V_GCR_BTLELDOCTRL_BB_VSEL_1_1              ((uint32_t)0x2UL) /**< BTLELDOCTRL_BB_VSEL_1_1 Value */
+#define MXC_S_GCR_BTLELDOCTRL_BB_VSEL_1_1              (MXC_V_GCR_BTLELDOCTRL_BB_VSEL_1_1 << MXC_F_GCR_BTLELDOCTRL_BB_VSEL_POS) /**< BTLELDOCTRL_BB_VSEL_1_1 Setting */
+#define MXC_V_GCR_BTLELDOCTRL_BB_VSEL_1_2              ((uint32_t)0x3UL) /**< BTLELDOCTRL_BB_VSEL_1_2 Value */
+#define MXC_S_GCR_BTLELDOCTRL_BB_VSEL_1_2              (MXC_V_GCR_BTLELDOCTRL_BB_VSEL_1_2 << MXC_F_GCR_BTLELDOCTRL_BB_VSEL_POS) /**< BTLELDOCTRL_BB_VSEL_1_2 Setting */
 
 #define MXC_F_GCR_BTLELDOCTRL_RF_EN_POS                4 /**< BTLELDOCTRL_RF_EN Position */
 #define MXC_F_GCR_BTLELDOCTRL_RF_EN                    ((uint32_t)(0x1UL << MXC_F_GCR_BTLELDOCTRL_RF_EN_POS)) /**< BTLELDOCTRL_RF_EN Mask */
