6a7e40ff01f3d4a5e4d6927b0a83a08c00e7d1d8
Omercan Kilic <134288264+kilicomercan@users.noreply.github.com>
2024-12-05 00:15:35 +0300

    fix(Examples,PeriphDrivers): Fix LPUART clock source settings for MAX78000 (#1284)

--- Examples/MAX78000/UART/main.c
+++ Examples/MAX78000/UART/main.c
@@ -102,8 +102,13 @@ int main(void)
         printf("-->Example Failed\n");
         return error;
     }
-
+#if defined(BOARD_EVKIT_V1)
     if ((error = MXC_UART_Init(WRITING_UART, UART_BAUD, MXC_UART_APB_CLK)) != E_NO_ERROR) {
+#elif defined(BOARD_FTHR_REVA)
+    // We define UART3(LPUART) as WRITING_UART for FTHR Board.
+    // LPUART can use IBRO and ERTCO clocks.
+    if ((error = MXC_UART_Init(WRITING_UART, UART_BAUD, MXC_UART_IBRO_CLK)) != E_NO_ERROR) {
+#endif
         printf("-->Error initializing UART: %d\n", error);
         printf("-->Example Failed\n");
         return error;
