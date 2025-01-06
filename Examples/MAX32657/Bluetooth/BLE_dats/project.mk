9bee6d15f47d0e8bd32125a75946845f30b49c9c
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2025-01-06 15:16:10 -0600

    added dats, and updated linker script

---
+++ Examples/MAX32657/Bluetooth/BLE_dats/project.mk
@@ -0,0 +1,55 @@
+###############################################################################
+ #
+ # Copyright (C) 2024 Analog Devices, Inc.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+ ##############################################################################
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://analogdevicesinc.github.io/msdk/USERGUIDE/#build-system
+
+# **********************************************************
+
+# Enable Cordio library
+LIB_CORDIO = 1
+
+
+CORDIO_DIR = $(LIBS_DIR)/Packetcraft-ADI
+LIB_PHY_DIR ?= $(LIBS_DIR)/RF-PHY
+
+# Cordio library options
+INIT_PERIPHERAL = 1
+INIT_CENTRAL = 0
+
+# TRACE option
+# Set to 0 to disable
+# Set to 1 to enable serial port trace messages
+# Set to 2 to enable verbose messages
+TRACE = 1
+
+# Add services directory to build
+IPATH += services
+VPATH += services
+
+# SET advertising name
+ADV_NAME?=DATS_ME30
+PROJ_CFLAGS += -DADV_NAME=\"$(ADV_NAME)\"
+
+
+### CONFIGURE security
+# /*! TRUE to initiate security upon connection*/
+PROJ_CFLAGS += -DINIT_SECURITY=FALSE
