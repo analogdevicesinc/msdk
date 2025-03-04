30dc051d587e1091a241fb867da0d8eac0b11bda
Brent Kowal <brent.kowal@analog.com>
2025-03-04 09:44:27 -0500

    MAX78000 RF Spectrum Sense Demo
    Initial commit of the RF Spectrum Sensing demo for MAX78000. The application software/demo was based on
    the UNet-highres-demo example and updated to leverage the model and sample data for the RF spectrum sensing
    application.

---
+++ Examples/MAX78000/CNN/spectrumsense_unet-demo/TFT/evkit/bitmap.h
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. (now owned by
+ * Analog Devices, Inc.),
+ * Copyright (C) 2023-2025 Analog Devices, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+#ifndef EXAMPLES_MAX78000_CNN_SPECTRUMSENSE_UNET_DEMO_TFT_EVKIT_BITMAP_H_
+#define EXAMPLES_MAX78000_CNN_SPECTRUMSENSE_UNET_DEMO_TFT_EVKIT_BITMAP_H_
+
+// bitmaps id
+#define ADI_Logo_RGB_Solid_small_256_bmp 0
+
+// fonts id
+#define urw_gothic_12_grey_bg_white 0
+
+#endif // EXAMPLES_MAX78000_CNN_SPECTRUMSENSE_UNET_DEMO_TFT_EVKIT_BITMAP_H_
