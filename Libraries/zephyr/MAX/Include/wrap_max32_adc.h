7e62653958980b1e0259fab52f6381e9e1e4f1f5
Pete Johanson <pete.johanson@analog.com>
2025-07-07 11:48:33 -0600

    fix(Other): Add fallback ADC wrapper on !MAX32690
    Only define the ADC async stream wrapper on MAX32690 where it is
    supported, otherwise include a fallback that returns E_NOT_SUPPORTED.
    
    Signed-off-by: Pete Johanson <pete.johanson@analog.com>

--- Libraries/zephyr/MAX/Include/wrap_max32_adc.h
+++ Libraries/zephyr/MAX/Include/wrap_max32_adc.h
@@ -144,6 +144,12 @@ static inline int Wrap_MXC_ADC_StartConversionAsync(uint32_t *sample_channels,
     return MXC_ADC_StartConversionAsync((mxc_adc_chsel_t)channel_id, callback);
 }
 
+static inline int Wrap_MXC_ADC_StartConversionAsyncStream(uint32_t *sample_channels,
+                                                          mxc_adc_complete_cb_t callback)
+{
+    return E_NOT_SUPPORTED;
+}
+
 static inline void Wrap_MXC_ADC_GetData(uint16_t **outdata)
 {
     MXC_ADC_GetData(*outdata);
@@ -284,6 +290,8 @@ static inline int Wrap_MXC_ADC_StartConversionAsync(uint32_t *sample_channels,
     return MXC_ADC_StartConversionAsync(callback);
 }
 
+#if defined(CONFIG_SOC_MAX32690)
+
 static inline int Wrap_MXC_ADC_StartConversionAsyncStream(uint32_t *sample_channels,
                                                           mxc_adc_complete_cb_t callback)
 {
@@ -291,6 +299,16 @@ static inline int Wrap_MXC_ADC_StartConversionAsyncStream(uint32_t *sample_chann
     Wrap_MXC_ADC_ChannelSelect(sample_channels);
     return MXC_ADC_StartConversionAsyncStream(callback);
 }
+
+#else
+
+static inline int Wrap_MXC_ADC_StartConversionAsyncStream(uint32_t *sample_channels,
+                                                          mxc_adc_complete_cb_t callback)
+{
+    return E_NOT_SUPPORTED;
+}
+
+#endif
 
 static inline void Wrap_MXC_ADC_GetData(uint16_t **outdata)
 {
