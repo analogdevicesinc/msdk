315a64ac7ead9a6a68463d438603994aa887a9a0
dnguye14-adi <dung.nguyen@analog.com>
2025-08-14 11:28:10 +0700

    feat(Examples): MSDK-1250: Add OWM Example for MAX32690
    Signed-off-by: Dung Nguyen <dung.nguyen@analog.com>

---
+++ Examples/MAX32690/OWM/project.mk
@@ -0,0 +1,40 @@
+###############################################################################
+ #
+ # Copyright (C) 2024-2025 Analog Devices, Inc.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+ ##############################################################################
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://analogdevicesinc.github.io/msdk/USERGUIDE/#build-system
+
+# **********************************************************
+
+# Add your config here!
+
+# If you have secure version of MCU, set SBT=1 to generate signed binary
+# For more information on how sing process works, see
+# https://www.analog.com/en/education/education-library/videos/6313214207112.html
+SBT=0
+
+
+
+
+
+
+
+
