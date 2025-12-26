50123d9ffe9340feead669a2f0d3f345abda549b
TSenguler <tayfun.senguler@analog.com>
2025-12-26 11:37:40 +0300

    Update copyright year to 2023-2025
    Updated copyright year for Analog Devices, Inc. from 2024 to 2025

--- Examples/MAX78002/USB/MAXUSB/USB_HIDKeyboard/main.c
+++ Examples/MAX78002/USB/MAXUSB/USB_HIDKeyboard/main.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. (now owned by 
  * Analog Devices, Inc.),
- * Copyright (C) 2023-2024 Analog Devices, Inc.
+ * Copyright (C) 2023-2025 Analog Devices, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
