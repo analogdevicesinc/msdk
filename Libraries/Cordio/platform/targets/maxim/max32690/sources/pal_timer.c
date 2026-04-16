b88eff949c42cc1d731d639243f7fb369a07517f
Kevin Peterson <khp@alum.mit.edu>
2026-04-16 11:49:54 -0700

    fix typo

--- Libraries/Cordio/platform/targets/maxim/max32690/sources/pal_timer.c
+++ Libraries/Cordio/platform/targets/maxim/max32690/sources/pal_timer.c
@@ -415,8 +415,8 @@ uint32_t PalTimerGetExpTime(void)
 
     /* Adjust time based on the calibrated value */
     time = time - ((time / PAL_TMR_CALIB_TIME) * palTimerCb.usecDiff);
-    time = time - ((((int32_t)time % PAL_TMR_CALIB_TIME_US) * palTimerCb.usecDiff) /
-                   PAL_TMR_CALIB_TIME_US);
+    time = time - ((((int32_t)time % PAL_TMR_CALIB_TIME) * palTimerCb.usecDiff) /
+                   PAL_TMR_CALIB_TIME);
 
     return time;
 }
