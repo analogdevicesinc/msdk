374e98371ea48d024c0354ed12582f1a756a3ce2
Kevin Peterson <khp@alum.mit.edu>
2026-04-28 09:21:45 -0700

    #1497: now that Cordio task priority fixed at configMAX_PRIORITIES - 3, set Timer task priority to configMAX_PRIORITIES - 2 to prevent deadlock on calls to xTimerStop/xTimerDelete

--- Examples/MAX32690/Bluetooth/RF_Test/FreeRTOSConfig.h
+++ Examples/MAX32690/Bluetooth/RF_Test/FreeRTOSConfig.h
@@ -67,7 +67,7 @@ increase the amount of time spent in standby mode, thus reducing average power c
 #define configUSE_MUTEXES 1
 
 #define configUSE_TIMERS 1
-#define configTIMER_TASK_PRIORITY (configMAX_PRIORITIES - 3)
+#define configTIMER_TASK_PRIORITY (configMAX_PRIORITIES - 2)
 #define configTIMER_QUEUE_LENGTH 8
 #define configTIMER_TASK_STACK_DEPTH configMINIMAL_STACK_SIZE
 
