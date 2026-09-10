c1983a4ee1f34f5e1cd7ebfafdff44501773a619
dnguye14-adi <dung.nguyen@analog.com>
2026-09-10 10:16:04 +0700

    fix(Examples): MSDK-1325: Fix ADC example build warning on MAX32672
    Signed-off-by: dnguye14-adi <dung.nguyen@analog.com>

--- Examples/MAX32672/ADC/main.c
+++ Examples/MAX32672/ADC/main.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. (now owned by 
  * Analog Devices, Inc.),
- * Copyright (C) 2023-2025 Analog Devices, Inc.
+ * Copyright (C) 2023-2026 Analog Devices, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -211,7 +211,7 @@ void temperature_average(float temperature)
 
     if (temp_samples == SAMPLE_AVG) {
         average = sum / SAMPLE_AVG;
-        printf("Average = %0.2fC\n\n", average);
+        printf("Average = %0.2fC\n\n", (double) average);
 
         temp_samples = 0;
         sum = 0;
